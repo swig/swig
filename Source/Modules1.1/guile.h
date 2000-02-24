6fb29ecbfb53dfc7d9597f43e5df02dcc4efa449
Thien-Thi Nguyen <ttn@glug.org>
2000-02-24 18:24:07 +0000

    {Incorporate support contributed by Clark McGrew.}
    (prefix, package): New data members.
    (get_pointer): Take additional arg `WrapperFunction&'.
    (usage_var, usage_func, usage_const): Last arg now `String&'.
    (usage_returns): New func.
    
    (GUILE): Move definition into guile.cxx.
    (parse_args, parse, create_function, link_variable,
    declare_const, initialize, headers, close, set_module,
    set_init, create_command): Don't declare these virtual.
    
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@256 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules1.1/guile.h
+++ Source/Modules1.1/guile.h
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Simplified Wrapper and Interface Generator  (SWIG)
- * 
+ *
  * Author : David Beazley
  *
- * Department of Computer Science        
+ * Department of Computer Science
  * University of Chicago
  * 1100 E 58th Street
  * Chicago, IL  60637
@@ -21,37 +21,32 @@
  * Guile implementation
  * (Caution : This is *somewhat* experimental)
  *
- * Seeking : highly motivated individual with plenty of spare time and
- *           a love of Guile.   Must be willing to modify this code and
- *           make it better.
  **************************************************************************/
 
-class GUILE : public Language {
+class GUILE : public Language
+{
 private:
   char   *guile_path;
+  char   *prefix;
   char   *module;
-  void   get_pointer(char *iname, int parm, DataType *t);
-  void   usage_var(char *, DataType *, char **);
-  void   usage_func(char *, DataType *, ParmList *, char **);
-  void   usage_const(char *, DataType *, char *, char **);
+  char   *package;
+  void   get_pointer(char *iname, int parm, DataType *t, WrapperFunction &f);
+  void   usage_var(char *, DataType *, String &usage);
+  void   usage_func(char *, DataType *, ParmList *, String &usage);
+  void   usage_returns(char *, DataType *, ParmList *, String &usage);
+  void   usage_const(char *, DataType *, char *, String &usage);
+
 public :
-  GUILE() {
-     module = 0;
-     guile_path = "guile";
-  }
-  virtual void parse_args(int, char *argv[]);
-  virtual void parse();
-  virtual void create_function(char *, char *, DataType *, ParmList *);
-  virtual void link_variable(char *, char *, DataType *);
-  virtual void declare_const(char *, char *, DataType *, char *);
-  virtual void initialize();
-  virtual void headers(void);
-  virtual void close(void);
-  virtual void set_module(char *, char **);
-  virtual void set_init(char *);
-  virtual void create_command(char *, char *) { };
+  GUILE ();
+  void parse_args (int, char *argv[]);
+  void parse ();
+  void create_function (char *, char *, DataType *, ParmList *);
+  void link_variable (char *, char *, DataType *);
+  void declare_const (char *, char *, DataType *, char *);
+  void initialize ();
+  void headers (void);
+  void close (void);
+  void set_module (char *, char **);
+  void set_init (char *);
+  void create_command (char *, char *) { };
 };
-
-
-
-
