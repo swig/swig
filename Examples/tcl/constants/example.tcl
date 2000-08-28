db29f1317d0c6394d7e3f3c4862569dfcdd66b7e
Dave Beazley <dave-swig@dabeaz.com>
2000-08-28 19:05:55 +0000

    Added some tests for escape sequences
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@701 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/tcl/constants/example.tcl
+++ Examples/tcl/constants/example.tcl
@@ -3,13 +3,15 @@
 catch { load ./example.so example}
 catch { load ./example.dll example}    ;# Windows
 
-puts "ICONST = $ICONST (should be 42)"
-puts "FCONST = $FCONST (should be 2.1828)"
-puts "CCONST = $CCONST (should be 'x')"
-puts "SCONST = $SCONST (should be 'Hello World')"
-puts "EXPR   = $EXPR (should be 48.5484)"
-puts "iconst = $iconst (should be 37)"
-puts "fconst = $fconst (should be 3.14)"
+puts "ICONST  = $ICONST (should be 42)"
+puts "FCONST  = $FCONST (should be 2.1828)"
+puts "CCONS T = $CCONST (should be 'x')"
+puts "CCONST2 = $CCONST2 (this should be on a separate line)"
+puts "SCONST  = $SCONST (should be 'Hello World')"
+puts "SCONST2 = $SCONST2 (should be '\"Hello World\"')" 
+puts "EXPR    = $EXPR (should be 48.5484)"
+puts "iconst  = $iconst (should be 37)"
+puts "fconst  = $fconst (should be 3.14)"
 
 if { [catch {
     puts "EXTERN = $EXTERN (Arg! This shouldn't print anything)"
