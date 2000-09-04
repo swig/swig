92093ca12d42a4b0e365b550c2a5de652bd6e74e
Dave Beazley <dave-swig@dabeaz.com>
2000-09-04 15:30:21 +0000

    new example
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@841 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/python/class/example.h
+++ Examples/python/class/example.h
@@ -9,11 +9,9 @@ public:
     nshapes--;
   };
   double  x, y;   
-  char   foo[256];
   void    move(double dx, double dy);
   virtual double area() = 0;
   virtual double perimeter() = 0;
-  char *name() { return "Shape"; }
   static  int nshapes;
 };
 
@@ -24,13 +22,6 @@ public:
   Circle(double r) : radius(r) { };
   virtual double area();
   virtual double perimeter();
-};
-
-class XCircle : public Circle {
- public:
-  XCircle(double r) : Circle(r) { };
-  virtual double area();
-  char *name() { return "XCircle"; }
 };
   
 class Square : public Shape {
--- Examples/python/class/example.i
+++ Examples/python/class/example.i
@@ -6,9 +6,5 @@
 %}
 
 /* Let's just grab the original header file here */
-%typemap(memberin) char [ANY] {
-    strncpy($target,$source,$dim0);
-}
-
 %include "example.h"
 
