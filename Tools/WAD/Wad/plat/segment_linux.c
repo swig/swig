832a1f4713da114f58507323016ca4f4a0da8ae8
Dave Beazley <dave-swig@dabeaz.com>
2000-11-11 19:02:42 +0000

    Cleanup
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@943 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Tools/WAD/Wad/plat/segment_linux.c
+++ Tools/WAD/Wad/plat/segment_linux.c
@@ -11,83 +11,50 @@
  * See the file LICENSE for information on usage and redistribution.	
  * ----------------------------------------------------------------------------- */
 
-static
-WadSegment *wad_segment_read() {
-  FILE   *f;
-  int     dz;
-  int     offset = 0;
-  int     i;
-  int     n = 0;
-  char    pbuffer[1024];
-  char    *c;
-  WadSegment *s;
-  
-  /* Try to load the virtual address map */
+static FILE *
+segment_open() {
+  FILE *f;
   f = fopen("/proc/self/maps", "r");
-  if (!f) return 0;
-  nsegments = 0;
-  while (1) {
-    if (fgets(pbuffer,1024,f) == NULL) break;
-    nsegments++;
-  }
-  nsegments++;
-  fclose(f);
+  return f;
+}
 
-  dz = open("/dev/zero", O_RDWR, 0644);
-  if (dz < 0) {
-    puts("Couldn't open /dev/zero\n");
-    return 0;
-  }
-  segments = (WadSegment *) mmap(NULL, nsegments*sizeof(WadSegment), PROT_READ | PROT_WRITE, MAP_PRIVATE, dz, 0);
-  close(dz);
-  segments_size = nsegments*sizeof(WadSegment);
+static int 
+segment_read(FILE *fs, WadSegment *s)
+{
+  char pbuffer[1024];
+  char *c;
+  int  len;
+  c = fgets(pbuffer,1024,fs);
+  if (!c) return 0;
+
+  pbuffer[strlen(pbuffer)-1] = 0;   /* Chop off endline */
+
+  /* Break up the field into records */
+  /*    0-8       : Starting address
+	9-17      : Ending Address
+	18        : r
+	19        : w
+	20        : x
+	21        : p
+	23-31     : Offset 
+	49-       : Filename */
   
-  f = fopen("/proc/self/maps","r");
-  if (!f) return;
-  i = 0;
-  s = segments;
-  while (1) {
-    c = fgets(pbuffer,1024,f);
-    if (!c) break;
-
-    pbuffer[strlen(pbuffer)-1] = 0;   /* Chop off endline */
-
-    /* Break up the field into records */
-    /*    0-8       : Starting address
-          9-17      : Ending Address
-          18        : r
-          19        : w
-          20        : x
-          21        : p
-          23-31     : Offset 
-          49-       : Filename */
-
-    pbuffer[8] = 0;
-    pbuffer[17] = 0;
-    pbuffer[31] = 0;
+  len = strlen(pbuffer);
+  pbuffer[8] = 0;
+  pbuffer[17] = 0;
+  pbuffer[31] = 0;
+  s->mapname[0] = 0;
+  s->mappath[0] = 0;
+  if (len >= 49) {
     strcpy(s->mapname, pbuffer+49);
     strcpy(s->mappath, pbuffer+49);
-
-    s->vaddr = (char *) strtoul(pbuffer,NULL,16);
-    s->size = strtoul(pbuffer+9,NULL,16) - (long) (s->vaddr);
-    s->offset = strtoul(pbuffer+23,NULL,16);
-    s->base = s->vaddr;
-    s++;
-  }
-  fclose(f);
-  return segments;
+  } 
+  s->vaddr = (char *) strtoul(pbuffer,NULL,16);
+  s->size = strtoul(pbuffer+9,NULL,16) - (long) (s->vaddr);
+  s->offset = strtoul(pbuffer+23,NULL,16);
+  s->base = s->vaddr;
+  s++;
+  return 1;
 }
 
-/* -----------------------------------------------------------------------------
- * wad_segment_release()
- *
- * This function releases all of the segments.
- * ----------------------------------------------------------------------------- */
 
-void 
-wad_segment_release() {
-  munmap((void *)segments, segments_size);
-  segments = 0;
-  segments_size = 0;
-  nsegments = 0;
-}
