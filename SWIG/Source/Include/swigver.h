5a09bacd6443b971255e666699772544eb45ce1a
William S Fulton <wsf@fultondesigns.co.uk>
2001-07-17 09:30:03 +0000

    Parser bug fixes: * Bug fix in parser for virtual destructor with void as parameter * Bug fix in parser #defines embedded within classes/structs/unions Consequently %constant  can now also be placed within a struct/class/union. * Bug fix in parser to allow sizeof(*I_am_a_pointer) within a #define
    Examples will be added into a test-suite in the near future.
    
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@1278 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Source/Include/swigver.h
+++ SWIG/Source/Include/swigver.h
@@ -2,9 +2,9 @@
 /* SWIG version information */
 
 #ifndef SWIG_VERSION
-#define SWIG_VERSION "1.3u-20010628-1103"
+#define SWIG_VERSION "1.3.6u-20010716-1424"
 #endif
 
 #ifndef SWIG_SPIN
-#define SWIG_SPIN "(Alpha 5)"
+#define SWIG_SPIN "6"
 #endif
