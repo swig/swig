bfd1580700bfdeb20ba39cc4971485f48f87fcc7
Richard Palmer <richard@magicality.org>
2001-11-14 23:06:08 +0000

    New PHP4 Module
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@1927 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Source/Modules1.1/php4.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Simplified Wrapper and Interface Generator  (SWIG)
+ * 
+ * Author : David Beazley
+ *
+ * Department of Computer Science        
+ * University of Chicago
+ * 1100 E 58th Street
+ * Chicago, IL  60637
+ * beazley@cs.uchicago.edu
+ *
+ * Please read the file LICENSE for the copyright and terms by which SWIG
+ * can be used and distributed.
+ *******************************************************************************/
+
+class PHP4 : public Language {
+public :
+	virtual void parse_args(int, char *argv[]);
+	virtual void parse();
+	virtual void create_function(char *, char *, SwigType *, ParmList *);
+	virtual void link_variable(char *, char *, SwigType *);
+	virtual void declare_const(char *, char *, SwigType *, char *);
+	virtual void initialize(void);
+	virtual void headers(void);
+	virtual void close(void);
+	virtual void set_module(char *);
+	virtual void add_native(char *, char *, SwigType *, ParmList *);
+	virtual void create_command(char *, char *);
+	virtual void import(char *);
+
+	// Modular Support
+	
+	/*
+	virtual void cpp_open_class(char *classname, char *rename, char *ctype, int strip);
+	virtual void cpp_close_class();
+	virtual void cpp_member_func(char *name, char *iname, SwigType *t, ParmList *;);
+	virtual void cpp_static_func(char *name, char *iname, SwigType *t, ParmList *l);
+	virtual void cpp_variable(char *name, char *iname, SwigType *t);
+	virtual void cpp_constructor(char *name, char *iname, ParmList *l);
+	virtual void cpp_destuctor(char *name, char *newname);
+	virtual void cpp_inherit(char **baseclass, int mode = INHERIT_ALL);
+	virtual void cpp_declare_const(char *name, char *iname, SwigType *type, char *value);
+	virtual void cpp_class_descl(char *, char *, char *);
+	virtual void add_typedef(SwigType *t, char *name);
+	*/
+};
