523951150e6b853855e3f46a49757bc327d603e2
Dave Beazley <dave-swig@dabeaz.com>
2001-12-08 22:26:38 +0000

    Whoops. Extra C++ comment in C source.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@2050 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Source/CParse/cparse.h
+++ SWIG/Source/CParse/cparse.h
@@ -4,5 +4,5 @@
 extern  char     *input_file;
 extern  int       line_number;
 extern  int       start_line;
-extern  int       CPlusPlus;                        // C++ mode
+extern  int       CPlusPlus;                     
 extern  int       CParse_errors();
