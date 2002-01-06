6bde2912ec601c882b8f5aa1e270e37a8cc56067
Richard Palmer <richard@magicality.org>
2002-01-06 19:53:28 +0000

    Renamed example.php file.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@2158 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/php4/pointer/main.php
@@ -0,0 +1,41 @@
+<?
+
+	# First create some objects using the pointer library.
+
+	print "Testing the pointer library\n";
+
+	$a = ptrcreate("int", 37);
+	$b = ptrcreate("int", 42);
+	$c = ptrcreate("int");
+
+	print "	a = $a\n";
+	print "	b = $b\n";
+	print "	c = $x\n";
+
+	# Call the add() function wuth some pointers
+	add($a,$b,$c);
+
+	# Now get the result
+	$r = ptrvalue($c);
+
+	print "	37 + 42 = $r\n";
+
+	# Clean up the pointers
+	ptrfree($a);
+	ptrfree($b);
+	ptrfree($c);
+
+	# Now try the typemap library
+	# This should be much easier. Now how it is no longer
+	# necessary to manufacture pointers.
+
+	print "Trying the typemap library\n";
+	$r = sub(37,42);
+	print "	37 - 42 = $r\n";
+
+	# Now try the version with multiple return values
+	# print "Testing multiple return values\n";
+	# ($q,$r) = divide(42,37);
+	# print "	42/37 = $q remainder $r\n";
+
+?>
