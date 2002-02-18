2c8f33a83d3036ef0a165725591ed3a5c4f1f33b
William S Fulton <wsf@fultondesigns.co.uk>
2002-02-18 21:05:07 +0000

    no message
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@2399 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Source/Include/swigver.h
+++ SWIG/Source/Include/swigver.h
@@ -2,9 +2,17 @@
 /* SWIG version information */
 
 #ifndef SWIG_VERSION
-#define SWIG_VERSION "1.3.10u-20011129-2251"
+#define SWIG_VERSION "1.3.11u-20020218-2007"
+#endif
+
+#ifndef SWIG_MAJOR_VERSION
+#define SWIG_MAJOR_VERSION 1
+#endif
+
+#ifndef SWIG_MINOR_VERSION
+#define SWIG_MINOR_VERSION 3
 #endif
 
 #ifndef SWIG_SPIN
-#define SWIG_SPIN "10"
+#define SWIG_SPIN 11
 #endif
