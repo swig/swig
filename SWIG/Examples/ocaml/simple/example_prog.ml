965d115f0452bee2a2addb2216a22c759f263b88
Art Yerkes <ayerkes@speakeasy.net>
2002-06-28 05:46:20 +0000

    Ocaml example (borrowed from python/simple and modified for ocaml.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3028 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/ocaml/simple/example_prog.ml
@@ -0,0 +1,30 @@
+(* example_prog.ml *)
+
+open Example
+
+(* Call our gcd() function *)
+
+let x = 42
+let y = 105
+let g = gcd(x,y)
+let _ = Printf.printf "The gcd of %d and %d is %d\n" x y g
+
+(* Manipulate the Foo global variable *)
+
+(* Output its current value *)
+let _ = Printf.printf "Foo = %f\n" (foo_get ())
+
+(* Change its value *)
+let _ = foo_set 3.1415926
+
+(* See if the change took effect *)
+let _ = Printf.printf "Foo = %f\n" (foo_get ())
+
+
+
+
+
+
+
+
+
