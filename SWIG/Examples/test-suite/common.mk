dfae320d92473ff03d2fb78523d7eba6889aed1a
Dave Beazley <dave-swig@dabeaz.com>
2002-07-09 20:11:51 +0000

    new test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3100 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/common.mk
+++ SWIG/Examples/test-suite/common.mk
@@ -43,6 +43,7 @@ DYNAMIC_LIB_PATH = $(RUNTIMEDIR):.
 
 # C++ test cases. (Can be run individually using make testcase.cpptest.)
 CPP_TEST_CASES += \
+	default_ns \
 	template_ns_enum2 \
 	overload_template \
 	overload_copy \
---
+++ SWIG/Examples/test-suite/default_ns.i
@@ -0,0 +1,14 @@
+%module default_ns
+%inline %{
+namespace AType 
+{ 
+   enum AType 
+   { 
+      NoType 
+   }; 
+} 
+
+void dummy(AType::AType aType = AType::NoType);
+%}
+
+ 
