808d863a4c54ebb03a0c85783e7b01692ef46cd2
Dave Beazley <dave-swig@dabeaz.com>
2002-07-30 18:46:57 +0000

    new test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3307 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/common.mk
+++ SWIG/Examples/test-suite/common.mk
@@ -43,6 +43,7 @@ DYNAMIC_LIB_PATH = $(RUNTIMEDIR):.
 
 # C++ test cases. (Can be run individually using make testcase.cpptest.)
 CPP_TEST_CASES += \
+	constructor_explicit \
 	lib_typemaps \
 	typemap_namespace \
 	smart_pointer_const \
