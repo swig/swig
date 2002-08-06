15beb9bf422a78d88c2739c327655a4692e24252
Dave Beazley <dave-swig@dabeaz.com>
2002-08-06 15:58:17 +0000

    new test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3397 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/common.mk
+++ SWIG/Examples/test-suite/common.mk
@@ -43,6 +43,7 @@ DYNAMIC_LIB_PATH = $(RUNTIMEDIR):.
 
 # C++ test cases. (Can be run individually using make testcase.cpptest.)
 CPP_TEST_CASES += \
+	valuewrapper_base \
 	smart_pointer_rename \
 	grouping \
 	enum_scope_template \
