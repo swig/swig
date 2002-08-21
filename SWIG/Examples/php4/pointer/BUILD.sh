ce654fead473548186a264f82e148b305e777544
Sam Liddicott <sam@liddicott.com>
2002-08-21 12:00:10 +0000

    Fixed examples that use example.c insead of example.cxx All examples now build and use the new php build system
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3610 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/php4/pointer/BUILD.sh
+++ SWIG/Examples/php4/pointer/BUILD.sh
@@ -1,4 +1,4 @@
 #! /bin/sh -e
 
-swig -php4 -phpfull -c++ -noproxy -withcxx example.cxx example.i
+swig -php4 -phpfull -c++ -noproxy -withc example.c example.i
 phpize && ./configure && make clean && make
