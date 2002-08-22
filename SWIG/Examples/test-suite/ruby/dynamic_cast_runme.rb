5d4f50d70bd3be66fa73c24cfc60631c42525183
Logan Johnson <ljohnson@users.sourceforge.net>
2002-08-22 17:44:01 +0000

    *** empty log message ***
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3630 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/dynamic_cast_runme.rb
@@ -0,0 +1,13 @@
+require 'dynamic_cast'
+
+f = Dynamic_cast::Foo.new
+b = Dynamic_cast::Bar.new
+
+x = f.blah
+y = b.blah
+
+a = Dynamic_cast.do_test(y)
+if a != "Bar::test"
+  puts "Failed!!"
+end
+    
