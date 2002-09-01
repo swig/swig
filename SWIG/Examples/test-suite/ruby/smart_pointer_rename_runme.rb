eba49ae8b3ecb4072230aca4f390a7312ff287ae
Logan Johnson <ljohnson@users.sourceforge.net>
2002-09-01 14:51:22 +0000

    Added smart pointer test case run test scripts for Ruby.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3699 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/smart_pointer_rename_runme.rb
@@ -0,0 +1,12 @@
+require 'smart_pointer_rename'
+
+include Smart_pointer_rename
+
+f = Foo.new
+b = Bar.new(f)
+
+raise RuntimeError if b.test() != 3
+
+raise RuntimeError if b.ftest1(1) != 1
+
+raise RuntimeError if b.ftest2(2,3) != 2
