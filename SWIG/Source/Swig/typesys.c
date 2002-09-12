fa7a42612aee6ac347dfb37afb7ba7589a9e0f44
Dave Beazley <dave-swig@dabeaz.com>
2002-09-12 02:08:09 +0000

    Cleanup.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3864 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Source/Swig/typesys.c
+++ SWIG/Source/Swig/typesys.c
@@ -509,7 +509,6 @@ SwigType *SwigType_typedef_resolve(SwigType *t) {
 
   resolved_scope = 0;
 
-
   if (!typedef_resolve_cache) {
     typedef_resolve_cache = NewHash();
   }
@@ -722,25 +721,22 @@ SwigType *SwigType_typedef_resolve_all(SwigType *t) {
   SwigType *n;
   SwigType *r;
 
-  /*  if (!typedef_all_cache) {
+  if (!typedef_all_cache) {
     typedef_all_cache = NewHash();
   }
   r = Getattr(typedef_all_cache,t);
   if (r) {
     return Copy(r);
   }
-  */
   r = NewString(t);
   while ((n = SwigType_typedef_resolve(r))) {
     Delete(r);
     r = n;
   }
-  /*
   {
     SwigType *rr = Copy(r);
     Setattr(typedef_all_cache,NewString(t),rr);
   }
-  */
   return r;
 }
 
@@ -752,8 +748,6 @@ SwigType *SwigType_typedef_resolve_all(SwigType *t) {
  * typedef scope rules.
  * ----------------------------------------------------------------------------- */
 
-#if 1
-
 SwigType *SwigType_typedef_qualified(SwigType *t) 
 {
   List   *elements;
@@ -761,7 +755,6 @@ SwigType *SwigType_typedef_qualified(SwigType *t)
   int     i,len;
 
  if (!typedef_qualified_cache) typedef_qualified_cache = NewHash();
-
   result = Getattr(typedef_qualified_cache,t);
   if (result) {
     String *rc = Copy(result);
@@ -914,181 +907,6 @@ SwigType *SwigType_typedef_qualified(SwigType *t)
   Setattr(typedef_qualified_cache,NewString(t),NewString(result));
   return result;
 }
-
-#endif
-
-#if 0
-
-/* -----------------------------------------------------------------------------
- * SwigType_typedef_qualified()
- *
- * Given a type declaration, this function tries to fully qualify it according to
- * typedef scope rules.
- * ----------------------------------------------------------------------------- */
-
-SwigType *SwigType_typedef_qualified(SwigType *t) 
-{
-  List   *elements;
-  String *result;
-  int     i,len;
-
-  if (!typedef_qualified_cache) typedef_qualified_cache = NewHash();
-
-  result = Getattr(typedef_qualified_cache,t);
-  if (result) {
-    Printf(stdout,"t = '%s' --> '%s'\n", t, result);
-    return Copy(result);
-  }
-
-  result = NewString("");
-  elements = SwigType_split(t);
-  len = Len(elements);
-  for (i = 0; i < len; i++) {
-    String *e = Getitem(elements,i);
-    if (SwigType_issimple(e)) {
-      if (!SwigType_istemplate(e)) {
-	String *isenum = 0;
-	if (SwigType_isenum(e)) {
-	  isenum = e;
-	  e = NewString(Char(e)+5);
-	}
-	resolved_scope = 0;
-	if (typedef_resolve(current_scope,e)) {
-	  /* resolved_scope contains the scope that actually resolved the symbol */
-	  String *qname = Getattr(resolved_scope,"qname");
-	  if (qname) {
-	    Insert(e,0,"::");
-	    Insert(e,0,qname);
-	    if (isenum) {
-	      Clear(isenum);
-	      Printf(isenum, "enum %s", e);
-	      Delete(e);
-	    }
-	  }
-	} else {
-	  if (Swig_scopename_check(e)) {
-	    String *tqname;
-	    String *qlast;
-	    String *qname = Swig_scopename_prefix(e);
-	    if (qname) {
-	      qlast = Swig_scopename_last(e);
-	      tqname = SwigType_typedef_qualified(qname);
-	      Clear(e);
-	      Printf(e,"%s::%s", tqname, qlast);
-	      Delete(qname);
-	      Delete(qlast);
-	      Delete(tqname);
-	    }
-	    /* Automatic template instantiation might go here??? */
-
-	  }
-	}
-	if (isenum) e = isenum;
-
-      } else {
-	/* Template.  We need to qualify template parameters as well as the template itself */
-	String *tprefix, *qprefix;
-	String *tsuffix;
-	Parm   *p;
-	List *parms = SwigType_parmlist(e);
-	tprefix = SwigType_templateprefix(e);
-	tsuffix = SwigType_templatesuffix(e);
-	qprefix = SwigType_typedef_qualified(tprefix);
-	Printf(qprefix,"<(");
-	p = Firstitem(parms);
-	while (p) {
-	  String *qt = SwigType_typedef_qualified(p);
-	  if ((Strcmp(qt,p) == 0)) { /*  && (!Swig_scopename_check(qt))) { */
-	    /* No change in value.  It is entirely possible that the parameter is an integer value.
-	       If there is a symbol table associated with this scope, we're going to check for this */
-
-	    if (current_symtab) {
-	      Node *lastnode = 0;
-	      String *value = Copy(p);
-	      while (1) {
-		Node *n = Swig_symbol_clookup(value,current_symtab);
-		if (n == lastnode) break;
-		lastnode = n;
-		if (n) {
-		  if (Strcmp(nodeType(n),"enumitem") == 0) {
-		    /* An enum item.   Generate a fully qualified name */
-		    String *qn = Swig_symbol_qualified(n);
-		    if (Len(qn)) {
-		      Append(qn,"::");
-		      Append(qn,Getattr(n,"name"));
-		      Delete(value);
-		      value = qn;
-		      continue;
-		    } else {
-		      break;
-		    }
-		  } else if ((Strcmp(nodeType(n),"cdecl") == 0) && (Getattr(n,"value"))) {
-		    Delete(value);
-		    value = Copy(Getattr(n,"value"));
-		    continue;
-		  }
-		}
-		break;
-	      }
-	      Append(qprefix,value);
-	    } else {
-	      Append(qprefix,p);
-	    }
-	  } else {
-	    Append(qprefix,qt);
-	  }
-	  Delete(qt);
-	  p= Nextitem(parms);
-	  if (p) {
-	    Append(qprefix,",");
-	  }
-	}
-	Append(qprefix,")>");
-	Append(qprefix,tsuffix);
-	Delete(tsuffix);
-	Clear(e);
-	Append(e,qprefix);
-	Delete(tprefix);
-	Delete(qprefix);
-      }
-      if (Strncmp(e,"::",2) == 0) {
-	Delitem(e,0);
-	Delitem(e,0);
-      }
-      Append(result,e);
-    } else if (SwigType_isfunction(e)) {
-      List *parms = SwigType_parmlist(e);
-      String *s = NewString("f(");
-      String *p;
-      p = Firstitem(parms);
-      while (p) {
-	Append(s,SwigType_typedef_qualified(p));
-	p = Nextitem(parms);
-	if (p) {
-	  Append(s,",");
-	}
-      }
-      Append(s,").");
-      Append(result,s);
-      Delete(s);
-    } else if (SwigType_isarray(e)) {
-      String *ndim;
-      String *dim = SwigType_parm(e);
-      ndim = Swig_symbol_string_qualify(dim,0);
-      Printf(result,"a(%s).",ndim);
-      Delete(dim);
-      Delete(ndim);
-    } else {
-      Append(result,e);
-    }
-  }
-  Delete(elements);
-  Printf(stdout,"result = '%s'\n", result);
-  Setattr(typedef_qualified_cache,NewString(t),Copy(result));
-  return result;
-}
-
-#endif
 
 /* ----------------------------------------------------------------------------- 
  * SwigType_istypedef()
