a1228b91983366c1843827f0c4b184e39f65c5b0
Art Yerkes <ayerkes@speakeasy.net>
2002-10-04 02:52:05 +0000

    Starting to add runme tests.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3960 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ocaml/unions_runme.ml
@@ -0,0 +1,20 @@
+(* Test the unions example... *)
+
+open Unions 
+
+let a = new_SmallStruct C_void
+let b = new_BigStruct C_void
+let c = new_UnionTest C_void
+let d = new_EmbeddedUnionTest C_void 
+
+let _ = (invoke a) "jill_set" (C_short 3)
+let _ = (invoke b) "jack_set" (C_char 'a')  (* Int conversion *)
+let _ = (invoke b) "smallstruct_set" a      (* Put a in b *)
+
+let _ = if get_int ((invoke a) "jill_get" C_void) != 3 then
+	raise (Failure "jill value is not preserved")
+let _ = if get_int ((invoke b) "jack_get" C_void) != (int_of_char 'a') then
+	raise (Failure "jack value is not preserved")
+let _ = if get_int ((invoke ((invoke b) "smallstruct_get" C_void))
+	"jill_get" C_void) != 3 then
+	raise (Failure "jill value is not embedded in bigstruct")
