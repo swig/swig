791b2bc4f57a41df4e45ecc5966e92c7576eb7cc
Art Yerkes <ayerkes@speakeasy.net>
2002-10-04 03:02:40 +0000

    Typename runme (idea stolen from ruby runme)
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3961 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/ocaml/unions_runme.ml
+++ SWIG/Examples/test-suite/ocaml/unions_runme.ml
@@ -10,6 +10,7 @@ let d = new_EmbeddedUnionTest C_void
 let _ = (invoke a) "jill_set" (C_short 3)
 let _ = (invoke b) "jack_set" (C_char 'a')  (* Int conversion *)
 let _ = (invoke b) "smallstruct_set" a      (* Put a in b *)
+let _ = (invoke c) "bs_set" b
 
 let _ = if get_int ((invoke a) "jill_get" C_void) != 3 then
 	raise (Failure "jill value is not preserved")
@@ -18,3 +19,9 @@ let _ = if get_int ((invoke b) "jack_get" C_void) != (int_of_char 'a') then
 let _ = if get_int ((invoke ((invoke b) "smallstruct_get" C_void))
 	"jill_get" C_void) != 3 then
 	raise (Failure "jill value is not embedded in bigstruct")
+let _ = if get_int ((invoke ((invoke c) "bs_get" C_void))
+	"jack_get" C_void) != (int_of_char 'a') then
+	raise (Failure "union set of bigstruct did not take")
+let _ = if get_int ((invoke ((invoke c) "ss_get" C_void))
+	"jill_get" C_void) != (int_of_char 'a') then
+	raise (Failure "corresponding union values are not the same")
