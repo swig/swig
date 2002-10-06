6e0b083f997ccbf875fb766ef1203434571b3987
Art Yerkes <ayerkes@speakeasy.net>
2002-10-06 16:59:25 +0000

    Fixed newobject.  Objects call their own destructors to delete themselves during finalization.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/rel-1-3@3965 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ocaml/sneaky1_runme.ml
@@ -0,0 +1,8 @@
+(* Stolen from ruby test cases *)
+
+open Sneaky1
+
+let x = Sneaky1._add (C_list [ C_int 3; C_int 4 ])
+let y = Sneaky1._sub (C_list [ C_int 3; C_int 4 ])
+let z = Sneaky1._mul (C_list [ C_int 3; C_int 4 ])
+let w = Sneaky1._divide (C_list [ C_int 3; C_int 4 ])
