790271faf75951c4510a05487639559ebc98888f
Dave Beazley <dave-swig@dabeaz.com>
2002-12-01 20:45:43 +0000

    *** empty log message ***
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@4143 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Mac/macswig.h
+++
@@ -1,7 +0,0 @@
-/* This file tells SWIG we are compiling the Mac version */
-
-#define MACSWIG
-#define NEED_ALLOC
-#define SWIG_LIB   ":Lib"
-#define SWIG_CC    "Metrowerks CodeWarrior"
-#define SWIG_LANG  TCL8
