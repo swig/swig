bcadf4f1fd7151a822170e682889b76df7c0fe99
Matthias Köppe <mkoeppe@mail.math.uni-magdeburg.de>
2002-12-13 16:45:24 +0000

    Fixed linkage
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4196 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/guile/casts_runme.scm
+++ Examples/test-suite/guile/casts_runme.scm
@@ -1,7 +1,7 @@
 ;; The SWIG modules have "passive" Linkage, i.e., they don't generate
 ;; Guile modules (namespaces) but simply put all the bindings into the
 ;; current module.  That's enough for such a simple test.
-(dynamic-call "scm_init_test_module" (dynamic-link "./libcasts.so"))
+(dynamic-call "scm_init_casts_module" (dynamic-link "./libcasts.so"))
 
 (define x (new-B))
 
