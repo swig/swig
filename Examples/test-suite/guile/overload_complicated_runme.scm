60e5f836ff2eb45e9645ce087da9c44064fc4022
Matthias Köppe <mkoeppe@mail.math.uni-magdeburg.de>
2002-12-13 17:23:19 +0000

    Minor fixes to Guile run tests
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4197 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/guile/overload_complicated_runme.scm
+++ Examples/test-suite/guile/overload_complicated_runme.scm
@@ -7,12 +7,15 @@
   `(if (not ,form)
        (error "Check failed: " ',form)))
 
+(define (=~ a b)
+  (< (abs (- a b)) 1e-8))
+
 ;; Check first method
-(check (= (foo 1 2 "bar" 4) 15))
+(check (=~ (foo 1 2 "bar" 4) 15))
 
 ;; Check second method
-(check (= (foo 1 2) 4811.4))
-(check (= (foo 1 2 3.2) 4797.2))
-(check (= (foo 1 2 3.2 #\Q) 4798.2))
+(check (=~ (foo 1 2) 4811.4))
+(check (=~ (foo 1 2 3.2) 4797.2))
+(check (=~ (foo 1 2 3.2 #\Q) 4798.2))
 
 (exit 0)
