031b7bddbf69ada92f8e7549ef283a6e93a3177e
William S Fulton <wsf@fultondesigns.co.uk>
2002-12-23 22:51:49 +0000

    Bootstrap changes sets up Automake and now also installs missing files for Autoconf.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4221 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,16 @@
 #! /bin/sh
-# Bootstrap the development environment
+
+# Bootstrap the development environment - add extra files needed to run configure. 
+# Note autoreconf should do what this file achieves, but it has a bug when working with automake!
+# The latest config.guess and config.sub should be copied into Tools/config and checked into CVS
+# when upgrading the autotools. Otherwise this script will ensure the latest is copied from 
+# your autotool installation.
 
 set -x
+test -d Tools/config || mkdir Tools/config
+aclocal -I Tools/config
 autoheader
+automake --add-missing --copy --force-missing
 autoconf
 (cd Tools; autoconf)
+
