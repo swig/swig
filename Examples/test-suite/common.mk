cc77dfe436f3ddb1a3fa0822a5f136606ec42123
William S Fulton <wsf@fultondesigns.co.uk>
2003-01-31 22:01:53 +0000

    New test case - passing arrays by reference (needs fixing)
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4249 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/common.mk
+++ Examples/test-suite/common.mk
@@ -43,6 +43,7 @@ DYNAMIC_LIB_PATH = $(RUNTIMEDIR):.
 # C++ test cases. (Can be run individually using make testcase.cpptest.)
 
 CPP_TEST_BROKEN = \
+	arrayref \
 	abstract_typedef \
 	namespace_nested \
 	template_default_arg \
