a087adeab5688044ee712a320f5a1c080b04f0a5
Marcelo Matus <mmatus@acms.arizona.edu>
2003-02-11 01:34:44 +0000

    Added broken example with cpp macro expansion
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4288 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/common.mk
+++ Examples/test-suite/common.mk
@@ -42,6 +42,7 @@ DYNAMIC_LIB_PATH = $(RUNTIMEDIR):.
 
 # Broken C++ test cases. (Can be run individually using make testcase.cpptest.)
 CPP_TEST_BROKEN += \
+	preproc_4 \
 	cast_operator \
 	arrayref \
 	abstract_typedef \
