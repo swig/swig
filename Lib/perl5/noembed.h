08c3b43b2405ec78bdbf28c63e4032ea35dac7da
Dave Beazley <dave-swig@dabeaz.com>
2003-02-14 18:04:21 +0000

    Added John's perl5 patches
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4310 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Lib/perl5/noembed.h
@@ -0,0 +1,51 @@
+#ifdef do_open
+  #undef do_open
+#endif
+#ifdef do_close
+  #undef do_close
+#endif
+#ifdef scalar
+  #undef scalar
+#endif
+#ifdef list
+  #undef list
+#endif
+#ifdef apply
+  #undef apply
+#endif
+#ifdef convert
+  #undef convert
+#endif
+#ifdef Error
+  #undef Error
+#endif
+#ifdef form
+  #undef form
+#endif
+#ifdef vform
+  #undef vform
+#endif
+#ifdef LABEL
+  #undef LABEL
+#endif
+#ifdef METHOD
+  #undef METHOD
+#endif
+#ifdef Move
+  #undef Move
+#endif
+#ifdef yylex
+  #undef yylex
+#endif
+#ifdef yyparse
+  #undef yyparse
+#endif
+#ifdef yyerror
+  #undef yyerror
+#endif
+#ifdef invert
+  #undef invert
+#endif
+#ifdef ref
+  #undef ref
+#endif
