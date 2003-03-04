719959886dc7263fcf6ed434962cef287d4b9c81
William S Fulton <wsf@fultondesigns.co.uk>
2003-03-04 22:49:38 +0000

    csharp examples
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4433 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/csharp/enum/runme.cs
@@ -0,0 +1,30 @@
+public class runme
+{
+    static void Main() 
+    {
+        // Print out the value of some enums
+        Console.WriteLine("*** color ***");
+        Console.WriteLine("    RED    = " + example.RED);
+        Console.WriteLine("    BLUE   = " + example.BLUE);
+        Console.WriteLine("    GREEN  = " + example.GREEN);
+
+        Console.WriteLine("\n*** Foo::speed ***");
+        Console.WriteLine("    Foo::IMPULSE   = " + Foo.IMPULSE);
+        Console.WriteLine("    Foo::WARP      = " + Foo.WARP);
+        Console.WriteLine("    Foo::LUDICROUS = " + Foo.LUDICROUS);
+
+        Console.WriteLine("\nTesting use of enums with functions\n");
+
+        example.enum_test(example.RED, Foo.IMPULSE);
+        example.enum_test(example.BLUE, Foo.WARP);
+        example.enum_test(example.GREEN, Foo.LUDICROUS);
+        example.enum_test(1234,5678);
+
+        Console.WriteLine( "\nTesting use of enum with class method" );
+        Foo f = new Foo();
+
+        f.enum_test(Foo.IMPULSE);
+        f.enum_test(Foo.WARP);
+        f.enum_test(Foo.LUDICROUS);
+    }
+}
---
+++ Examples/csharp/simple/runme.cs
@@ -0,0 +1,23 @@
+public class runme
+{
+    static void Main() 
+    {
+        // Call our gcd() function
+
+        int x = 42;
+        int y = 105;
+        int g = example.gcd(x,y);
+        Console.WriteLine("The gcd of " + x + " and " + y + " is " + g);
+
+        // Manipulate the Foo global variable
+
+        // Output its current value
+        Console.WriteLine("Foo = " + example.getFoo());
+
+        // Change its value
+        example.setFoo(3.1415926);
+
+        // See if the change took effect
+        Console.WriteLine("Foo = " + example.getFoo());
+    }
+}
