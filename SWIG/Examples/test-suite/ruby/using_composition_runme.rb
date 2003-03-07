87d2016a27ca7740510e4bcd8ccfc50ad9dde04e
Logan Johnson <ljohnson@users.sourceforge.net>
2003-03-07 17:21:58 +0000

    *** empty log message ***
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@4460 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/using_composition_runme.rb
@@ -0,0 +1,17 @@
+require 'using_composition'
+
+include Using_composition
+
+f = FooBar.new
+if f.blah(3) != 3
+  raise RuntimeError,"blah(int)"
+end
+
+if f.blah(3.5) != 3.5
+  raise RuntimeError,"blah(double)"
+end
+
+if f.blah("hello") != "hello"
+  raise RuntimeError,"blah(char *)"
+end
+
---
+++ SWIG/Examples/test-suite/ruby/using_inherit_runme.rb
@@ -0,0 +1,13 @@
+require 'using_inherit'
+
+include Using_inherit
+
+b = Bar.new
+if b.test(3) != 3
+  raise RuntimeError,"test(int)"
+end
+
+if b.test(3.5) != 3.5
+  raise RuntimeError, "test(double)"
+end
+
