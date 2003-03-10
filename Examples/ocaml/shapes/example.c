0749cef9e3546fcb07a0a6cc8d466da1cdd917c3
Art Yerkes <ayerkes@speakeasy.net>
2003-03-10 04:09:14 +0000

    Added overloading example.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4487 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/ocaml/shapes/example.c
@@ -0,0 +1,20 @@
+/* File : example.c */
+#include <stdio.h>
+#include "example.h"
+
+shape::shape() { }
+
+shape::~shape() { }
+
+bool shape::cover( double x, double y ) { return false; }
+
+void draw_shape_coverage( shape *s, int div_x, int div_y ) {
+    double i,j;
+
+    for( i = 0; i < 1.0; i += 1.0 / ((float)div_y) ) {
+	for( j = 0; j < 1.0; j += 1.0 / ((float)div_x) ) {
+	    if( s->cover( i,j ) ) putchar( 'x' ); else putchar( ' ' );
+	}
+	printf( "\n" );
+    }
+}
---
+++ Examples/ocaml/shapes/example.i
@@ -0,0 +1,8 @@
+/* File : example.i */
+%module(directors="1") example
+%{
+#include "example.h"
+%}
+
+%feature("director");
+%include "example.h"
