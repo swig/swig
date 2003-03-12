ae635218f2af5117bc9e5a20d81c1976ade906ce
Art Yerkes <ayerkes@speakeasy.net>
2003-03-12 15:25:27 +0000

    Added some tests to mimic the ones in the python directory.  Working my way through the alphabet.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@4519 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ocaml/varargs_runme.ml
@@ -0,0 +1,10 @@
+(* Test case stolen from the python directory *)
+
+open Varargs
+
+let _ = if _test (C_string "Hello") <> (C_string "Hello") then
+    raise (Failure "1")
+
+let f = new_Foo C_void
+let _ = if (invoke f) "test" (C_string "Hello") <> (C_string "Hello") then
+    raise (Failure "2")
