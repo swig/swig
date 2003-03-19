437e503be973533c40a280d7f7fd038da47a065c
William S Fulton <wsf@fultondesigns.co.uk>
2003-03-19 20:50:54 +0000

    more varargs runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4579 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/ruby/varargs_runme.rb
+++ Examples/test-suite/ruby/varargs_runme.rb
@@ -4,7 +4,11 @@ if Varargs.test("Hello") != "Hello"
   raise RuntimeError, "Failed"
 end
 
-f = Varargs::Foo.new
+f = Varargs::Foo.new("Greetings")
+if f.str != "Greetings"
+  raise RuntimeError, "Failed"
+end
+
 if f.test("Hello") != "Hello"
   raise RuntimeError, "Failed"
 end
