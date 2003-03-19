a91f8ad5e6141a8fdb1887439ea8c8567418f569
William S Fulton <wsf@fultondesigns.co.uk>
2003-03-19 20:50:05 +0000

    varargs runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@4578 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/java/varargs_runme.java
@@ -0,0 +1,28 @@
+// varargs test
+
+import varargs.*;
+
+public class varargs_runme {
+
+  static {
+    try {
+	System.loadLibrary("varargs");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" + e);
+      System.exit(1);
+    }
+  }
+
+  public static void main(String argv[]) {
+
+    if (!varargs.test("Hello").equals("Hello"))
+      throw new RuntimeException("Failed");
+
+    Foo f = new Foo("Greetings");
+    if (!f.getStr().equals("Greetings"))
+      throw new RuntimeException("Failed");
+        
+    if (!f.test("Hello").equals("Hello"))
+      throw new RuntimeException("Failed");
+  }
+}
