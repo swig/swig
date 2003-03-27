a2db8d0bb20108f64c20237274daa9a5473fd4ac
Art Yerkes <ayerkes@speakeasy.net>
2003-03-27 15:13:13 +0000

    Fixed runme for current code generation.  No _get or _set on method names for getting and setting struct or union members.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4635 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/ocaml/unions_runme.ml
+++ Examples/test-suite/ocaml/unions_runme.ml
@@ -7,21 +7,21 @@ let b = new_BigStruct C_void
 let c = new_UnionTest C_void
 let d = new_EmbeddedUnionTest C_void 
 
-let _ = (invoke a) "jill_set" (C_short 3)
-let _ = (invoke b) "jack_set" (C_char 'a')  (* Int conversion *)
-let _ = (invoke b) "smallstruct_set" a      (* Put a in b *)
-let _ = (invoke c) "bs_set" b
+let _ = (invoke a) "jill" (C_short 3)
+let _ = (invoke b) "jack" (C_char 'a')  (* Int conversion *)
+let _ = (invoke b) "smallstruct" a      (* Put a in b *)
+let _ = (invoke c) "bs" b
 
-let _ = if get_int ((invoke a) "jill_get" C_void) != 3 then
+let _ = if get_int ((invoke a) "jill" C_void) != 3 then
 	raise (Failure "jill value is not preserved")
-let _ = if get_int ((invoke b) "jack_get" C_void) != (int_of_char 'a') then
+let _ = if get_int ((invoke b) "jack" C_void) != (int_of_char 'a') then
 	raise (Failure "jack value is not preserved")
-let _ = if get_int ((invoke ((invoke b) "smallstruct_get" C_void))
-	"jill_get" C_void) != 3 then
+let _ = if get_int ((invoke ((invoke b) "smallstruct" C_void))
+	"jill" C_void) != 3 then
 	raise (Failure "jill value is not embedded in bigstruct")
-let _ = if get_int ((invoke ((invoke c) "bs_get" C_void))
-	"jack_get" C_void) != (int_of_char 'a') then
+let _ = if get_int ((invoke ((invoke c) "bs" C_void))
+	"jack" C_void) != (int_of_char 'a') then
 	raise (Failure "union set of bigstruct did not take")
-let _ = if get_int ((invoke ((invoke c) "ss_get" C_void))
-	"jill_get" C_void) != (int_of_char 'a') then
+let _ = if get_int ((invoke ((invoke c) "ss" C_void))
+	"jill" C_void) != (int_of_char 'a') then
 	raise (Failure "corresponding union values are not the same")
