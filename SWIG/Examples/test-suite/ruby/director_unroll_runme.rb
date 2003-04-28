ca6eae96a3392d31763b650a48fd1c7830f4278d
Logan Johnson <ljohnson@users.sourceforge.net>
2003-04-28 23:12:36 +0000

    *** empty log message ***
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@4735 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/director_unroll_runme.rb
@@ -0,0 +1,17 @@
+require 'director_unroll'
+
+class MyFoo < Director_unroll::Foo
+  def ping
+    "MyFoo::ping()"
+  end
+end
+
+a = MyFoo.new
+
+b = Director_unroll::Bar.new
+
+b.set(a)
+c = b.get()
+
+raise RuntimeError if a != c
+
