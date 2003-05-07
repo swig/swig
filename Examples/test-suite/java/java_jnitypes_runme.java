61f06a81f5a6a130c4ad33c2545525b0794dc851
William S Fulton <wsf@fultondesigns.co.uk>
2003-05-07 18:56:21 +0000

    Test fixes
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4771 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/java/java_jnitypes_runme.java
+++ Examples/test-suite/java/java_jnitypes_runme.java
@@ -19,8 +19,8 @@ public class java_jnitypes_runme {
 
     Test test = new Test();
     boolean booleanArray[] = new boolean[] {true, false};
-    char charArray[] = new char[] {10, 20};
-    byte byteArray[] = new byte[] {'A', 'B'};
+    char charArray[] = new char[] {'A', 'B'};
+    byte byteArray[] = new byte[] {10, 20};
     short shortArray[] = new short[] {10, 20};
     int intArray[] = new int[] {10, 20};
     long longArray[] = new long[] {10, 20};
@@ -29,18 +29,18 @@ public class java_jnitypes_runme {
     Test objectArray[] = new Test[] {new Test(), test};
 
     if (java_jnitypes.jnifunc(true) != true)                testFailed("jboolean");
-    if (java_jnitypes.jnifunc(100) != 100)                  testFailed("jchar");
-    if (java_jnitypes.jnifunc('A') != 'A')                  testFailed("jbyte");
-    if (java_jnitypes.jnifunc(100) != 100)                  testFailed("jshort");
+    if (java_jnitypes.jnifunc('A') != 'A')                  testFailed("jchar");
+    if (java_jnitypes.jnifunc((byte)100) != (byte)100)      testFailed("jbyte");
+    if (java_jnitypes.jnifunc((short)100) != (short)100)    testFailed("jshort");
     if (java_jnitypes.jnifunc(100) != 100)                  testFailed("jint");
-    if (java_jnitypes.jnifunc(100) != 100)                  testFailed("jlong");
-    if (java_jnitypes.jnifunc(100) != 100)                  testFailed("jfloat");
-    if (java_jnitypes.jnifunc(100.0) != 100)                testFailed("jdouble");
+    if (java_jnitypes.jnifunc((long)100) != (long)100)      testFailed("jlong");
+    if (java_jnitypes.jnifunc((float)100) != (float)100)    testFailed("jfloat");
+    if (java_jnitypes.jnifunc(100.0) != 100.0)              testFailed("jdouble");
     if (java_jnitypes.jnifunc("100") != "100")              testFailed("jstring");
     if (java_jnitypes.jnifunc(test) != test)                testFailed("jobject");
     if (java_jnitypes.jnifunc(booleanArray)[1] != false)    testFailed("jbooleanArray");
-    if (java_jnitypes.jnifunc(charArray)[1] != 20)          testFailed("jcharArray");
-    if (java_jnitypes.jnifunc(byteArray)[1] != 'B')         testFailed("jbyteArray");
+    if (java_jnitypes.jnifunc(charArray)[1] != 'B')         testFailed("jcharArray");
+    if (java_jnitypes.jnifunc(byteArray)[1] != 20)          testFailed("jbyteArray");
     if (java_jnitypes.jnifunc(shortArray)[1] != 20)         testFailed("jshortArray");
     if (java_jnitypes.jnifunc(intArray)[1] != 20)           testFailed("jintArray");
     if (java_jnitypes.jnifunc(longArray)[1] != 20)          testFailed("jlongArray");
