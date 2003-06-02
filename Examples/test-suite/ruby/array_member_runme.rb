bcdd12ce6930faa2133fbb1971a47c811b15276b
Logan Johnson <ljohnson@users.sourceforge.net>
2003-06-02 22:21:54 +0000

    *** empty log message ***
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@4851 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/ruby/array_member_runme.rb
@@ -0,0 +1,25 @@
+require 'array_member'
+
+include Array_member
+
+f = Foo.new
+f.data = Array_member.global_data
+
+0.upto(7) { |i|
+  unless get_value(f.data, i) == get_value(Array_member.global_data, i)
+    raise RuntimeError, "Bad array assignment"
+  end
+}
+
+0.upto(7) { |i|
+  set_value(f.data, i, -i)
+}
+
+Array_member.global_data = f.data
+
+0.upto(7) { |i|
+  unless get_value(f.data, i) == get_value(Array_member.global_data, i)
+    raise RuntimeError, "Bad array assignment"
+  end
+}
+
---
+++ Examples/test-suite/ruby/arrays_global_runme.rb
@@ -0,0 +1,4 @@
+require 'arrays_global'
+
+Arrays_global.array_i = Arrays_global.array_const_i
+
