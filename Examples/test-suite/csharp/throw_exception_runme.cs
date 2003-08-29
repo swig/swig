7ad2fa11dc3f9f807a1aa4d0ebe29539e37960a7
William S Fulton <wsf@fultondesigns.co.uk>
2003-08-29 21:07:12 +0000

    exception runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5023 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/csharp/throw_exception_runme.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class runme
+{
+    static void Main() {
+        Foo f = new Foo();
+        try {
+            f.test_int();
+            throw new Exception("Integer exception should have been thrown");
+        } catch (System.Exception) {
+        }
+        try {
+            f.test_msg();
+            throw new Exception("String exception should have been thrown");
+        } catch (System.Exception) {
+        }
+        try {
+            f.test_cls();
+            throw new Exception("Class exception should have been thrown");
+        } catch (System.Exception) {
+        }
+    }
+}
