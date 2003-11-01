4fc81e02493a9bdee58659b6be88ca633c35a5da
William S Fulton <wsf@fultondesigns.co.uk>
2003-11-01 00:50:00 +0000

    simple runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5225 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/csharp/imports_runme.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class runme
+{
+    static void Main() {
+        B b = new B();
+        b.hello(); //call member function in A which is in a different SWIG generated library.
+        b.bye();
+    }
+}
