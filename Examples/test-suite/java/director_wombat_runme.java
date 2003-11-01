57b8d0ea70607f0d1edb2b43489964e5004cc5c2
William S Fulton <wsf@fultondesigns.co.uk>
2003-11-01 20:06:09 +0000

    Enhanced to check typedef'd parameters in directors - Scott Michel contribution.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5238 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/java/director_wombat_runme.java
+++ Examples/test-suite/java/director_wombat_runme.java
@@ -19,25 +19,27 @@ public class director_wombat_runme
 
     a = b.meth();
     if ((retval = a.meth(49)) != 49) {
-      System.out.println("Test failed: retval = " + retval + ", expected 49");
-      System.exit(1);
+      throw new RuntimeException ("Test failed: retval = " + retval + ", expected 49");
     }
 
     a.delete();
 
     a = new Foo_integers_derived();
     if ((retval = a.meth(62)) != 62 + 2) {
-      System.out.println("Test failed: retval = " + retval + ", expected 62 + 2");
-      System.exit(1);
+      throw new RuntimeException ("Test failed: retval = " + retval + ", expected 62 + 2");
     }
 
     a.delete();
 
     a = new Foo_integers_derived_2();
     if ((retval = a.meth(37)) != 37) {
-      System.out.println("Test failed: retval = " + retval + ", expected 37");
-      System.exit(1);
+      throw new RuntimeException ("Test failed: retval = " + retval + ", expected 37");
     }
+
+    b.delete();
+
+    b = new Bar_derived_1();
+    b.foo_meth(a, 0);
   }
 }
 
@@ -59,5 +61,20 @@ class Foo_integers_derived_2 extends Foo_integers
   public Foo_integers_derived_2()
   {
     super();
+  }
+}
+
+class Bar_derived_1 extends Bar
+{
+  public Bar_derived_1()
+  {
+    super();
+  }
+
+  public void foo_meth(Foo_integers foo_obj, int param)
+  {
+    if (!(foo_obj instanceof Foo_integers_derived_2)) {
+      throw new RuntimeException ("Test failed: foo_obj is not Foo_integers_derived_2, got " + foo_obj);
+    }
   }
 }
