ae4042799578561c09a0008ef508d263fd7bf68b
Dave Beazley <dave-swig@dabeaz.com>
2003-11-05 21:38:02 +0000

    new example
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5268 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/tcl/contract/example.tcl
@@ -0,0 +1,23 @@
+# file: example.tcl
+# Try to load as a dynamic module.
+
+catch { load ./example.so example}
+catch { load ./example.dll example}    ;# Windows
+
+# Call our gcd() function
+set x 42
+set y 105
+set g [gcd $x $y]
+puts "The gcd of $x and $y is $g"
+
+# Manipulate the Foo global variable
+
+# Output its current value
+puts "Foo = $Foo"
+
+# Change its value
+set Foo 3.1415926
+
+# See if the change took effect
+puts "Foo = $Foo"
+
