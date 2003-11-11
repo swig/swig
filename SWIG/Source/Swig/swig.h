97c4e444371edda767176bb1ab9f425b2ab3953c
Dave Beazley <dave-swig@dabeaz.com>
2003-11-11 20:16:35 +0000

    Added Swig_push_directory() and Swig_pop_directory()
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5297 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Source/Swig/swig.h
+++ SWIG/Source/Swig/swig.h
@@ -84,6 +84,8 @@ typedef  DOH     SwigType;
 /* --- File interface --- */
 
 extern void     Swig_add_directory(const String_or_char *dirname);
+extern void     Swig_push_directory(const String_or_char *dirname);
+extern void     Swig_pop_directory();
 extern String  *Swig_last_file();
 extern List    *Swig_search_path();
 extern FILE    *Swig_open(const String_or_char *name);
