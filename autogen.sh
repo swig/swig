814eeca4497a30e5c01793b7658641bd391ed922
Dave Beazley <dave-swig@dabeaz.com>
2003-11-11 18:51:43 +0000

    Use glibtoolize on Mac OSX
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5293 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- autogen.sh
+++ autogen.sh
@@ -10,6 +10,14 @@ set -x
 test -d Tools/config || mkdir Tools/config
 aclocal -I Tools/config
 autoheader
-libtoolize --force --copy
+
+# Note: on Mac OS X there is a different program 'libtoolize' that is definitely not
+# what we want to run.  Instead, we want to use 'glibtoolize'
+
+if test -x /usr/bin/glibtoolize; then
+   glibtoolize --force --copy
+else
+   libtoolize --force --copy
+fi
 automake --add-missing --copy --force-missing
 autoconf
