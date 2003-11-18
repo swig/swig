164b304971e1027523436886f4565c0de80183f2
William S Fulton <wsf@fultondesigns.co.uk>
2003-11-18 22:38:34 +0000

    Contract assert macro modification to handle void and non-void return types
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5339 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/java.cxx
+++ Source/Modules/java.cxx
@@ -974,6 +974,9 @@ class JAVA : public Language {
     /* Substitute the cleanup code */
     Replaceall(f->code,"$cleanup",cleanup);
 
+    /* Contract macro modification */
+    Replaceall(f->code, "SWIG_contract_assert(", "SWIG_contract_assert($null, ");
+
     if(!is_void_return)
       Replaceall(f->code,"$null","0");
     else
