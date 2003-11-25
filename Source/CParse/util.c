e7c45edd391a65352fb505327301c3453139b4eb
Dave Beazley <dave-swig@dabeaz.com>
2003-11-25 18:26:03 +0000

    Fixed [ 836903 ] C++ inconsistency (with void arguments).
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5398 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/CParse/util.c
+++ Source/CParse/util.c
@@ -69,3 +69,21 @@ void Swig_cparse_replace_descriptor(String *s) {
   }
 }
 
+/* -----------------------------------------------------------------------------
+ * cparse_normalize_void()
+ *
+ * This function is used to replace arguments of the form (void) with empty
+ * arguments in C++
+ * ----------------------------------------------------------------------------- */
+
+void cparse_normalize_void(Node *n) {
+  String *decl = Getattr(n,"decl");
+  Parm   *parms = Getattr(n,"parms");
+
+  if (SwigType_isfunction(decl)) {
+    if ((ParmList_len(parms) == 1) && (SwigType_type(Getattr(parms,"type")) == T_VOID)) {
+      Replaceall(decl,"f(void).","f().");
+      Delattr(n,"parms");
+    }
+  }
+}
