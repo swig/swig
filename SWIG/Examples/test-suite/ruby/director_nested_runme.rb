6b6b041b64368ebc22a313091c673ff666e573f7
Marcelo Matus <mmatus@acms.arizona.edu>
2003-12-09 10:38:34 +0000

    emitting and testing protected methods for director protected members
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5512 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/ruby/director_nested_runme.rb
+++ SWIG/Examples/test-suite/ruby/director_nested_runme.rb
@@ -1,32 +1,41 @@
 require 'director_nested'
 
+NoProtectedError = Kernel.const_defined?("NoMethodError") ? NoMethodError : NameError
+
 class A < Director_nested::FooBar_int
-  def do_step
-    "A::do_step;"
-  end
+  protected
+    def do_step
+      "A::do_step;"
+    end
   
-  def get_value
-    "A::get_value"
-  end
+    def get_value
+      "A::get_value"
+    end
 end
 
 a = A.new
+
+begin 
+  a.do_advance
+rescue NoProtectedError
+end
 
 raise RuntimeError if a.step != "Bar::step;Foo::advance;Bar::do_advance;A::do_step;"
 
 
 class B < Director_nested::FooBar_int
-  def do_advance
-    "B::do_advance;" + do_step
-  end
+  protected
+    def do_advance
+      "B::do_advance;" + do_step
+    end
 
-  def do_step
-    "B::do_step;"
-  end
+    def do_step
+      "B::do_step;"
+    end
   
-  def get_value
-    "B::get_value"
-  end
+    def get_value
+      "B::get_value"
+    end
 end
 
 
