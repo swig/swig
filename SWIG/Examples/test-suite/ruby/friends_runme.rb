8158b1f2eccd130f0e30e3b79c245885bb8354a2
Marcelo Matus <mmatus@acms.arizona.edu>
2003-12-20 07:46:27 +0000

    These is the current tests for the friend function support.
    They will give you and idea of what is currently working.
    
    If you have another case you think we need to test, please
    send me an email to add it and trying it here.
    
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5581 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/friends_runme.rb
@@ -0,0 +1,8 @@
+require 'friends'
+
+
+a = Friends::A.new(2)
+
+raise RuntimeError if Friends::get_val1(a) != 2
+raise RuntimeError if Friends::get_val2(a) != 4
+raise RuntimeError if Friends::get_val3(a) != 6
