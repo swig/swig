f9e55f5ec845490564c07ace33618495ff087887
William S Fulton <wsf@fultondesigns.co.uk>
2003-12-23 21:51:46 +0000

    C# runtime test, based on Java one.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5593 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/csharp/virtual_poly_runme.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class runme {
+    static void Main() {
+
+        NDouble d = new NDouble(3.5);
+        NInt i = new NInt(2);
+
+        //
+        // These two natural 'copy' forms fail because no covariant (polymorphic) return types 
+        // are supported in C#. 
+        //
+        // NDouble dc = d.copy();
+        // NInt ic = i.copy();
+
+        //
+        // Unlike C++, we have to downcast instead.
+        //
+        NDouble dc = (NDouble)d.copy();
+        NInt ic = (NInt)i.copy();
+
+        NDouble ddc = (NDouble)NDouble.narrow(dc);
+        NInt dic = (NInt)NInt.narrow(ic);
+
+        virtual_poly.incr(ic);
+        if ( (i.get() + 1) != ic.get() )
+            throw new Exception("incr test failed");
+  }
+}
