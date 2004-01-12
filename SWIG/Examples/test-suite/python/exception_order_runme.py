b32024f2ab2673de890bc29de2f8884adf6d4a01
Marcelo Matus <mmatus@acms.arizona.edu>
2004-01-12 23:40:58 +0000

    fix precatching exception behaviour to postcatching
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5617 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/exception_order_runme.py
+++ SWIG/Examples/test-suite/python/exception_order_runme.py
@@ -6,9 +6,21 @@ a = exception_order.A()
 try:
   a.foo()
 except RuntimeError,e:
-  if e.args[0] != "E":
+  if e.args[0] != "E1":
     print "bad exception order", 
     raise RuntimeError, e.args
 
+try:
+  a.bar()
+except RuntimeError,e:
+  if e.args[0] != "E2":
+    print "bad exception order", 
+    raise RuntimeError, e.args
 
-  
+try:
+  a.foobar()
+except RuntimeError,e:
+  if e.args[0] != "postcatch unknown":
+    print "bad exception order", 
+    raise RuntimeError, e.args
+
