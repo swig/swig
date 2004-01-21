9cfb9a490b4db2e739ca7d8312f2f451649923eb
Marcelo Matus <mmatus@acms.arizona.edu>
2004-01-21 00:46:08 +0000

    fixes for the nodirector feature and the '0' director member case
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@5657 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/java/frob_runme.java
@@ -0,0 +1,24 @@
+
+import frob.*;
+import java.lang.reflect.*;
+
+public class frob_runme
+{
+  static {
+    try {
+      System.loadLibrary("frob");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" + e);
+      System.exit(1);
+    }
+  }
+
+  public static void main(String args[])
+  {
+    Bravo foo = new Bravo();
+
+    String s = foo.abs_method();
+    if (!s.equals("Bravo::abs_method()"))
+        throw new RuntimeException( "error" );
+  }
+}
