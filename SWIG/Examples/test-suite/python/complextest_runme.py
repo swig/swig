359d862a495825ba08eb541c094f7c3dc088f7ea
Marcelo Matus <mmatus@acms.arizona.edu>
2004-03-17 09:49:17 +0000

    add vector test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5758 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/complextest_runme.py
+++ SWIG/Examples/test-suite/python/complextest_runme.py
@@ -4,3 +4,11 @@ a = complex(1,2)
 
 if complextest.Conj(a) != a.conjugate():
   raise RuntimeError, "bad complex mapping"
+
+
+v = (complex(1,2), complex(2,3), complex(4,3), 1)
+
+try:
+  print complextest.Copy_h(v)
+except:
+  pass
