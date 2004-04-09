74eb5b7dc324f7f185c1e229e81c294ee8bfb8e9
Marcelo Matus <mmatus@acms.arizona.edu>
2004-04-09 21:56:14 +0000

    add inplace test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5857 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/python/inplaceadd_runme.py
@@ -0,0 +1,18 @@
+import inplaceadd
+a = inplaceadd.A(7)
+
+a += 5
+if a.val != 12:
+  raise RuntimeError
+
+if a.thisown != 1:
+  raise RuntimeError  
+
+a -= 5
+if a.val != 7:
+  raise RuntimeError
+
+a *= 2
+
+if a.val != 14:
+  raise RuntimeError
