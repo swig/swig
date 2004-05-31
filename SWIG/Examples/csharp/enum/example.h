4192a5025c643fabfc3a023468cd383c08518606
William S Fulton <wsf@fultondesigns.co.uk>
2004-05-31 07:13:12 +0000

    Update for new enum wrapping which uses proper C# enums
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@5952 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/csharp/enum/example.h
+++ SWIG/Examples/csharp/enum/example.h
@@ -5,7 +5,7 @@ enum color { RED, BLUE, GREEN };
 class Foo {
  public:
   Foo() { }
-  enum speed { IMPULSE, WARP, LUDICROUS };
+  enum speed { IMPULSE=10, WARP=20, LUDICROUS=30 };
   void enum_test(speed s);
 };
 
--- SWIG/Examples/csharp/enum/runme.cs
+++ SWIG/Examples/csharp/enum/runme.cs
@@ -6,27 +6,26 @@ public class runme
     {
         // Print out the value of some enums
         Console.WriteLine("*** color ***");
-        Console.WriteLine("    RED    = " + example.RED);
-        Console.WriteLine("    BLUE   = " + example.BLUE);
-        Console.WriteLine("    GREEN  = " + example.GREEN);
+        Console.WriteLine("    " + color.RED + " = " + (int)color.RED);
+        Console.WriteLine("    " + color.BLUE + " = " + (int)color.BLUE);
+        Console.WriteLine("    " + color.GREEN + " = " + (int)color.GREEN);
 
         Console.WriteLine("\n*** Foo::speed ***");
-        Console.WriteLine("    Foo::IMPULSE   = " + Foo.IMPULSE);
-        Console.WriteLine("    Foo::WARP      = " + Foo.WARP);
-        Console.WriteLine("    Foo::LUDICROUS = " + Foo.LUDICROUS);
+        Console.WriteLine("    Foo::" + Foo.speed.IMPULSE + " = " + (int)Foo.speed.IMPULSE);
+        Console.WriteLine("    Foo::" + Foo.speed.WARP + " = " + (int)Foo.speed.WARP);
+        Console.WriteLine("    Foo::" + Foo.speed.LUDICROUS + " = " + (int)Foo.speed.LUDICROUS);
 
         Console.WriteLine("\nTesting use of enums with functions\n");
 
-        example.enum_test(example.RED, Foo.IMPULSE);
-        example.enum_test(example.BLUE, Foo.WARP);
-        example.enum_test(example.GREEN, Foo.LUDICROUS);
-        example.enum_test(1234,5678);
+        example.enum_test(color.RED, Foo.speed.IMPULSE);
+        example.enum_test(color.BLUE, Foo.speed.WARP);
+        example.enum_test(color.GREEN, Foo.speed.LUDICROUS);
 
         Console.WriteLine( "\nTesting use of enum with class method" );
         Foo f = new Foo();
 
-        f.enum_test(Foo.IMPULSE);
-        f.enum_test(Foo.WARP);
-        f.enum_test(Foo.LUDICROUS);
+        f.enum_test(Foo.speed.IMPULSE);
+        f.enum_test(Foo.speed.WARP);
+        f.enum_test(Foo.speed.LUDICROUS);
     }
 }
