3d26007c84931164cb036261a7e6ea0960311c38
William S Fulton <wsf@fultondesigns.co.uk>
2004-07-10 16:21:06 +0000

    Libtool removed Runtime library build removed
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6019 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- autogen.sh
+++ autogen.sh
@@ -10,14 +10,5 @@ set -x
 test -d Tools/config || mkdir Tools/config
 aclocal -I Tools/config
 autoheader
-
-# Note: on Mac OS X there is a different program 'libtoolize' that is definitely not
-# what we want to run.  Instead, we want to use 'glibtoolize'
-
-if test -x /usr/bin/glibtoolize; then
-   glibtoolize --force --copy
-else
-   libtoolize --force --copy
-fi
 automake --add-missing --copy --force-missing
 autoconf
