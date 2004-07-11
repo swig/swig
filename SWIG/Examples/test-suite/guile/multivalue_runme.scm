fccfc058497412d0801fdbb33c12ccdcd566e38f
Matthias Köppe <mkoeppe@mail.math.uni-magdeburg.de>
2004-07-11 17:09:13 +0000

    Fix name of module init function.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6027 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/guile/multivalue_runme.scm
+++ SWIG/Examples/test-suite/guile/multivalue_runme.scm
@@ -3,5 +3,5 @@
 ;; The SWIG modules have "passive" Linkage, i.e., they don't generate
 ;; Guile modules (namespaces) but simply put all the bindings into the
 ;; current module.  That's enough for such a simple test.
-(dynamic-call "scm_init_test_module" (dynamic-link "./libmultivalue.so"))
+(dynamic-call "scm_init_multivalue_module" (dynamic-link "./libmultivalue.so"))
 (load "../schemerunme/multivalue.scm")
