9fea8cb365829951e8974e61a2e64b6adf1852b0
Marcelo Matus <mmatus@acms.arizona.edu>
2004-09-01 09:59:32 +0000

    fix for directors
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6201 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/python/imports_runme.py
+++ Examples/test-suite/python/imports_runme.py
@@ -1,9 +1,9 @@
 # This is the import runtime testcase.
 
-import _imports_a
-import _imports_b
+import imports_a
+import imports_b
 import sys
 
-x = _imports_b.new_B()
-_imports_a.A_hello(x)
+x = imports_b.B()
+imports_a.A.hello(x)
 
