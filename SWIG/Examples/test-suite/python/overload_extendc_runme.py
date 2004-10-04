73215c55582fafd781656364f6832c23b4f3983c
William S Fulton <wsf@fultondesigns.co.uk>
2004-10-04 20:05:59 +0000

    default arguments added to overloaded methods Fix so that the test uses the correct module!
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6303 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/overload_extendc_runme.py
+++ SWIG/Examples/test-suite/python/overload_extendc_runme.py
@@ -1,11 +1,16 @@
-import overload_extend
+import overload_extendc
 
-f = overload_extend.Foo()
+f = overload_extendc.Foo()
 if f.test(3) != 1:
     raise RuntimeError
 if f.test("hello") != 2:
     raise RuntimeError
 if f.test(3.5,2.5) != 3:
     raise RuntimeError
+if f.test("hello",20) != 1020:
+    raise RuntimeError
+if f.test("hello",20,100) != 120:
+    raise RuntimeError
+
 
 
