7757e311392623ae55ffb5b3cb8750c3ab4982bd
Marcelo Matus <mmatus@acms.arizona.edu>
2004-10-04 03:18:57 +0000

    fix %extend+overload error
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6298 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/extend_placement_runme.py
+++ SWIG/Examples/test-suite/python/extend_placement_runme.py
@@ -1,10 +1,36 @@
 import extend_placement
-import types
 
+foo = extend_placement.Foo()
 foo = extend_placement.Foo(1)
-print type(foo)
-print foo.spam()
-print foo.spam("hello")
+foo.spam()
+foo.spam("hello")
+foo.spam(1)
+foo.spam(1,1)
+foo.spam(1,1,1)
 
-foo = extend_placement.Foo(1)
-print type(foo)
+
+bar = extend_placement.Bar()
+bar = extend_placement.Bar(1)
+bar.spam()
+bar.spam("hello")
+bar.spam(1)
+bar.spam(1,1)
+bar.spam(1,1,1)
+
+
+foo = extend_placement.FooTi()
+foo = extend_placement.FooTi(1)
+foo.spam()
+foo.spam("hello")
+foo.spam(1)
+foo.spam(1,1)
+foo.spam(1,1,1)
+
+
+bar = extend_placement.BarTi()
+bar = extend_placement.BarTi(1)
+bar.spam()
+bar.spam("hello")
+bar.spam(1)
+bar.spam(1,1)
+bar.spam(1,1,1)
