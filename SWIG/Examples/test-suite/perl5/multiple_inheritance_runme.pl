88c0b425e2f796f367cdc9e0432c369f65554377
William S Fulton <wsf@fultondesigns.co.uk>
2004-10-04 20:04:22 +0000

    minor runtime test mods
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6302 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/perl5/multiple_inheritance_runme.pl
+++ SWIG/Examples/test-suite/perl5/multiple_inheritance_runme.pl
@@ -3,16 +3,13 @@ use multiple_inheritance;
 
 $fooBar = new multiple_inheritance::FooBar();
 if ($fooBar->foo() != 2) {
-  print "Runtime test1 failed\n";
-  exit 1;
+  die "Runtime test1 failed\n";
 }
 
 if ($fooBar->bar() != 1) {
-  print "Runtime test2 failed\n";
-  exit 1;
+  die "Runtime test2 failed\n";
 }
 
 if ($fooBar->fooBar() != 3) {
-  print "Runtime test3 failed\n";
-  exit 1;
+  die "Runtime test3 failed\n";
 }
