c187e14eb53b44784362ec122b0018e6ff74cebd
William S Fulton <wsf@fultondesigns.co.uk>
2004-10-16 20:49:46 +0000

    default args added to runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6397 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/ruby/using_private_runme.rb
+++ SWIG/Examples/test-suite/ruby/using_private_runme.rb
@@ -9,3 +9,11 @@ if f.blah(4) != 4
   raise RuntimeError, "blah(int)"
 end
 
+if f.defaulted() != -1
+  raise RuntimeError, "defaulted()"
+end
+
+if f.defaulted(222) != 222
+  raise RuntimeError, "defaulted(hi)"
+end
+
