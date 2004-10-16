53446db5bd959f47d6b5cf5f782c6875d5287cac
William S Fulton <wsf@fultondesigns.co.uk>
2004-10-16 21:04:28 +0000

    fixes for changes in the test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6404 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/ruby/overload_extend_runme.rb
+++ SWIG/Examples/test-suite/ruby/overload_extend_runme.rb
@@ -4,5 +4,5 @@ f = Overload_extend::Foo.new
 
 raise RuntimeError if f.test(3) != 1
 raise RuntimeError if f.test("hello") != 2
-raise RuntimeError if f.test(3.5,2.5) != 3
+raise RuntimeError if f.test(3.5,2.5) != 6
 
