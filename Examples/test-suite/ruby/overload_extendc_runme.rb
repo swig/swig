6cdc20d8e625ce80c6dc6d4cc1a1764dd44fc32d
William S Fulton <wsf@fultondesigns.co.uk>
2004-10-16 21:04:28 +0000

    fixes for changes in the test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6404 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/ruby/overload_extendc_runme.rb
+++ Examples/test-suite/ruby/overload_extendc_runme.rb
@@ -4,4 +4,4 @@ f = Overload_extend::Foo.new
 
 raise RuntimeError if f.test(3) != 1
 raise RuntimeError if f.test("hello") != 2
-raise RuntimeError if f.test(3.5,2.5) != 3
+raise RuntimeError if f.test(3.5,2.5) != 6
