1800d7914f386839be1c031a7f3e2d0b11824ac4
William S Fulton <wsf@fultondesigns.co.uk>
2004-10-20 21:06:10 +0000

    default args in C code test added
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6454 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/python/overload_extendc_runme.py
+++ Examples/test-suite/python/overload_extendc_runme.py
@@ -12,5 +12,11 @@ if f.test("hello",20) != 1020:
 if f.test("hello",20,100) != 120:
     raise RuntimeError
 
-
+# C default args
+if f.test(f) != 30:
+    raise RuntimeError
+if f.test(f,100) != 120:
+    raise RuntimeError
+if f.test(f,100,200) != 300:
+    raise RuntimeError
 
