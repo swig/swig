628916c7945d92739fa6972e187311e1718f86c2
Marcelo Matus <mmatus@acms.arizona.edu>
2004-10-21 22:23:23 +0000

    add silly abstract case
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6476 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/director_abstract_runme.py
+++ SWIG/Examples/test-suite/python/director_abstract_runme.py
@@ -1,6 +1,8 @@
 import director_abstract
 
 class MyFoo(director_abstract.Foo):
+	def __init__(self):
+		director_abstract.Foo.__init__(self)
 	def ping(self):
 		return "MyFoo::ping()"
 
@@ -13,4 +15,16 @@ if a.ping() != "MyFoo::ping()":
 if a.pong() != "Foo::pong();MyFoo::ping()":
 	raise RuntimeError, a.pong()
 
+
+class MyExample(director_abstract.Example1):
+	def __init__(self):
+		director_abstract.Example1.__init__(self)
+		pass
+	
+	def Color(self, r, g, b):
+		return r
+
+me = MyExample()
+
+print me.Color(1,2,3)
 
