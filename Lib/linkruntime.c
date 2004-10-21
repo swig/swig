0c57969c61e37911bcf6145d04cdcf2b5e3a96e6
John Lenz <jlenz2@math.uiuc.edu>
2004-10-21 21:53:54 +0000

    - Add SWIG_RUNTIME_VERSION to new type sharing code.  This is for future-proof, if   the format of swig_type_info ever changes, this number should be incremented - Add SWIG_LINK_RUNTIME and SWIG_STATIC_RUNTIME symbols to python - Convert inline into SWIGINLINE
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6475 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Lib/linkruntime.c
@@ -0,0 +1,24 @@
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#  if defined(_MSC_VER) || defined(__GNUC__)
+#    if defined(STATIC_LINKED)
+#      define SWIGEXPORT(a) a
+#    else
+#      define SWIGEXPORT(a) __declspec(dllexport) a
+#    endif
+#  else
+#    if defined(__BORLANDC__)
+#      define SWIGEXPORT(a) a _export
+#    else
+#      define SWIGEXPORT(a) a
+#    endif
+#  endif
+#else
+#  define SWIGEXPORT(a) a
+#endif
+
+static void *ptr = 0;
+SWIGEXPORT(void *)
+SWIG_ReturnGlobalTypeList(void *t) {
+ if (!ptr) ptr = t;
+ return ptr;
+}
