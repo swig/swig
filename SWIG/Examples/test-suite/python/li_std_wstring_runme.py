743f258ba3064f5bc2bf4d67733b5d1d3e24fb1a
William S Fulton <wsf@fultondesigns.co.uk>
2004-11-01 23:35:08 +0000

    lib_xxx renamed to li_xxx
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6599 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/li_std_wstring_runme.py
+++ SWIG/Examples/test-suite/python/li_std_wstring_runme.py
@@ -1,26 +1,26 @@
-import lib_std_wstring
+import li_std_wstring
 
 x=u"h"
 
-if lib_std_wstring.test_wcvalue(x) != x:
+if li_std_wstring.test_wcvalue(x) != x:
   raise RuntimeError, "bad string mapping"
 
 x=u"hello"
-if lib_std_wstring.test_ccvalue(x) != x:
+if li_std_wstring.test_ccvalue(x) != x:
   raise RuntimeError, "bad string mapping"
 
-if lib_std_wstring.test_cvalue(x) != x:
+if li_std_wstring.test_cvalue(x) != x:
   raise RuntimeError, "bad string mapping"
 
-if lib_std_wstring.test_value(x) != x:
-  print x, lib_std_wstring.test_value(x)
+if li_std_wstring.test_value(x) != x:
+  print x, li_std_wstring.test_value(x)
   raise RuntimeError, "bad string mapping"
 
-if lib_std_wstring.test_const_reference(x) != x:
+if li_std_wstring.test_const_reference(x) != x:
   raise RuntimeError, "bad string mapping"
 
 
-s = lib_std_wstring.wstring(u"he")
+s = li_std_wstring.wstring(u"he")
 s += u"llo"
 
 if s != x:
@@ -30,21 +30,21 @@ if s != x:
 if s[1:4] != x[1:4]:
   raise RuntimeError, "bad string mapping"
 
-if lib_std_wstring.test_value(s) != x:
+if li_std_wstring.test_value(s) != x:
   raise RuntimeError, "bad string mapping"
 
-if lib_std_wstring.test_const_reference(s) != x:
+if li_std_wstring.test_const_reference(s) != x:
   raise RuntimeError, "bad string mapping"
 
-a = lib_std_wstring.A(s)
+a = li_std_wstring.A(s)
 
-if lib_std_wstring.test_value(a) != x:
+if li_std_wstring.test_value(a) != x:
   raise RuntimeError, "bad string mapping"
 
-if lib_std_wstring.test_const_reference(a) != x:
+if li_std_wstring.test_const_reference(a) != x:
   raise RuntimeError, "bad string mapping"
 
-b = lib_std_wstring.wstring(" world")
+b = li_std_wstring.wstring(" world")
 
 if a + b != "hello world":
   raise RuntimeError, "bad string mapping"
@@ -61,14 +61,14 @@ if c.find_last_of("l") != 9:
   
 s = "hello world"
 
-b = lib_std_wstring.B("hi")
+b = li_std_wstring.B("hi")
 
-b.name = lib_std_wstring.wstring(u"hello")
+b.name = li_std_wstring.wstring(u"hello")
 if b.name != "hello":
   raise RuntimeError, "bad string mapping"
 
 
-b.a = lib_std_wstring.A("hello")
+b.a = li_std_wstring.A("hello")
 if b.a != u"hello":
   raise RuntimeError, "bad string mapping"
 
