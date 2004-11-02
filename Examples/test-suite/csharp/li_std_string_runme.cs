d174b1b231d3f31a91085d58fc830ec30b5fed24
William S Fulton <wsf@fultondesigns.co.uk>
2004-11-02 22:31:12 +0000

    lib_xxx to li_xxx name change fixes
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6633 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/csharp/li_std_string_runme.cs
+++ Examples/test-suite/csharp/li_std_string_runme.cs
@@ -1,34 +1,34 @@
 using System;
-using lib_std_stringNamespace;
+using li_std_stringNamespace;
 
 public class runme
 {
     static void Main() 
     {
         // Checking expected use of %typemap(in) std::string {}
-        lib_std_string.test_value("Fee");
+        li_std_string.test_value("Fee");
 
         // Checking expected result of %typemap(out) std::string {}
-        if (lib_std_string.test_value("Fi") != "Fi")
+        if (li_std_string.test_value("Fi") != "Fi")
             throw new Exception("Test 1 failed");
 
         // Verify type-checking for %typemap(in) std::string {}
         try {
-            lib_std_string.test_value(null);
+            li_std_string.test_value(null);
             throw new Exception("Test 2 failed");
         } catch (NullReferenceException) {
         }
 
         // Checking expected use of %typemap(in) const std::string & {}
-        lib_std_string.test_const_reference("Fo");
+        li_std_string.test_const_reference("Fo");
 
         // Checking expected result of %typemap(out) const std::string& {}
-        if (lib_std_string.test_const_reference("Fum") != "Fum")
+        if (li_std_string.test_const_reference("Fum") != "Fum")
             throw new Exception("Test 3 failed");
 
         // Verify type-checking for %typemap(in) const std::string & {}
         try {
-            lib_std_string.test_const_reference(null);
+            li_std_string.test_const_reference(null);
             throw new Exception("Test 4 failed");
         } catch (NullReferenceException) {
         }
@@ -41,17 +41,17 @@ public class runme
         
         SWIGTYPE_p_std__string stringPtr = null;
         
-        stringPtr = lib_std_string.test_pointer_out();
+        stringPtr = li_std_string.test_pointer_out();
 
-        lib_std_string.test_pointer(stringPtr);
+        li_std_string.test_pointer(stringPtr);
 
-        stringPtr = lib_std_string.test_const_pointer_out();
+        stringPtr = li_std_string.test_const_pointer_out();
 
-        lib_std_string.test_const_pointer(stringPtr);
+        li_std_string.test_const_pointer(stringPtr);
 
-        stringPtr = lib_std_string.test_reference_out();
+        stringPtr = li_std_string.test_reference_out();
 
-        lib_std_string.test_reference(stringPtr);
+        li_std_string.test_reference(stringPtr);
 
     }
 }
--- Examples/test-suite/php4/li_carrays_runme.php4
+++ Examples/test-suite/php4/li_carrays_runme.php4
@@ -2,7 +2,7 @@
 // Sample test file
 
 require "tests.php4";
-require "lib_carrays.php";
+require "li_carrays.php";
 
 // No new functions
 check::functions(array(new_intarray,delete_intarray,intarray_getitem,intarray_setitem));
