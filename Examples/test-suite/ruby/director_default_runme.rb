41a0b2dd59beaae366d6614d6b6f98d7c0d0b761
Marcelo Matus <mmatus@acms.arizona.edu>
2004-11-08 05:31:27 +0000

    fix def args + director + constructors
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6689 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/ruby/director_default_runme.rb
@@ -0,0 +1,19 @@
+require 'director_default'
+
+
+
+a = Director_default::Foo.new 1
+a = Director_default::Foo.new 
+
+a.Msg 
+a.Msg "hello"
+a.GetMsg
+a.GetMsg "hello"
+
+a = Director_default::Bar.new 1
+a = Director_default::Bar.new 
+
+a.Msg 
+a.Msg "hello"
+a.GetMsg
+a.GetMsg "hello"
