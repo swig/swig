3903b13479540e33b65441ef21572f4c83ba6837
William S Fulton <wsf@fultondesigns.co.uk>
2004-11-09 21:24:57 +0000

    *** empty log message ***
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6704 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/java/default_args_runme.java
+++ SWIG/Examples/test-suite/java/default_args_runme.java
@@ -63,6 +63,9 @@ public class default_args_runme {
     Foo foo = new Foo();
     foo.newname(); 
     foo.newname(10); 
+    foo.renamed3arg(10, 10.0); 
+    foo.renamed2arg(10); 
+    foo.renamed1arg(); 
      
     // exception specifications
     try {
