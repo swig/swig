5d36275309b84a9b47f687cea7aa6f1a250a98d1
William S Fulton <wsf@fultondesigns.co.uk>
2004-11-30 20:08:38 +0000

    const enum reference runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6795 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/java/director_enum_runme.java
+++ SWIG/Examples/test-suite/java/director_enum_runme.java
@@ -19,11 +19,17 @@ public class director_enum_runme {
       if (a.ping(Hallo.awright) != Hallo.yo) {
           throw new RuntimeException ( "a.ping()" );
       }
+      if (a.ping_ref(Hallo.awright) != Hallo.hello) {
+          throw new RuntimeException ( "a.ping_ref()" );
+      }
 
       Foo b = new Foo();
 
       if (b.ping(Hallo.awright) != Hallo.awright) {
           throw new RuntimeException ( "b.ping()" );
+      }
+      if (b.ping_ref(Hallo.awright) != Hallo.awright) {
+          throw new RuntimeException ( "b.ping_ref()" );
       }
   }
 }
@@ -31,6 +37,9 @@ public class director_enum_runme {
 class MyFoo extends Foo {
     public Hallo say_hi(Hallo h) {
         return Hallo.yo;
+    }
+    public Hallo say_hi_ref(Hallo h) {
+        return Hallo.hello;
     }
 }
 
