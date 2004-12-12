eef4efb2eb44d40598866b0e5e29a2b85dbdf580
Marcelo Matus <mmatus@acms.arizona.edu>
2004-12-12 21:40:18 +0000

    add test case using 'super'
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@6864 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/director_constructor_runme.rb
@@ -0,0 +1,26 @@
+require 'director_constructor'
+
+include Director_constructor
+
+class Test < Foo
+  def initialize(i)
+    super(i)
+  end
+
+  def doubleit()
+    self.a = (self.a * 2)
+  end
+
+  def test
+    3
+  end
+end
+
+a = Test.new(5) #dies here
+
+raise RuntimeError if a.getit != 5
+raise RuntimeError if a.do_test != 3
+
+a.doubleit
+raise RuntimeError if a.getit != 10
+
