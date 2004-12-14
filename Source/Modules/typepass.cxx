25d65e6ce9232bb9e2e4382bffe59171d8e27ac7
Marcelo Matus <mmatus@acms.arizona.edu>
2004-12-14 06:47:19 +0000

    fix warning for forward base class
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@6876 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/typepass.cxx
+++ Source/Modules/typepass.cxx
@@ -180,7 +180,8 @@ class TypePass : private Dispatcher {
 		if (Strcmp(nodeType(bcls),"classforward") != 0) {
 		  Swig_error(Getfile(bname),Getline(bname),"'%s' is not a class. \n",bname);
 		} else {
-		  Swig_warning(WARN_TYPE_INCOMPLETE,Getfile(bname),Getline(bname),"Base class '%s' is incomplete.\n", bname);
+		  Swig_warning(WARN_TYPE_INCOMPLETE,Getfile(cls),Getline(cls),"Base class '%s' is incomplete.\n", bname);
+		  Swig_warning(WARN_TYPE_INCOMPLETE,Getfile(bcls),Getline(bcls),"Only forward declaration '%s' was found.\n", bname);
 		  clsforward = 1;
 		}
 		bcls = 0;
