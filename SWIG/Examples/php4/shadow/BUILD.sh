663862918dafca6c88a50b5e97b2e870e1c41889
Kevin Ruland <kruland@sunflower.com>
2005-09-15 15:50:50 +0000

    Removed the -xmlout from swig.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@7447 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/php4/shadow/BUILD.sh
+++ SWIG/Examples/php4/shadow/BUILD.sh
@@ -1,5 +1,5 @@
 #! /bin/sh -e
 
-${SWIG:=swig} -xmlout e.xml -php4 -make -c++ -withcxx example.cxx example.i
+${SWIG:=swig} -php4 -make -c++ -withcxx example.cxx example.i
 make
 php -d extension_dir=. runme.php4
