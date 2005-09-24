56fd0791639ab0fa48b6711d59e36d224a3f5552
Charlie Savage <cfis@interserv.com>
2005-09-24 22:30:30 +0000

    Added test case for Ruby using track objects and directors at the same time.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@7524 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/ruby/track_objects_directors_runme.rb
@@ -0,0 +1,29 @@
+require 'test/unit'
+require 'track_objects_directors'
+	
+class MyFoo < Track_objects_directors::Foo
+  def ping
+    "MyFoo::ping()"
+  end
+end
+
+a = MyFoo.new
+
+raise RuntimeError if a.ping != "MyFoo::ping()"
+raise RuntimeError if a.pong != "Foo::pong();MyFoo::ping()"
+
+b = Track_objects_directors::Foo.new
+
+raise RuntimeError if b.ping != "Foo::ping()"
+raise RuntimeError if b.pong != "Foo::pong();Foo::ping()"
+
+container = Track_objects_directors::Container.new
+foo = MyFoo.new
+container.set_foo(foo)
+    
+myFoo1 = container.get_foo
+myFoo2 = container.get_foo
+
+if not myFoo1.equal?(myFoo2)
+  raise RuntimeError, "MyFoo's should be the same"
+end
