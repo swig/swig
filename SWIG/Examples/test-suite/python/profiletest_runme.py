b630cd6340224b8b5c1f34cae6567273f077d60f
Marcelo Matus <mmatus@acms.arizona.edu>
2005-11-03 10:25:24 +0000

    full fn call
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@7782 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/profiletest_runme.py
+++ SWIG/Examples/test-suite/python/profiletest_runme.py
@@ -1,7 +1,6 @@
-import _profiletest
-#import profiletest
+import profiletest       
 
-a = _profiletest.new_A()
-b = _profiletest.new_B()
-for i in range(0,10000000):
-  a = _profiletest.B_fn(b, a)
+a = profiletest.A()
+b = profiletest.B()
+for i in range(0,1000000):
+  a = b.fn(a)
