6cac9f0a1ffb5f0e76a0e330b48c8889c8a82e29
Marcelo Matus <mmatus@acms.arizona.edu>
2005-11-05 16:07:39 +0000

    better equivalent test, doesn't use range
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@7815 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/python/profiletest_runme.py
+++ Examples/test-suite/python/profiletest_runme.py
@@ -2,5 +2,26 @@ import profiletest
 
 a = profiletest.A()
 b = profiletest.B()
-for i in range(0,1000000):
-  a = b.fn(a)
+i = 50000
+while i:
+  a = b.fn(a) #1
+  a = b.fn(a) #2
+  a = b.fn(a) #3
+  a = b.fn(a) #4
+  a = b.fn(a) #5
+  a = b.fn(a) #6
+  a = b.fn(a) #7
+  a = b.fn(a) #8
+  a = b.fn(a) #9
+  a = b.fn(a) #10
+  a = b.fn(a) #1
+  a = b.fn(a) #2
+  a = b.fn(a) #3
+  a = b.fn(a) #4
+  a = b.fn(a) #5
+  a = b.fn(a) #6
+  a = b.fn(a) #7
+  a = b.fn(a) #8
+  a = b.fn(a) #9
+  a = b.fn(a) #20
+  i -= 1
