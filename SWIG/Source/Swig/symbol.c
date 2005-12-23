c26263a2f433e959de5d1391a3640b3cedfb1402
Marcelo Matus <mmatus@acms.arizona.edu>
2005-12-23 10:12:15 +0000

    normalize operation, first reduce, then qualify
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@8055 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Source/Swig/symbol.c
+++ SWIG/Source/Swig/symbol.c
@@ -1058,8 +1058,8 @@ Swig_symbol_clookup(String_or_char *name, Symtab *n) {
   }
   if (!s) {
     if (SwigType_istemplate(name)) {
-      SwigType *qt = Swig_symbol_type_qualify(name,hsym);
-      SwigType *rt = Swig_symbol_template_reduce(qt, hsym);
+      SwigType *qt = Swig_symbol_template_reduce(name, hsym);
+      SwigType *rt = Swig_symbol_type_qualify(qt,hsym);
       if (!Equal(rt,name)) {
 	s = Swig_symbol_clookup(rt, hsym);
       }
