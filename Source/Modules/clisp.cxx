ac66843e9e8907910e873b23cb7168bf40b822c2
Surendra Singhi <efuzzyone@netscape.net>
2005-12-24 23:16:23 +0000

    fixed the copyright information.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@8063 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/clisp.cxx
+++ Source/Modules/clisp.cxx
@@ -5,7 +5,6 @@
  *
  * Author(s) : Surendra Singhi (surendra@asu.edu)
  *
- * Copyright (C) 2005 Surendra Singhi
  * See the file LICENSE for information on usage and redistribution.
  * ----------------------------------------------------------------------------- */
 
