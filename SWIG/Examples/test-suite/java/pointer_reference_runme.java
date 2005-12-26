33a81636bee77d06999efc501286c6fb39631492
William S Fulton <wsf@fultondesigns.co.uk>
2005-12-26 23:13:27 +0000

    pointer reference runtime test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@8066 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ SWIG/Examples/test-suite/java/pointer_reference_runme.java
@@ -0,0 +1,23 @@
+import pointer_reference.*;
+
+public class pointer_reference_runme {
+
+  static {
+    try {
+        System.loadLibrary("pointer_reference");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" + e);
+      System.exit(1);
+    }
+  }
+
+  public static void main(String argv[]) throws Throwable
+  {
+    Struct s  = pointer_reference.get();
+    if (s.getValue() != 10) throw new RuntimeException("get test failed");
+
+    Struct ss = new Struct(20);
+    pointer_reference.set(ss);
+    if (Struct.getInstance().getValue() != 20) throw new RuntimeException("set test failed");
+  }
+}
