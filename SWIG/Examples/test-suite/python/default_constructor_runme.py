e71d976af6a9b8ec2adccda2348213ee1a7f44f6
Marcelo Matus <mmatus@acms.arizona.edu>
2006-01-02 17:48:05 +0000

    update for -copyctor option
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@8192 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/python/default_constructor_runme.py
+++ SWIG/Examples/test-suite/python/default_constructor_runme.py
@@ -2,73 +2,78 @@ import _default_constructor
 
 dc = _default_constructor
 
-a = dc.new_A
-del_a = dc.delete_A
+a = dc.new_A()
+dc.delete_A(a)
 
-aa = dc.new_AA
-del_aa = dc.delete_AA
+aa = dc.new_AA()
+dc.delete_AA(aa)
 
-b = dc.new_B
+try:
+    b = dc.new_B()
+    print "Whoa. new_BB created."
+except:
+    pass
+
 del_b = dc.delete_B
 
 try:
-    bb = dc.new_BB;
+    bb = dc.new_BB();
     print "Whoa. new_BB created."
-except AttributeError:
+except:
     pass
 
 del_bb = dc.delete_BB
 
 try:
-    c = dc.new_C
+    c = dc.new_C()
     print "Whoa. new_C created."
-except AttributeError:
+except:
     pass
 
 del_c = dc.delete_C
 
-cc = dc.new_CC
-del_cc = dc.delete_CC
+cc = dc.new_CC()
+dc.delete_CC(cc)
 
 try:
-    d = dc.new_D;
+    d = dc.new_D();
     print "Whoa. new_D created"
-except AttributeError:
+except:
     pass
 
 del_d = dc.delete_D
 
 try:
-    dd = dc.new_DD
+    dd = dc.new_DD()
     print "Whoa. new_DD created"
-except AttributeError:
+except:
     pass
 
 dd = dc.delete_DD
 
 try:
-    ad = dc.new_AD
+    ad = dc.new_AD()
     print "Whoa. new_AD created"
-except AttributeError:
+except:
     pass
 
 del_ad = dc.delete_AD
 
-e = dc.new_E
-del_e = dc.delete_E
+e = dc.new_E()
+dc.delete_E(e)
 
-ee = dc.new_EE
-del_ee = dc.delete_EE
+ee = dc.new_EE()
+dc.delete_EE(ee)
 
 try:
-    eb = dc.new_EB
+    eb = dc.new_EB()
     print "Whoa. new_EB created"
-except AttributeError:
+except:
     pass
 
 del_eb = dc.delete_EB
 
-f = dc.new_F
+f = dc.new_F()
 
 try:
     del_f = dc.delete_F
@@ -76,14 +81,14 @@ try:
 except AttributeError:
     pass
 
-ff = dc.new_FFF
+ff = dc.new_FFF()
 try:
     del_ff = dc.delete_FFF
     print "Whoa. delete_FFF created"
 except AttributeError:
     pass
 
-g = dc.new_G
+g = dc.new_G()
 
 try:
     del_g = dc.delete_G
@@ -91,8 +96,8 @@ try:
 except AttributeError:
     pass
 
-gg = dc.new_GG
-del_gg = dc.delete_GG
+gg = dc.new_GG()
+dc.delete_GG(gg)
 
 
 import default_constructor
