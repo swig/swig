d59d8638c95bc53c24659736626266afd935d6fb
William S Fulton <wsf@fultondesigns.co.uk>
2006-01-07 01:06:30 +0000

    add in complete increment/decrement operator checks
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@8270 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/csharp/operator_overload_runme.cs
+++ Examples/test-suite/csharp/operator_overload_runme.cs
@@ -6,11 +6,31 @@ public class runme
 {
   static void Main() 
   {
-    Op op = new Op(100);
-    op++;
-    if (op.i != 101) throw new Exception("operator++ failed");
-    op--;
-    if (op.i != 100) throw new Exception("operator-- failed");
+    Op.sanity_check();
+    {
+      Op op = new Op(100);
+      Op opNew = op++;
+      if (op.i != 101) throw new Exception("operator++ postfix failed (op)");
+      if (opNew.i != 100) throw new Exception("operator++ postfix failed (opNew)");
+    }
+    {
+      Op op = new Op(100);
+      Op opNew = op--;
+      if (op.i != 99) throw new Exception("operator-- postfix failed (op)");
+      if (opNew.i != 100) throw new Exception("operator-- postfix failed (opNew)");
+    }
+    {
+      Op op = new Op(100);
+      Op opNew = ++op;
+      if (op.i != 101) throw new Exception("operator++ prefix failed (op)");
+      if (opNew.i != 101) throw new Exception("operator++ prefix failed (opNew)");
+    }
+    {
+      Op op = new Op(100);
+      Op opNew = --op;
+      if (op.i != 99) throw new Exception("operator-- prefix failed (op)");
+      if (opNew.i != 99) throw new Exception("operator-- prefix failed (opNew)");
+    }
   }
 }
 
