7a417324a58c15d41d48ba0c29da9f295f131203
Marcelo Matus <mmatus@acms.arizona.edu>
2006-02-04 00:02:45 +0000

    add test for 'void *' argument
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@8705 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/perl5/voidtest_runme.pl
+++ SWIG/Examples/test-suite/perl5/voidtest_runme.pl
@@ -1,7 +1,9 @@
 use voidtest;
 
-$f = new voidtest::Foo();
-$v1 = voidtest::vfunc1($f);
-$v2 = voidtest::vfunc2($f);
-$v3 = voidtest::vfunc3($v1);
-$v4 = voidtest::vfunc4($f);
+if (1) {
+    $f = new voidtest::Foo();
+    $v1 = voidtest::vfunc1($f);
+    $v2 = voidtest::vfunc2($f);
+    $v3 = voidtest::vfunc3($v1);
+    $v4 = voidtest::vfunc4($f);
+}
