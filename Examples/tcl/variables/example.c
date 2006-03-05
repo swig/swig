c772286a5de85a5b7bad8d6b6e0f5c065d153891
William S Fulton <wsf@fultondesigns.co.uk>
2006-03-05 00:38:54 +0000

    remove sprintf deprecated message
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@8959 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/tcl/variables/example.c
+++ Examples/tcl/variables/example.c
@@ -2,6 +2,11 @@
 
 /* I'm a file containing some C global variables */
 
+/* Deal with Microsoft's attempt at deprecating C standard runtime functions */
+#if !defined(SWIG_NO_CRT_SECURE_NO_DEPRECATE) && defined(_MSC_VER)
+# define _CRT_SECURE_NO_DEPRECATE
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "example.h"
