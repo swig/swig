3e7b5d75090ec484d0e670cc03fb69f03e20646b
William S Fulton <wsf@fultondesigns.co.uk>
2006-03-07 23:05:11 +0000

    fix for perl keyword
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@8990 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/test-suite/ocaml/sneaky1_runme.ml
+++ SWIG/Examples/test-suite/ocaml/sneaky1_runme.ml
@@ -4,6 +4,6 @@ open Swig
 open Sneaky1
 
 let x = Sneaky1._add (C_list [ C_int 3; C_int 4 ])
-let y = Sneaky1._sub (C_list [ C_int 3; C_int 4 ])
+let y = Sneaky1._subtract (C_list [ C_int 3; C_int 4 ])
 let z = Sneaky1._mul (C_list [ C_int 3; C_int 4 ])
 let w = Sneaky1._divide (C_list [ C_int 3; C_int 4 ])
