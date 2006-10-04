e66bd43f1ce3a1c0dd645c707f88296d65a4dc98
Olly Betts <olly@survex.com>
2006-10-04 12:43:35 +0000

    Remove some superfluous semicolons from after function definitions.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@9405 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- SWIG/Examples/php4/class/example.h
+++ SWIG/Examples/php4/class/example.h
@@ -7,8 +7,8 @@ public:
   }
   virtual ~Shape() {
     nshapes--;
-  };
-  double  x, y;   
+  }
+  double  x, y;
   void    move(double dx, double dy);
   virtual double area(void) = 0;
   virtual double perimeter(void) = 0;
@@ -20,8 +20,8 @@ class Circle : public Shape {
 private:
   double radius;
 public:
-  Circle(double r) : radius(r) { };
-  ~Circle() { };
+  Circle(double r) : radius(r) { }
+  ~Circle() { }
   void set_radius( double r );
   virtual double area(void);
   virtual double perimeter(void);
@@ -31,13 +31,8 @@ class Square : public Shape {
 private:
   double width;
 public:
-  Square(double w) : width(w) { };
+  Square(double w) : width(w) { }
   ~Square() { }
   virtual double area(void);
   virtual double perimeter(void);
 };
-
-
-
-
-  
