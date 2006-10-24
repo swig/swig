9d06b43af161023b20980ed40de7b70378b80981
William S Fulton <wsf@fultondesigns.co.uk>
2006-10-24 20:50:38 +0000

    new test to test smart pointers with virtual methods and hidden methods
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk/SWIG@9481 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/common.mk
+++ Examples/test-suite/common.mk
@@ -226,6 +226,7 @@ CPP_TEST_CASES += \
 	smart_pointer_const \
 	smart_pointer_const2 \
 	smart_pointer_extend \
+	smart_pointer_inherit \
 	smart_pointer_member \
 	smart_pointer_multi \
 	smart_pointer_multi_typedef \
