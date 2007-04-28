928f12bd44d564e1f18f401ad6a0b51cd51db7c6
Gonzalo Garramuno <ggarra@advancedsl.com.ar>
2007-04-28 15:50:35 +0000

    Updated swig_assert to optionally be more verbose and report stack traces nicelly.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@9692 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/ruby/swig_assert.rb
+++ Examples/test-suite/ruby/swig_assert.rb
@@ -11,6 +11,31 @@
 class SwigRubyError < RuntimeError
 end
 
+class SwigAssertError < StandardError
+end
+
+
 def swig_assert( condition, *args )
-  raise SwigRubyError.new("FAILED CHECK: #{condition} #{args.join(' ')}") unless eval(condition)
+  begin
+    ok = eval(condition.to_s)
+  rescue => e
+    raise SwigAssertError.new("Wrong assert: #{condition.to_s} - #{e}")
+  end
+
+  unless ok
+    raise SwigRubyError.new("FAILED CHECK: #{condition} #{args.join(' ')}")
+  end
+
+  if $VERBOSE
+    $stdout.puts "\tPASSED #{condition} #{args.join(' ')}"
+  end
+
+  return ok
+rescue => e
+  trace = e.backtrace[1..-1]
+  $stderr.puts "#{trace[0,1]}: #{e}"
+  if trace.size > 1
+    $stderr.puts "\tfrom #{trace[1..-1].join("\n\t     ")}"
+  end
+  exit(1)
 end
