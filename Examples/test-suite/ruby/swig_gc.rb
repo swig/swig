ca5f54f668c03d5555716cc4b5e39ab60b79e4c4
Gonzalo Garramuno <ggarra@advancedsl.com.ar>
2007-04-29 23:41:56 +0000

    Made GC.stats report things only in verbose mode.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@9717 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/ruby/swig_gc.rb
+++ Examples/test-suite/ruby/swig_gc.rb
@@ -14,17 +14,19 @@
 module GC
   @@last_stat = nil
   def self.stats(klass = nil)
+    return unless $VERBOSE
+
     stats = Hash.new(0)
     ObjectSpace.each_object {|o| stats[o.class] += 1}
 
     if klass
       v = stats[klass]
-      printf "%-30s  %10d", klass.to_s, v
+      printf "\t%-30s  %10d", klass.to_s, v
       printf " | delta %10d", (v - @@last_stat[klass]) if @@last_stat
       puts
     else
       stats.sort {|(k1,v1),(k2,v2)| v2 <=> v1}.each do |k,v|
-        printf "%-30s  %10d", k, v
+        printf "\t%-30s  %10d", k, v
         printf " | delta %10d", (v - @@last_stat[k]) if @@last_stat
         puts
       end
