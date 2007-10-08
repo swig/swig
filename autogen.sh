3a04a343337044dace992f157201071bbaf40f55
Olly Betts <olly@survex.com>
2007-10-08 14:30:29 +0000

    CVS -> SVN
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@9968 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 # Bootstrap the development environment - add extra files needed to run configure. 
 # Note autoreconf should do what this file achieves, but it has a bug when working with automake!
-# The latest config.guess and config.sub should be copied into Tools/config and checked into CVS
+# The latest config.guess and config.sub should be copied into Tools/config and checked into SVN
 # when upgrading the autotools. Otherwise this script will ensure the latest is copied from 
 # your autotool installation.
 
