4ade44ec6487824170e755b0435bfadfa5dd32f6
William S Fulton <wsf@fultondesigns.co.uk>
2007-10-23 21:53:30 +0000

    vc++ fix
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@10027 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/python/swigrun/example.cxx
+++ Examples/python/swigrun/example.cxx
@@ -9,7 +9,7 @@ Manager* convert_to_Manager(PyObject *py_obj)
 {
   Manager* c_ptr;
   swig_type_info *ty = SWIG_TypeQuery("Manager *");
-  printf("manager ty %x \n", ty);
+  printf("manager ty %p \n", ty);
   if (SWIG_ConvertPtr(py_obj, (void **) &c_ptr, ty, 0) == -1) {
     c_ptr = 0;
   } else {
