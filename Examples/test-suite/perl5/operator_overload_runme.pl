e294f2e158a8b410d1d38edf8ff639ff73521057
Jason Stewart <jason@openinformatics.com>
2008-04-03 11:20:37 +0000

    fixed test-name typo ; add tests for pre- increment/decrement
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@10333 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/perl5/operator_overload_runme.pl
+++ Examples/test-suite/perl5/operator_overload_runme.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
-use Test::More tests => 36;
+use Test::More tests => 38;
 
 use operator_overload;
 
@@ -35,7 +35,7 @@ ok_not(($op == $op2), "operator equal: not equal");
 $op->{i} = 3;
 ok(($op == $op2), "operator equal: equal");
 
-# operator equal
+# operator not equal
 $op->{i} = 5;
 $op2->{i} = 3;
 
@@ -126,15 +126,25 @@ $op2->{i} = 3;
 ok(($op2 <= $op), "operator less than or equal");
 ok(($op <= $op2), "operator less than or equal");
 
-# increment operator
+# post-increment operator
 $op->{i} = 7;
 $op++;
-is($op->{i}, 8, "operator increment");
+is($op->{i}, 8, "operator post-increment");
 
-# decrement operator
+# pre-increment operator
+$op->{i} = 7;
+++$op;
+is($op->{i}, 8, "operator pre-increment");
+
+# post-decrement operator
 $op->{i} = 7;
 $op--;
-is($op->{i}, 6, "operator decrement");
+is($op->{i}, 6, "operator post-decrement");
+
+# pre-decrement operator
+$op->{i} = 7;
+--$op;
+is($op->{i}, 6, "operator pre-decrement");
 
 # neg operator
 $op->{i} = 3;
@@ -147,6 +157,14 @@ is(!$op, !0, "operator not");
 
 $op->{i} = 1;
 is(!$op, !1, "operator not");
+
+### # and operator
+### $op->{i} = 4;
+### $op2->{i} = 2;
+### 
+### is($op & $op2, 4 & 2, "operator and");
+### 
+### isnt(($op & $op2), (10 & 2), "operator and - false");
 
 # fail("testing failed condition");
 
