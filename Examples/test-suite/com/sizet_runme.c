3f23bb3bb0c32050068ab17026d359b8379ba458
Jan Jezabek <jezabek@poczta.onet.pl>
2008-07-18 23:31:46 +0000

    Actually add the test suite.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2008-jezabek@10680 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/com/sizet_runme.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+  printf("Hello!\n");
+
+  return 0;
+}
