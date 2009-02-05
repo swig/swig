6574067b725d7acc74cd98a7f15a78680f066211
Robert Stone <talby@trap.mtview.ca.us>
2009-02-05 15:47:42 +0000

    Fix SF#2564192
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11110 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/perl5/char_strings_runme.pl
+++ Examples/test-suite/perl5/char_strings_runme.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests => 4;
+use Test::More tests => 5;
 BEGIN { use_ok('char_strings') }
 require_ok('char_strings');
 
@@ -10,3 +10,6 @@ is(char_strings::CharPingPong($val1), "100", 'cstr1');
 my $val2 = "greetings";
 is(char_strings::CharPingPong($val2), "greetings", 'cstr2');
 
+# SF#2564192
+"this is a test" =~ /(\w+)$/;
+is(char_strings::CharPingPong($1), "test", "handles Magical");
