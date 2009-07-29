22351914fe181db63743d29a593d4639fbb34f6a
Miklos Vajna <vmiklos@frugalware.org>
2009-07-29 11:03:22 +0000

    PHP: Avoid calling is_resource() twice with no good reason
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11466 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/php.cxx
+++ Source/Modules/php.cxx
@@ -1633,8 +1633,10 @@ public:
 	     * _p_Foo -> Foo, _p_ns__Bar -> Bar
 	     * TODO: do this in a more elegant way
 	     */
-	    Printf(output, "\t\tif (is_resource($r)) $class='%s'.substr(get_resource_type($r), (strpos(get_resource_type($r), '__') ? strpos(get_resource_type($r), '__') + 2 : 3));\n", prefix);
-	    Printf(output, "\t\treturn is_resource($r) ? new $class($r) : $r;\n");
+	    Printf(output, "\t\tif (is_resource($r)) {\n");
+	    Printf(output, "\t\t\t$class='%s'.substr(get_resource_type($r), (strpos(get_resource_type($r), '__') ? strpos(get_resource_type($r), '__') + 2 : 3));\n", prefix);
+	    Printf(output, "\t\t\treturn new $class($r);\n\t\t}\n");
+	    Printf(output, "\t\telse return $r;\n");
 	  } else {
 	    Printf(output, "\t\t$this->%s = $r;\n", SWIG_PTR);
 	    Printf(output, "\t\treturn $this;\n");
