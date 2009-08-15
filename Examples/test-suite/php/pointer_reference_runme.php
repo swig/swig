719df6b10a089fbf5c6518f59e6f73a0fa0dfba7
Olly Betts <olly@survex.com>
2009-08-15 06:08:28 +0000

    Fix pointer_reference_runme.php for PHP5 - now passes.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11579 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/php/pointer_reference_runme.php
+++ Examples/test-suite/php/pointer_reference_runme.php
@@ -3,12 +3,13 @@
 require "tests.php";
 require "pointer_reference.php";
 
-$s  = get();
-check::equal(Struct_value_get($s), 10, "pointer_reference.get failed");
+$s  = pointer_reference::get();
+check::equal($s->value, 10, "pointer_reference::get() failed");
 
-$ss = new_Struct(20);
-set($ss);
-check::equal(Struct_value_get(Struct_instance()), 20, "pointer_reference.set failed");
+$ss = new Struct(20);
+pointer_reference::set($ss);
+$i = Struct::instance();
+check::equal($i->value, 20, "pointer_reference::set() failed");
 
 check::done();
 ?>
