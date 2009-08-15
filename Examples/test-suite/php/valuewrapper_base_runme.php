5118c399f0516890267ba72b2b74cbade1bfba83
Olly Betts <olly@survex.com>
2009-08-15 07:39:56 +0000

    Fix valuewrapper_base_runme.php for PHP5.  Still fails though.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11580 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/php/valuewrapper_base_runme.php
+++ Examples/test-suite/php/valuewrapper_base_runme.php
@@ -7,7 +7,7 @@ require "valuewrapper_base.php";
 check::classes(array("valuewrapper_base","Base","Interface_BP"));
 check::functions("make_interface_bp");
 
-$ibp=make_interface_bp();
+$ibp=valuewrapper_base::make_interface_bp();
 check::classname("interface_bp",$ibp);
 
 check::done();
