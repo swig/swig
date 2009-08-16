3cabcd05537d361cbe6d216e223f08d6979f4895
William S Fulton <wsf@fultondesigns.co.uk>
2009-08-16 22:14:06 +0000

    remove debug print statements
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@11597 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/octave/empty_runme.m
+++ Examples/test-suite/octave/empty_runme.m
@@ -1,17 +1,2 @@
 empty
 
-printf("begin\n");
-who global
-
-printf("after load\n");
-who global
-
-#clear -g
-printf("after clear\n");
-who global
-
-#clear empty
-printf("after clear specific\n");
-who global
-
-printf("before shutdown\n");
