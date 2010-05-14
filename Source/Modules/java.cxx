76bbd7373d469149b8cfa091c32cf35d34861da3
William S Fulton <wsf@fultondesigns.co.uk>
2010-05-14 19:03:01 +0000

    Fix wrapping of C++ enum boolean values when using %javaconst(1)
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@12029 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/java.cxx
+++ Source/Modules/java.cxx
@@ -1352,6 +1352,13 @@ public:
     // Note that this is used in enumValue() amongst other places
     Setattr(n, "value", tmpValue);
 
+    // Deal with enum values that are bools
+    if (SwigType_type(Getattr(n, "type")) == T_BOOL) {
+      String *boolValue = NewStringf("%s ? 1 : 0", Getattr(n, "enumvalue"));
+      Setattr(n, "enumvalue", boolValue);
+      Delete(boolValue);
+    }
+
     {
       EnumFeature enum_feature = decodeEnumFeature(parent);
 
