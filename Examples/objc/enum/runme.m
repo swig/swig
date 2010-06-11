a569ee5e7c2f7c032a022d9bb7832bdcce58d10a
Ashish Sharma <ashishs99@gmail.com>
2010-06-11 15:47:17 +0000

    Code to run enum example
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2009-ashishs99@12118 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/objc/enum/runme.m
+++ Examples/objc/enum/runme.m
@@ -2,5 +2,29 @@
 #import "exampleProxy.h"
 
 int main(int argc, char* argv[]) {
+	// Print out the value of some enums
+	NSLog(@"*** color ***");
+ 	NSLog(@"color::RED =%d", RED);
+ 	NSLog(@"color::BLUE =%d", BLUE);
+	NSLog(@"color::GREEN =%d", GREEN);
+
+ 	NSLog(@"\n*** Foo::speed ***");
+ 	NSLog(@"speed::IMPULSE =%d", IMPULSE);
+ 	NSLog(@"speed::WARP =%d", WARP);
+ 	NSLog(@"speed::LUDICROUS =%d", LUDICROUS);
+
+	NSLog(@"\nTesting use of enums with functions\n");
+
+	enum_test(RED, IMPULSE);
+  	enum_test(BLUE, WARP);
+  	enum_test(GREEN, LUDICROUS);
+
+  	NSLog(@"\nTesting use of enum with class method" );
+  	Foo* f = [[Foo alloc] init];
+
+  	[f enum_test:IMPULSE];
+  	[f enum_test:WARP];
+  	[f enum_test:LUDICROUS];
+    [f release];
 	return 0;
 }
