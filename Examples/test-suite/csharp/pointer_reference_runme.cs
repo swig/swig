640cce2c50ac3fa86f8575fe5262c1fd257dbb44
William S Fulton <wsf@fultondesigns.co.uk>
2011-01-30 00:42:27 +0000

    Fix typecheck typemaps for SWIGTYPE *const&
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@12413 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/csharp/pointer_reference_runme.cs
+++ Examples/test-suite/csharp/pointer_reference_runme.cs
@@ -12,6 +12,9 @@ public class pointer_reference_runme {
     Struct ss = new Struct(20);
     pointer_reference.set(ss);
     if (Struct.instance.value != 20) throw new Exception("set test failed");
+
+    if (pointer_reference.overloading(1) != 111) throw new Exception("overload test 1 failed");
+    if (pointer_reference.overloading(ss) != 222) throw new Exception("overload test 2 failed");
   }
 
 }
