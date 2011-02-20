6c02fa572931211dbeef52ef3fee2522df24acb2
Ian Lance Taylor <iant@google.com>
2011-02-20 04:47:17 +0000

    More varargs overload testing for Go.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@12497 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/go/varargs_overload_runme.go
+++ Examples/test-suite/go/varargs_overload_runme.go
@@ -23,4 +23,16 @@ func main() {
 	if varargs_overload.Vararg_over3(2, 2.2, "hey") != "2 2.2 hey" {
 		panic(5)
 	}
+
+	if varargs_overload.Vararg_over4("Hello") != "Hello" {
+		panic(6)
+	}
+
+	if varargs_overload.Vararg_over4(123) != "123" {
+		panic(7)
+	}
+
+	if varargs_overload.Vararg_over4("Hello", 123) != "Hello" {
+		panic(8)
+	}
 }
