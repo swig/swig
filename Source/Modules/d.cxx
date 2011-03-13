a63cde3827e6c86ca4de28b25a7d3a29cbebc1ca
David Nadlinger <code@klickverbot.at>
2011-03-13 00:31:13 +0000

    [D] Removed "-noproxy" switch from usage text.
    I forgot to remove it from the list when I stripped the (undocumented and untested) code paths for it, which was even before the merge to trunk.
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@12533 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/d.cxx
+++ Source/Modules/d.cxx
@@ -4342,8 +4342,6 @@ extern "C" Language *swig_d(void) {
  * ----------------------------------------------------------------------------- */
 const char *D::usage = (char *) "\
 D Options (available with -d)\n\
-     -noproxy             - Generate the low-level functional interface instead\n\
-                            of proxy classes\n\
      -package <pkg>       - Write generated D modules into package <pkg>\n\
      -splitproxy          - Write each D type to a dedicated file instead of\n\
                             generating a single proxy D module.\n\
