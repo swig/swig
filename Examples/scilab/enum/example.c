6db40d8378eb6692c9870af2eaf2eab73af7e7f3
Vincent Couvert <vincent.couvert@scilab.org>
2011-05-20 08:30:01 +0000

    Remove useless space
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2009-sploving@12697 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/scilab/enum/example.c
+++ Examples/scilab/enum/example.c
@@ -7,7 +7,7 @@ void enum_test(color c) {
   if (c == RED) {
     printf("color = RED\n");
   } else if (c == BLUE) {
-    printf("color = BLUE\n ");
+    printf("color = BLUE\n");
   } else if (c == GREEN) {
     printf("color = GREEN\n");
   } else {
