daffde6c2814a145cb0658a3c8dff440fb9f0dbd
Karl Wette <karl.wette@gmail.com>
2012-05-14 09:24:34 +0000

    Updated Octave module_load examples for new module loading
    - Module compiled twice to check '-globals .' behaviour
    - Only one runme.m needed since clearing modules should
      now be safe for all Octave versions.
    - Tests new module loading syntax and behaviour
    
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/trunk@13089 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/octave/module_load/runme.m
@@ -0,0 +1,112 @@
+# file: runme_args.m
+
+# test module loading with arguments
+clear all
+
+# access module, no global load
+example = example;
+assert(example.cvar.ivar == example.ifunc());
+clear all
+example = example;
+assert(example.cvar.ivar == example.ifunc());
+clear all
+
+# load module globally
+example;
+assert(cvar.ivar == ifunc);
+assert(exist("example","var"));
+clear all
+example;
+assert(cvar.ivar == ifunc);
+assert(exist("example","var"));
+clear all
+
+# access module in a function, no global load
+function testme
+  example = example;
+  assert(example.cvar.ivar == example.ifunc());
+endfunction
+testme
+testme
+example = example;
+assert(example.cvar.ivar == example.ifunc());
+clear all
+function testme
+  example = example;
+  assert(example.cvar.ivar == example.ifunc());
+endfunction
+testme
+testme
+example = example;
+assert(example.cvar.ivar == example.ifunc());
+clear all
+
+# load module in a function globally before base context
+function testme
+  example;
+  assert(cvar.ivar == ifunc);
+  assert(exist("example","var"));
+endfunction
+testme
+testme
+example;
+assert(cvar.ivar == ifunc);
+assert(exist("example","var"));
+clear all
+function testme
+  example;
+  assert(cvar.ivar == ifunc);
+  assert(exist("example","var"));
+endfunction
+testme
+testme
+example;
+assert(cvar.ivar == ifunc);
+assert(exist("example","var"));
+clear all
+
+# load module in a function globally after base context
+example;
+assert(cvar.ivar == ifunc);
+assert(exist("example","var"));
+function testme
+  example;
+  assert(cvar.ivar == ifunc);
+  assert(exist("example","var"));
+endfunction
+testme
+testme
+clear all
+example;
+assert(cvar.ivar == ifunc);
+assert(exist("example","var"));
+function testme
+  example;
+  assert(cvar.ivar == ifunc);
+  assert(exist("example","var"));
+endfunction
+testme
+testme
+clear all
+
+# access module with no cvar, no global load
+example2 = example2;
+assert(example2.ivar == example2.ifunc());
+assert(!isglobal("cvar"))
+clear all
+example2 = example2;
+assert(example2.ivar == example2.ifunc());
+assert(!isglobal("cvar"))
+clear all
+
+# load module with no cvar globally
+example2;
+assert(example2.ivar == ifunc);
+assert(exist("example2","var"));
+assert(!isglobal("cvar"))
+clear all
+example2;
+assert(example2.ivar == ifunc);
+assert(exist("example2","var"));
+assert(!isglobal("cvar"))
+clear all
