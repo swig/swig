ed66fe279abb6b4ff4c9983bdb775033c4ec3110
Swati Sharma <itawswati@gmail.com>
2012-06-10 03:40:27 +0000

    Fixed Broken Examples(Callback,Constants)-Added code in runme.m files
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-objc@13156 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/objc/callback/runme.m
+++ Examples/objc/callback/runme.m
@@ -2,5 +2,15 @@
 #import "exampleProxy.h"
 
 int main(int argc, char* argv[]) {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	Caller *caller=[[Caller alloc]init];
+	Callback *callback=[[Callback alloc]init];
+	[caller setCallback:callback];
+	[caller call];
+	[caller delCallback];
+		
+	[callback release];
+	[pool drain];
+
 	return 0;
 }
--- Examples/objc/constants/runme.m
+++ Examples/objc/constants/runme.m
@@ -2,5 +2,17 @@
 #import "exampleProxy.h"
 
 int main(int argc, char* argv[]) {
+	
+	
+	printf("ICONST  = %i (should be 42)\n", ICONST);
+	printf("FCONST  = %f (should be 2.1828)\n", FCONST);
+	printf("CCONST  = %c (should be 'x')\n", CCONST);
+	printf("CCONST2 = %c (this should be on a new line)\n", CCONST2);
+	printf("SCONST  = %s (should be 'Hello World')\n", SCONST);
+	printf("SCONST2 = %s (should be '\"Hello World\"')\n", SCONST2);
+	printf("EXPR    = %f (should be 48.5484)\n", EXPR);
+	printf("iconst  = %i (should be 37)\n", iconst);
+	printf("fconst  = %f (should be 3.14)\n",fconst);
 	return 0;
+	
 }
