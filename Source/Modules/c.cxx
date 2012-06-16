ed7e3143ce53fffa8f8ba13b1551b58bfab8e05e
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-06-16 18:57:55 +0000

    Use different types to generate typesafe API.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13169 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/c.cxx
+++ Source/Modules/c.cxx
@@ -1288,7 +1288,7 @@ ready:
 
       // declare type for specific class in the proxy header
       if (proxy_flag)
-        Printv(f_proxy_header, "\ntypedef SwigObj ", name, ";\n\n", NIL);
+        Printv(f_proxy_header, "\ntypedef SwigObj_", name, " ", name, ";\n\n", NIL);
 
       Delete(sobj);
       Delete(name);
