8a683e42d0341388a46b964572d46ecf40a69796
Dmitry Kabak <userdima@gmail.com>
2012-07-23 13:50:27 +0000

    Fixed and updated all doxygen python tests
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-doxygen@13346 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/python/doxygen_basic_translate_runme.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import doxygen_basic_translate
+import string
+import sys
+
+def check(got, expected):
+  #if got is Null
+  #  raise RuntimeError('Expected comment string\n')
+  gotStr = string.replace(got, ' ', '')
+  gotStr = string.replace(gotStr, '\n', '')
+  gotStr = string.replace(gotStr, '\t', '')
+  expectedStr = string.replace(expected, ' ', '')
+  expectedStr = string.replace(expectedStr, '\n', '')
+  expectedStr = string.replace(expectedStr, '\t', '')
+  if not gotStr == expectedStr:
+    raise RuntimeError("Expected: [" + str(expected) + "]\n" + "Got     : [" + str(got) + "]\n")
+
+check(doxygen_basic_translate.function.__doc__, ''
+'  Brief description.\n'
+'  The comment text\n'
+'  Authors:\n'
+'  Some author\n'
+'  Return:\n'
+'  Some number\n'
+'  See also:\n'
+'  function2\n'
+)
+check(doxygen_basic_translate.function2.__doc__, ''
+'  A test of a very very very very very very very very very very very'
+'  very very very very very'
+'  very very very very very long comment'
+'  string.'
+)
+check(doxygen_basic_translate.function3.__doc__, ''
+'  ----------------------------------------------------------------\n'
+'  Overload 1:\n'
+'  ----------------------------------------------------------------\n'
+'  A test for overloaded functions\n'
+'  This is function __one__\n'
+'  ----------------------------------------------------------------\n'
+'  Overload 2:\n'
+'  ----------------------------------------------------------------\n'
+'  A test for overloaded functions\n'
+'  This is function __two__\n'
+)
+check(doxygen_basic_translate.function4.__doc__, ''
+'  A test of some mixed tag usage\n' 
+'  If: CONDITION {\n'
+'  This _code_fragment shows us something .\n'
+'  Title: Minuses:\n'
+'   -it\'s senseless\n'
+'   -it\'s stupid\n'
+'   -it\'s null\n'
+'  Warning:\n'
+'  This may not work as expected\n'
+'  int main() { while(true); }\n'
+'  }'
+)
+check(doxygen_basic_translate.function5.__doc__, ''
+'  Test for default args'
+)
--- Examples/test-suite/python/doxygen_parsing_runme.py
+++ Examples/test-suite/python/doxygen_parsing_runme.py
@@ -1,39 +1,47 @@
 #!/usr/bin/python
 
 import doxygen_parsing
+import string
 import sys
-import re
 
 def check(got, expected):
-  if not re.match(str(expected), str(got)):
-    raise RuntimeError("\n" + "Expected: [" + str(expected) + "]\n" + "Got     : [" + str(got) + "]")
+  #if got is Null
+  #  raise RuntimeError('Expected comment string\n')
+  gotStr = string.replace(got, ' ', '')
+  gotStr = string.replace(gotStr, '\n', '')
+  gotStr = string.replace(gotStr, '\t', '')
+  expectedStr = string.replace(expected, ' ', '')
+  expectedStr = string.replace(expectedStr, '\n', '')
+  expectedStr = string.replace(expectedStr, '\t', '')
+  if not gotStr == expectedStr:
+    raise RuntimeError("Expected: [" + str(expected) + "]\n" + "Got     : [" + str(got) + "]\n")
 
-check(doxygen_parsing.someFunction.__doc__, '\s+The function comment\s+')
-check(doxygen_parsing.SomeClass.__doc__, '\s+The class comment\s+')
-check(doxygen_parsing.SomeStruct.__doc__, '\s+The struct comment\s+')
-check(doxygen_parsing.SomeAnotherClass.classMethod.__doc__, '\s+The class method comment\s+')
+check(doxygen_parsing.someFunction.__doc__, 'The function comment')
+check(doxygen_parsing.SomeClass.__doc__, 'The class comment')
+check(doxygen_parsing.SomeStruct.__doc__, 'The struct comment')
+check(doxygen_parsing.SomeAnotherClass.classMethod.__doc__, 'The class method comment')
 check(doxygen_parsing.SomeAnotherClass.classMethodExtended.__doc__, ''
-'\s+The class method with parameter'
-'\s+Arguments:\s+'
-'a \(int\)\s+-- Parameter a\s+'
-'b \(int\)\s+-- Parameter b\s+'
+'The class method with parameter\n'
+'Arguments:\n'
+'a (int) -- Parameter a\n'
+'b (int) -- Parameter b\n'
 )
 check(doxygen_parsing.SomeAnotherClass.classMethodExtended2.__doc__, ''
-'\s+The class method with parameter'
-'\s+Arguments:\s+'
-'a \(int\)\s+-- Parameter a\s+'
-'b \(int\)\s+-- Parameter b\s+'
+'The class method with parameter\n'
+'Arguments:\n'
+'a (int)-- Parameter a\n'
+'b (int)-- Parameter b\n'
 )
-check(doxygen_parsing.SomeAnotherStruct.structMethod.__doc__, '\s+The struct method comment\s+')
+check(doxygen_parsing.SomeAnotherStruct.structMethod.__doc__, 'The struct method comment')
 check(doxygen_parsing.SomeAnotherStruct.structMethodExtended.__doc__, ''
-'\s+The struct method with parameter'
-'\s+Arguments:\s+'
-'a \(int\)\s+-- Parameter a\s+'
-'b \(int\)\s+-- Parameter b\s+'
+'The struct method with parameter\n'
+'Arguments:\n'
+'a (int)-- Parameter a\n'
+'b (int)-- Parameter b\n'
 )
 check(doxygen_parsing.SomeAnotherStruct.structMethodExtended2.__doc__, ''
-'\s+The struct method with parameter'
-'\s+Arguments:\s+'
-'a \(int\)\s+-- Parameter a\s+'
-'b \(int\)\s+-- Parameter b\s+'
+'The struct method with parameter\n'
+'Arguments:\n'
+'a (int)-- Parameter a\n'
+'b (int)-- Parameter b\n'
 )
