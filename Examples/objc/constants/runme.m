e779d1ecb2cab9d4d9ee4b2f5287c680f84e8e9c
Swati Sharma <itawswati@gmail.com>
2012-07-25 08:19:39 +0000

    Changes with respect to Refactored Module Code
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-objc@13397 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/objc/constants/runme.m
+++ Examples/objc/constants/runme.m
@@ -4,15 +4,15 @@
 int main(int argc, char* argv[]) {
 	
 	
-	printf("ICONST  = %i (should be 42)\n", ICONST);
-	printf("FCONST  = %f (should be 2.1828)\n", FCONST);
-	printf("CCONST  = %c (should be 'x')\n", CCONST);
-	printf("CCONST2 = %c (this should be on a new line)\n", CCONST2);
-	printf("SCONST  = %s (should be 'Hello World')\n", SCONST);
-	printf("SCONST2 = %s (should be '\"Hello World\"')\n", SCONST2);
-	printf("EXPR    = %f (should be 48.5484)\n", EXPR);
-	printf("iconst  = %i (should be 37)\n", iconst);
-	printf("fconst  = %f (should be 3.14)\n",fconst);
+	NSLog(@"ICONST  = %i (should be 42)\n", ICONST);
+	NSLog(@"FCONST  = %f (should be 2.1828)\n", FCONST);
+	NSLog(@"CCONST  = %c (should be 'x')\n", CCONST);
+	NSLog(@"CCONST2 = %c (this should be on a new line)\n", CCONST2);
+	NSLog(@"SCONST  = %@ (should be 'Hello World')\n", SCONST);
+	NSLog(@"SCONST2 = %@ (should be '\"Hello World\"')\n", SCONST2);
+	NSLog(@"EXPR    = %f (should be 48.5484)\n", EXPR);
+	NSLog(@"iconst  = %i (should be 37)\n", iconst);
+	NSLog(@"fconst  = %f (should be 3.14)\n",fconst);
 	return 0;
 	
 }
