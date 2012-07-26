48fe82841fac1f1e6248682e0f233c87d774b7a4
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-07-26 20:20:29 +0000

    Add C implementation of test cpp_atomic_global_var_class
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13408 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/c/cpp_atomic_global_var_class_runme.c
@@ -0,0 +1,9 @@
+#include <assert.h>
+#include "cpp_atomic_global_var_class/cpp_atomic_global_var_class_proxy.h"
+
+int main(int argc, const char *argv[])
+{
+    assert(myGlobalClassInstance);
+
+    return 0;
+}
