15e8b647a2c7f70cc2db213947570f8d12c4add8
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-07-26 20:23:27 +0000

    Add C implementation of test cpp_atomic_class_enum
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13420 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/c/cpp_atomic_class_enum_runme.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include "cpp_atomic_class_enum/cpp_atomic_class_enum_proxy.h"
+
+int main(int argc, const char *argv[])
+{
+    enum MyClass_SomeEnum myEnum = MyClass_FIRST_VALUE;
+
+    assert(myEnum != MyClass_SECOND_VALUE);
+
+    return 0;
+}
