f24ce340ff02b581c65f6e702176cb0eb249d8d9
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-07-26 20:22:37 +0000

    Add C implementation of test cpp_atomic_class_var_pub_member_atom
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13416 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/c/cpp_atomic_class_var_pub_member_atom_runme.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include "cpp_atomic_class_var_pub_member_atom/cpp_atomic_class_var_pub_member_atom_proxy.h"
+
+int main(int argc, const char *argv[])
+{
+    MyClass *mc = new_MyClass();
+
+    assert(MyClass_myPubInt_get(mc) == 42);
+    MyClass_myPubInt_set(mc, 4711);
+    assert(MyClass_myPubInt_get(mc) == 4711);
+
+    delete_MyClass(mc);
+
+    return 0;
+}
