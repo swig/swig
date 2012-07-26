dbd8d3aabdcb0d3833969c8da5f9fca73cecfdc7
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-07-26 20:22:08 +0000

    Add C implementation of test cpp_atomic_class_var_pub_member_class
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13414 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/c/cpp_atomic_class_var_pub_member_class_runme.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include "cpp_atomic_class_var_pub_member_class/cpp_atomic_class_var_pub_member_class_proxy.h"
+
+int main(int argc, const char *argv[])
+{
+    MyClass *mc;
+    MySecondClass *mc2;
+
+    mc2 = new_MySecondClass();
+    mc = new_MyClass();
+
+    assert(MySecondClass_myPubClassInstance_get(mc2));
+    MySecondClass_myPubClassInstance_set(mc2, mc);
+    assert(MySecondClass_myPubClassInstance_get(mc2));
+
+    delete_MyClass(mc);
+    delete_MySecondClass(mc2);
+
+    return 0;
+}
