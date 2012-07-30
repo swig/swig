ddc0b8c819be97724391a3f259fe2eb932a0abf2
Swati Sharma <itawswati@gmail.com>
2012-07-30 17:33:37 +0000

    Fix for enums.
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-objc@13432 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/objc/enum/runme.m
+++ Examples/objc/enum/runme.m
@@ -9,21 +9,21 @@ int main(int argc, char* argv[]) {
 	NSLog(@"color::GREEN =%d", GREEN);
 
  	NSLog(@"\n*** Foo::speed ***");
- 	NSLog(@"speed::IMPULSE =%d", IMPULSE);
- 	NSLog(@"speed::WARP =%d", WARP);
- 	NSLog(@"speed::LUDICROUS =%d", LUDICROUS);
+ 	NSLog(@"speed::IMPULSE =%d",Foo_IMPULSE);
+ 	NSLog(@"speed::WARP =%d",Foo_WARP);
+ 	NSLog(@"speed::LUDICROUS =%d",Foo_LUDICROUS);
 
 	NSLog(@"\nTesting use of enums with global function\n");
-	ObjcEnum_test(RED, IMPULSE);
-  	ObjcEnum_test(BLUE, WARP);
-  	ObjcEnum_test(GREEN, LUDICROUS);
+	ObjcEnum_test(RED,Foo_IMPULSE);
+  	ObjcEnum_test(BLUE,Foo_WARP);
+  	ObjcEnum_test(GREEN,Foo_LUDICROUS);
    
   	NSLog(@"\nTesting use of enum with class method" );
   	Foo* f = [[Foo alloc] init];
 
-  	[f enum_test:IMPULSE];
-  	[f enum_test:WARP];
-  	[f enum_test:LUDICROUS];
+  	[f enum_test:Foo_IMPULSE];
+  	[f enum_test:Foo_WARP];
+  	[f enum_test:Foo_LUDICROUS];
         [f release];
 	
        return 0;
