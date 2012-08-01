b0318dae692375f02f30cc0d8bf84e508dea9b2e
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-08-01 12:53:17 +0000

    Add test for 'natural' C++ std::string handling in C (char*).
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13482 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/c/c_backend_cpp_natural_std_string_runme.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "c_backend_cpp_natural_std_string/c_backend_cpp_natural_std_string_proxy.h"
+
+int main()
+{
+	char *myComposedString = myStringAppend("World, ", "Hello!");
+
+	assert(myComposedString);
+	assert(strcmp(myComposedString, "World, Hello!") == 0);
+
+	free(myComposedString);
+
+	return 0;
+}
