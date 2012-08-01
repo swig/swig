44f4dd4dc02ed39c146bdea1af7a29f424f65e7a
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-08-01 00:51:50 +0000

    Extend test to check dynamic binding of virtual methods
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13480 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Examples/test-suite/c/cpp_basic_class_virtual_method_runme.c
+++ Examples/test-suite/c/cpp_basic_class_virtual_method_runme.c
@@ -6,14 +6,19 @@ int main()
     BaseClass *bc = new_BaseClass();
     NonMethodOverwritingClass *noc = new_NonMethodOverwritingClass();
     MethodOverwritingClass *oc = new_MethodOverwritingClass();
+    BaseClass *inherited_bc = (BaseClass*)new_MethodOverwritingClass();
 
     assert(BaseClass_myInt(bc) == 0xba53);
     assert(NonMethodOverwritingClass_myInt(noc) == 0xba53);
     assert(MethodOverwritingClass_myInt(oc) == 0xa173123d);
+    assert(BaseClass_myInt((BaseClass*)noc) == 0xba53);
+    assert(BaseClass_myInt((BaseClass*)oc) == 0xa173123d);
+    assert(BaseClass_myInt(inherited_bc) == 0xa173123d);
     
     delete_BaseClass(bc);
     delete_NonMethodOverwritingClass(noc);
     delete_MethodOverwritingClass(oc);
+    delete_BaseClass(inherited_bc);
     
     return 0;
 }
