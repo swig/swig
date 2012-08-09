d272709899d61b71567f896e8b98734ffe428c5e
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-08-09 13:06:22 +0000

    Add C runtime test for corresponding C++ basic template test
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-c@13573 626c5289-ae23-0410-ae9c-e8d60b6d4f22

---
+++ Examples/test-suite/c/cpp_basic_template_class_runme.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include "cpp_basic_template_class/cpp_basic_template_class_proxy.h"
+
+int main() {
+	MyTemplateClass_Int *ci = new_MyTemplateClass_Int();
+
+	MyTemplateClass_Int_someMemberVariable_set(ci, 42);
+	assert(MyTemplateClass_Int_someMemberVariable_get(ci) == 42);
+
+	delete_MyTemplateClass_Int(ci);
+
+	return 0;
+}
