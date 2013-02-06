08093ec17e1492826b70571d161b1c9a6a0e8385
Marko Klopcic <markok3.14@gmail.com>
2013-02-06 22:59:30 +0100

    updated two additonal tests for python doxygen code comments

---
+++ Examples/test-suite/python/doxygen_basic_notranslate_runme.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import doxygen_basic_notranslate
+import string
+import sys
+import commentVerifier
+
+commentVerifier.check(doxygen_basic_notranslate.function.__doc__,
+  r"""
+  \brief
+  Brief description.
+
+  The comment text
+  \author Some author
+  \return Some number
+  \sa function2
+
+  """
+)
+
+commentVerifier.check(doxygen_basic_notranslate.function2.__doc__,
+  r"""
+  A test of a very very very very very very very very very very very very very very very very
+  very very very very very long comment string.
+
+  """
+)
+
+commentVerifier.check(doxygen_basic_notranslate.function3.__doc__,
+  r"""
+  ----------------------------------------------------------------
+  Overload 1:
+  ----------------------------------------------------------------
+   A test for overloaded functions
+   This is function \b one
+   
+  ----------------------------------------------------------------
+  Overload 2:
+  ----------------------------------------------------------------
+   A test for overloaded functions
+   This is function \b two
+   
+  """
+)
+
+commentVerifier.check(doxygen_basic_notranslate.function4.__doc__,
+  r"""
+  A test of some mixed tag usage
+  \if CONDITION
+  This \a code fragment shows us something \.
+  \par Minuses:
+  \arg it's senseless
+  \arg it's stupid
+  \arg it's null
+
+  \warning This may not work as expected
+
+  \code
+  int main() { while(true); }
+  \endcode
+  \endif
+
+  """
+)
+commentVerifier.check(doxygen_basic_notranslate.function5.__doc__,
+  r"""
+  This is a post comment. 
+  """
+)
+commentVerifier.check(doxygen_basic_notranslate.function6.__doc__,
+  r"""
+  Test for default args
+  @param a Some parameter, default is 42
+
+  """
+)
+commentVerifier.check(doxygen_basic_notranslate.function7.__doc__,
+  r"""
+  Test for a parameter with difficult type
+  (mostly for python)
+  @param a Very strange param
+
+  """
+)
--- Examples/test-suite/python/doxygen_parsing_runme.py
+++ Examples/test-suite/python/doxygen_parsing_runme.py
@@ -3,45 +3,60 @@
 import doxygen_parsing
 import string
 import sys
+import commentVerifier
 
-def check(got, expected):
-  #if got is Null
-  #  raise RuntimeError('Expected comment string\n')
-  gotStr = string.replace(got, ' ', '')
-  gotStr = string.replace(gotStr, '\n', '')
-  gotStr = string.replace(gotStr, '\t', '')
-  expectedStr = string.replace(expected, ' ', '')
-  expectedStr = string.replace(expectedStr, '\n', '')
-  expectedStr = string.replace(expectedStr, '\t', '')
-  if not gotStr == expectedStr:
-    raise RuntimeError("Expected: [" + str(expected) + "]\n" + "Got     : [" + str(got) + "]\n")
+commentVerifier.check(doxygen_parsing.someFunction.__doc__,
+r"""
+  The function comment
+  """)
+commentVerifier.check(doxygen_parsing.SomeClass.__doc__,
+r"""
+    The class comment
+    """)
+commentVerifier.check(doxygen_parsing.SomeStruct.__doc__,
+r"""
+    The struct comment
+    """)
+commentVerifier.check(doxygen_parsing.SomeAnotherClass.classMethod.__doc__,
+r"""
+        The class method comment
+        """)
+commentVerifier.check(doxygen_parsing.SomeAnotherClass.classMethodExtended.__doc__,
+r"""
+        The class method with parameter
 
-check(doxygen_parsing.someFunction.__doc__, 'The function comment')
-check(doxygen_parsing.SomeClass.__doc__, 'The class comment')
-check(doxygen_parsing.SomeStruct.__doc__, 'The struct comment')
-check(doxygen_parsing.SomeAnotherClass.classMethod.__doc__, 'The class method comment')
-check(doxygen_parsing.SomeAnotherClass.classMethodExtended.__doc__, ''
-'The class method with parameter\n'
-'Arguments:\n'
-'a (int) -- Parameter a\n'
-'b (int) -- Parameter b\n'
+        Arguments:
+          a (int) -- Parameter a
+          b (int) -- Parameter b
+        """
 )
-check(doxygen_parsing.SomeAnotherClass.classMethodExtended2.__doc__, ''
-'The class method with parameter\n'
-'Arguments:\n'
-'a (int)-- Parameter a\n'
-'b (int)-- Parameter b\n'
+commentVerifier.check(doxygen_parsing.SomeAnotherClass.classMethodExtended2.__doc__, 
+r"""
+        The class method with parameter
+
+        Arguments:
+          a (int) -- Parameter a
+          b (int) -- Parameter b
+        """
 )
-check(doxygen_parsing.SomeAnotherStruct.structMethod.__doc__, 'The struct method comment')
-check(doxygen_parsing.SomeAnotherStruct.structMethodExtended.__doc__, ''
-'The struct method with parameter\n'
-'Arguments:\n'
-'a (int)-- Parameter a\n'
-'b (int)-- Parameter b\n'
+commentVerifier.check(doxygen_parsing.SomeAnotherStruct.structMethod.__doc__,
+r"""
+        The struct method comment
+        """)
+commentVerifier.check(doxygen_parsing.SomeAnotherStruct.structMethodExtended.__doc__,
+r"""
+        The struct method with parameter
+
+        Arguments:
+          a (int) -- Parameter a
+          b (int) -- Parameter b
+        """
 )
-check(doxygen_parsing.SomeAnotherStruct.structMethodExtended2.__doc__, ''
-'The struct method with parameter\n'
-'Arguments:\n'
-'a (int)-- Parameter a\n'
-'b (int)-- Parameter b\n'
-)
+commentVerifier.check(doxygen_parsing.SomeAnotherStruct.structMethodExtended2.__doc__,
+r"""
+        The struct method with parameter
+
+        Arguments:
+          a (int) -- Parameter a
+          b (int) -- Parameter b
+        """)
