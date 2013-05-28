f0a5ec45a2c491125bc584c2536b3b1d8b566e18
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2013-05-28 05:33:56 +0200

    Fixed SF bug #1297
    This changeset resolves several issues related to python imports.
    For example, it's possible now to import modules having same module
    names, but belonging to different packages.
    
    From the user's viewpoint, this patch gives a little bit more control on
    import directives generated by swig. The user may choose to use relative
    or absolute imports (docs are provided in separate PR).
    
    Some details:
      - we (still) generate import directives in form 'import a.b.c' which
        corresponds to absolute imports in python3 and (the only available)
        ambiguous one in python2.
      - added -relativeimport option to use explicit relative import syntax
        (python3),
    
    Tests are under Examples/python, these are in fact regression tests but
    with the current swig testing framework it seems to be impossible to put
    appropriate tests under test-suite.

---
+++ Examples/python/import_packages/relativeimport2/runme3.py
@@ -0,0 +1,9 @@
+# Test import of modules content from within __init__.py
+print("Testing %module(package=...) + python 'import' in __init__.py")
+import sys
+if sys.version_info < (3, 0):
+  import py2.pkg2.bar
+  print("  Finished importing py2.pkg2.bar")
+else:
+  import py3.pkg2.bar
+  print("  Finished importing py3.pkg2.bar")
