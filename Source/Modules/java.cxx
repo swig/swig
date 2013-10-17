22034e9930b309a29a0c14bc21baac5018859acb
Marvin Greenberg <public.marvin@gmail.com>
2013-10-17 08:57:06 -0400

    Change ambiguous warning 826 to errors when generated java code will be invalid.

--- Source/Modules/java.cxx
+++ Source/Modules/java.cxx
@@ -179,11 +179,15 @@ public:
     else
       full_imclass_name = NewStringf("%s", imclass_name);
 
-    if (nspace && !package) {
-      String *name = Getattr(n, "name") ? Getattr(n, "name") : NewString("<unnamed>");
-      Swig_warning(WARN_JAVA_NSPACE_WITHOUT_PACKAGE, Getfile(n), Getline(n),
-	  "The nspace feature is used on '%s' without -package. "
-	  "The generated code may not compile as Java does not support types declared in a named package accessing types declared in an unnamed package.\n", name);
+    if (!nspace && !package) {
+      String *name = Getattr(n, "name");
+      Swig_error(Getfile(n), Getline(n),
+		 "Generating '%s' without using %%nspace or -package \"pkgname\" creates java classes in an unnamed package, which cannot be used from other code", name ? name : "<unnamed>");
+    }
+    if (nspace && !package && !imclass_package) {
+      String *name = Getattr(n, "name");
+      Swig_error(Getfile(n), Getline(n),
+		 "The nspace feature is used on '%s' without -package. Interface must provide %%pragma(java) jniclasspackage=\"pkgname\" to generate valid code\n", name ? name : "<unnamed>");
     }
   }
 
