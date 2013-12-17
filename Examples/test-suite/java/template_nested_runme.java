32b4890be086f06af96ed35b97dad67cae19cdb9
Vladimir Kalinin <vkalinin@opendesign.com>
2013-12-17 20:10:07 +0400

    nested %template test

--- Examples/test-suite/java/template_nested_runme.java
+++ Examples/test-suite/java/template_nested_runme.java
@@ -29,6 +29,8 @@ public class template_nested_runme {
     OuterClass.T_OuterClassInner2NormalClass inner2 = new OuterClass.T_OuterClassInner2NormalClass();
     inner2.setEmbeddedVar(2);
     OuterClass.T_OuterClassInner2NormalClass inner22 = new OuterClass().useInner2Again(inner2);
+		OuterClass.T_OuterClassInner1Double inner3 = new OuterClass.T_OuterClassInner1Double();
+		OuterClass.T_OuterClassInner2Double	inner4 = new OuterClass.T_OuterClassInner2Double();
   }
 }
 
