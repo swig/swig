0de4cf13a8aa4612ddead85b2680694fa1bf585e
Olly Betts <olly@survex.com>
2014-02-24 10:10:24 +1300

    Further cleaning up of class examples

--- Examples/pike/class/example.h
+++ Examples/pike/class/example.h
@@ -2,12 +2,16 @@
 
 class Shape {
 public:
-  Shape();
-  virtual ~Shape();
-  double  x, y;   
+  Shape() {
+    nshapes++;
+  }
+  virtual ~Shape() {
+    nshapes--;
+  }
+  double  x, y;
   void    move(double dx, double dy);
-  virtual double area() const = 0;
-  virtual double perimeter() const = 0;
+  virtual double area() = 0;
+  virtual double perimeter() = 0;
   static  int nshapes;
 };
 
@@ -15,21 +19,16 @@ class Circle : public Shape {
 private:
   double radius;
 public:
-  Circle(double r) : radius(r) { };
-  virtual double area() const;
-  virtual double perimeter() const;
+  Circle(double r) : radius(r) { }
+  virtual double area();
+  virtual double perimeter();
 };
 
 class Square : public Shape {
 private:
   double width;
 public:
-  Square(double w) : width(w) { };
-  virtual double area() const;
-  virtual double perimeter() const;
+  Square(double w) : width(w) { }
+  virtual double area();
+  virtual double perimeter();
 };
-
-
-
-
-  
