a40425ab08b0fc8f4b20e51280562feadf3a2f97
Oliver Buchtala <oliver.buchtala@googlemail.com>
2014-03-06 06:47:10 +0100

    Remove pandoc toolchain.

--- Doc/Manual/markdown_to_html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" ]; then
-	echo "Usage: markdown_to_html <Language>"
-	echo "-- Example: markdown_to_html Javascript"
-else
-	pandoc -f markdown -t json $1.md | ./pandoc_filter.py | pandoc -f json -t html -s --template pandoc_template.html -o $1.html
-fi
-
