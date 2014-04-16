4837c5c9eb172d400a8e82e83b0bed2e2e36bda8
William S Fulton <wsf@fultondesigns.co.uk>
2014-04-16 07:50:28 +0100

    Add missing license info

--- Source/Modules/javascript.cxx
+++ Source/Modules/javascript.cxx
@@ -1,3 +1,16 @@
+/* -----------------------------------------------------------------------------
+ * This file is part of SWIG, which is licensed as a whole under version 3 
+ * (or any later version) of the GNU General Public License. Some additional
+ * terms also apply to certain portions of SWIG. The full details of the SWIG
+ * license and copyrights can be found in the LICENSE and COPYRIGHT files
+ * included with the SWIG source code as distributed by the SWIG developers
+ * and at http://www.swig.org/legal.html.
+ *
+ * javascript.cxx
+ *
+ * Javascript language module for SWIG.
+ * ----------------------------------------------------------------------------- */
+
 #include "swigmod.h"
 
 /**
