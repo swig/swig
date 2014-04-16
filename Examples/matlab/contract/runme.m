c721501dfb6e39f83050e7745eb4d97cbcd497de
Joel Andersson <j.a.e.andersson@gmail.com>
2014-04-16 20:18:29 +0200

    [MATLAB] Added contract example

---
+++ Examples/matlab/contract/runme.m
@@ -0,0 +1,19 @@
+% file: runme.m
+
+% Call our gcd() function
+
+x = 42;
+y = 105;
+g = swigexample.gcd(x,y);
+disp(sprintf('The gcd of %d and %d is %d',x,y,g))
+
+% Manipulate the Foo global variable
+
+% Output its current value
+disp(sprintf('Foo = %f', swigexample.Foo))
+
+% Change its value
+swigexample.Foo(3.1415926);
+
+% See if the change took effect
+disp(sprintf('Foo = %f', swigexample.Foo))
