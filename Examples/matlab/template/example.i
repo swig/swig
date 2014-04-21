3ad6a1a416c0732b27d4f094f56ab61b95ca66fd
Joel Andersson <j.a.e.andersson@gmail.com>
2014-04-21 12:19:16 +0200

    [MATLAB] Added 'template' example, resolved item access and assignment conflicts

---
+++ Examples/matlab/template/example.i
@@ -0,0 +1,16 @@
+/* File : example.i */
+%module swigexample
+
+%{
+#include "example.h"
+%}
+
+/* Let's just grab the original header file here */
+%include "example.h"
+
+/* Now instantiate some specific template declarations */
+
+%template(maxint) max<int>;
+%template(maxdouble) max<double>;
+%template(vecint) vector<int>;
+%template(vecdouble) vector<double>;
---
+++ Examples/matlab/template/runme.m
@@ -0,0 +1,33 @@
+% file: runme.m
+
+% Call some templated functions
+swigexample.maxint(3,7)
+swigexample.maxdouble(3.14,2.18)
+
+% Create some class
+
+iv = swigexample.vecint(100)
+dv = swigexample.vecdouble(1000)
+
+for i=0:99,
+  iv.setitem(i,2*i);
+end
+
+for i=0:999,
+  dv.setitem(i, 1.0/(i+1));
+end
+
+sum = 0;
+for i=0:99
+      sum = sum + iv.getitem(i);
+end
+sum
+
+sum = 0.0;
+for i=0:999,
+      sum = sum + dv.getitem(i);
+end
+sum
+
+clear iv
+clear dv
