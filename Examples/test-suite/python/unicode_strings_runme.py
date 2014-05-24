2533d0210fb2f3b2e9f441c38bd03b99f4a57a44
Harvey Falcic <harvey.falcic@gmail.com>
2014-05-24 16:50:33 -0400

    unicode_strings test: check return values

--- Examples/test-suite/python/unicode_strings_runme.py
+++ Examples/test-suite/python/unicode_strings_runme.py
@@ -2,6 +2,11 @@ import sys
 
 import unicode_strings
 
+# The 'u' string prefix isn't valid in Python 3.0 - 3.2 and is redundant
+# in 3.3+. Since this file is run through 2to3 before testing, though,
+# mark this as a unicode string in 2.x so it'll become a str in 3.x.
+test_string = u'h\udce9llo w\u00f6rld'
+
 if sys.version_info > (3, 0):
-    unicode_strings.non_utf8_c_str()
-    unicode_strings.non_utf8_std_string()
+    assert unicode_strings.non_utf8_c_str() == test_string
+    assert unicode_strings.non_utf8_std_string() == test_string
