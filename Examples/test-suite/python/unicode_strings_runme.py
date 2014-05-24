ab527b0e4bf592e55b038a6de0fb8fe4540eea92
Harvey Falcic <harvey.falcic@gmail.com>
2014-05-24 17:54:53 -0400

    unicode_strings_runme.py: fix version check
    Python 3.0.1 shouldn't pass.

--- Examples/test-suite/python/unicode_strings_runme.py
+++ Examples/test-suite/python/unicode_strings_runme.py
@@ -7,6 +7,6 @@ import unicode_strings
 # mark this as a unicode string in 2.x so it'll become a str in 3.x.
 test_string = u'h\udce9llo w\u00f6rld'
 
-if sys.version_info > (3, 0):
+if sys.version_info[0:2] >= (3, 1):
     assert unicode_strings.non_utf8_c_str() == test_string
     assert unicode_strings.non_utf8_std_string() == test_string
