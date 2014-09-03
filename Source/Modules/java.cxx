e7db081d5e175024f1d5baa9ac028474958a2542
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-09-03 09:37:25 +0300

    Java: fix generation of ProxyName when JNI descriptor is requested - for inner classes '$' should be used as separator instead of '/'

--- Source/Modules/java.cxx
+++ Source/Modules/java.cxx
@@ -209,7 +209,7 @@ public:
 	   String *symname = Copy(Getattr(n, "sym:name"));
 	   if (symname && !GetFlag(n, "feature:flatnested")) {
 	     for (Node *outer_class = Getattr(n, "nested:outer"); outer_class; outer_class = Getattr(outer_class, "nested:outer")) {
-	       Push(symname, ".");
+	       Push(symname, jnidescriptor ? "$" : ".");
 	       Push(symname, Getattr(outer_class, "sym:name"));
 	     }
 	   }
