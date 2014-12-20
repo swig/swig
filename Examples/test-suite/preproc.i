swig/swig

commit 255c929c5636f54c16261bd92b8eea2005d61b11

Issue an error for unknown SWIG preprocessor directives, rather than quietly ignoring them.  Reported by jrhelsey in issue#217.

Source/Preprocessor/cpp.c
@@ -1768,6 +1768,8 @@ String *Preprocessor_parse(String *s) {
 	}
       } else if (Equal(id, kpp_level)) {
 	Swig_error(Getfile(s), Getline(id), "cpp debug: level = %d, startlevel = %d\n", level, start_level);
+      } else {
+	Swig_error(Getfile(s), Getline(id), "Unknown SWIG preprocessor directive: %s\n", id);
       }
       for (i = 0; i < cpp_lines; i++)
 	Putc('\n', ns);

commit 93d58cd3ed1cc2cf0490e95d43eed4e8de62763d

Fix use of preprocessor null directive
This was broken recently in commit 255c929c5636f54c16261bd92b8eea2005d61b11
for issue #217

Source/Preprocessor/cpp.c
@@ -1768,6 +1768,8 @@ String *Preprocessor_parse(String *s) {
 	}
       } else if (Equal(id, kpp_level)) {
 	Swig_error(Getfile(s), Getline(id), "cpp debug: level = %d, startlevel = %d\n", level, start_level);
+      } else if (Equal(id, "")) {
+	/* Null directive */
       } else {
 	Swig_error(Getfile(s), Getline(id), "Unknown SWIG preprocessor directive: %s\n", id);
       }

Examples/test-suite/preproc.i
@@ -346,3 +346,20 @@ int method(struct TypeNameTraits tnt) {
   return tnt.val;
 }
 %}
+
+/* Null directive */
+# /* comment 1 */
+# // comment 2
+# /** comment 3 */
+# /* comment 4 */ /*comment 5*/
+# /** comment 6
+#
+# more comment 6 */
+# 
+#
+#	    
+int methodX(int x);
+%{
+int methodX(int x) { return x+100; }
+%}
+

Examples/test-suite/python/preproc_runme.py
@@ -12,3 +12,5 @@ if preproc.defined != 1:
 if 2*preproc.one != preproc.two:
   raise RuntimeError
 
+if preproc.methodX(99) != 199:
+  raise RuntimeError