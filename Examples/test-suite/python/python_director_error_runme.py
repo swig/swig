449a0d12ecd3d7039145bd2b17ab055f124c12a6
Blaise Gassend <gassend@google.com>
2015-03-11 15:15:16 -0700

    Increased python indentation from 2 to 4 spaces.

--- Examples/test-suite/python/python_director_error_runme.py
+++ Examples/test-suite/python/python_director_error_runme.py
@@ -1,29 +1,29 @@
 import python_director_error
 
 class MyException(Exception):
-  pass
+    pass
 
 go_called_count = 0
 class Derived(python_director_error.Foo):
-  def go(self):
-    global go_called_count
-    go_called_count += 1
-    raise MyException
+    def go(self):
+      global go_called_count
+      go_called_count += 1
+      raise MyException
 
 if go_called_count != 0:
-  raise RuntimeError, "go_called_count before"
+    raise RuntimeError, "go_called_count before"
 if python_director_error.GetExceptionCount() != 0:
-  raise RuntimeError, "GetExceptionCount() before"
+    raise RuntimeError, "GetExceptionCount() before"
 
 f = Derived()
 try:
-  python_director_error.CallGo(f);
-  raise RuntimeError  # Callee should have exited with an exception.
+    python_director_error.CallGo(f);
+    raise RuntimeError  # Callee should have exited with an exception.
 except MyException:
-  pass
+    pass
 
 if go_called_count != 1:
-  raise RuntimeError, "go_called_count after"
+    raise RuntimeError, "go_called_count after"
 if python_director_error.GetExceptionCount() != 1:
-  raise RuntimeError, "GetExceptionCount() after: %i" % python_director_error.GetExceptionCount()
+    raise RuntimeError, "GetExceptionCount() after: %i" % python_director_error.GetExceptionCount()
 
