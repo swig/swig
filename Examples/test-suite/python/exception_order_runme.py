78655fc63a59cc87bc4356a44d82a2aced813738
Yoann Vandoorselaere <yoannv@gmail.com>
2015-04-25 07:28:58 +0200

    Fix SWIG Python builtin custom exception
    When builtin mode is enabled, triggering custom exception object didn't work
    and always lead to SWIG_RuntimeError exception.
    
    We can now enable the exception_order_runme test when using builtin.

--- Examples/test-suite/python/exception_order_runme.py
+++ Examples/test-suite/python/exception_order_runme.py
@@ -1,10 +1,5 @@
 from exception_order import *
 
-# This test is expected to fail with -builtin option.
-# Throwing builtin classes as exceptions not supported
-if is_python_builtin():
-  exit(0)
-
 a = A()
 
 try:
