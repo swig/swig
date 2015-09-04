9e69a2c198ff684b757ce26ca95a3b2112dbc530
Olly Betts <olly@survex.com>
2015-09-04 12:14:21 +1200

    Use name of PHP resource not wrapped C++ type
    Since callback::foo_T<int> isn't a PHP resource, that error message doesn't
    really make sense as it was.  As discussed in #467.

--- Examples/test-suite/php/callback_runme.php
+++ Examples/test-suite/php/callback_runme.php
@@ -3,7 +3,7 @@
 require "tests.php";
 require "callback.php";
 // In 2.0.6 and earlier, the constant was misnamed.
-if (gettype(callback::FOO_I_Cb_Ptr) !== 'resource') die("callback::foo_T<int> not a resource\n");
+if (gettype(callback::FOO_I_Cb_Ptr) !== 'resource') die("callback::FOO_I_Cb_Ptr not a resource\n");
 
 check::done();
 ?>
