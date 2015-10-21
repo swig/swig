cbaf796cdd7709ef92bafcc91813e8bdc0bdf8c4
Richard Beare <richard.beare@monash.edu>
2015-10-21 14:06:28 +1100

    runme files added

--- Examples/test-suite/r/enum_rename_runme.R
+++ Examples/test-suite/r/enum_rename_runme.R
@@ -1,14 +1,7 @@
 clargs <- commandArgs(trailing=TRUE)
 source(file.path(clargs[1], "unittest.R"))
-nn <- "preproc_constants"
+nn <- "enum_rename"
 shared <- paste(nn, .Platform$dynlib.ext, sep="")
 rfile <- paste(nn, ".R", sep="")
 dyn.load(shared)
 source(rfile)
-
-
-v <- enumToInteger('kValue', 'MyEnum')
-print(v)
-# temporarily removed until fixed (in progress, see Github patch #500)
-unittest(v,4)
-q(save="no")
