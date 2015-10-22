15eccd5e7c3b3f171888bded7e4dd84f6de033f6
Richard Beare <richard.beare@monash.edu>
2015-10-22 20:59:55 +1100

    unittest for R allprotected

--- Examples/test-suite/r/allprotected_runme.R
+++ Examples/test-suite/r/allprotected_runme.R
@@ -5,3 +5,13 @@ shared <- paste(nn, .Platform$dynlib.ext, sep="")
 rfile <- paste(nn, ".R", sep="")
 dyn.load(shared)
 source(rfile)
+
+EV1 <- enumToInteger("EnumVal1", "PublicBase_AnEnum")
+EV2 <- enumToInteger("EnumVal2", "PublicBase_AnEnum")
+unittest(EV1, 0)
+unittest(EV2, 1)
+
+EV1 <- enumToInteger("EnumVal1", "ProtectedBase_AnEnum")
+EV2 <- enumToInteger("EnumVal2", "ProtectedBase_AnEnum")
+unittest(EV1, 0)
+unittest(EV2, 1)
