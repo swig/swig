9aafe9648355cbdc8fa9009909e0fef0fbbabfe7
Richard Beare <richard.beare@monash.edu>
2015-10-22 20:56:06 +1100

    unittest for R - abstract_typedef2

--- Examples/test-suite/r/abstract_typedef2_runme.R
+++ Examples/test-suite/r/abstract_typedef2_runme.R
@@ -5,3 +5,10 @@ shared <- paste(nn, .Platform$dynlib.ext, sep="")
 rfile <- paste(nn, ".R", sep="")
 dyn.load(shared)
 source(rfile)
+
+UnaryField <- enumToInteger("UnaryField", "FieldDim")
+BinaryField <- enumToInteger("BinaryField", "FieldDim")
+
+unittest(UnaryField, 0)
+unittest(BinaryField, 1)
+
