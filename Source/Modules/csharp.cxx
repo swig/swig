1458326f4c93de3f3413e4963ff047dc6823e9d8
Johan Gustavsson <pjohangustavsson@hotmail.se>
2016-02-04 19:34:19 +0100

    Update csharp.cxx
    When people use the -namespace option, they often intend for the input to swig to wrap a particular namespace in a single module. This option should also have an effect on how C++ wrapper function names are mangled, and not just the C# proxy code.

--- Source/Modules/csharp.cxx
+++ Source/Modules/csharp.cxx
@@ -415,8 +415,16 @@ public:
     }
 
     Printf(f_runtime, "\n");
+    if (namespce) {
+      String *wrapper_name = NewStringf("");
+      Printf(wrapper_name, "CSharp_%s_%%f", namespce);
+      Swig_name_register("wrapper", wrapper_name);
+      Delete(wrapper_name);
+    }
+    else {
+      Swig_name_register("wrapper", "CSharp_%f");
+    }
 
-    Swig_name_register("wrapper", "CSharp_%f");
     if (old_variable_names) {
       Swig_name_register("set", "set_%n%v");
       Swig_name_register("get", "get_%n%v");
