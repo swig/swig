0075f4fff47f31e2708860c8b725614c221e9093
William S Fulton <wsf@fultondesigns.co.uk>
2016-03-12 12:13:44 +0000

    Scilab warnings as errors now fully turned on in Travis testing

--- Tools/testflags.py
+++ Tools/testflags.py
@@ -47,7 +47,7 @@ def get_cxxflags(language, std, compiler):
         "python":"-Werror " + cxx_common,
              "r":"-Werror " + cxx_common,
           "ruby":"-Werror " + cxx_common,
-        "scilab":             cxx_common,
+        "scilab":"-Werror " + cxx_common,
            "tcl":"-Werror " + cxx_common,
     }
     if compiler == 'clang':
