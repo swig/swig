b062a63cd1a7f2688943c8d2248374c7a9fdb076
Vadim Zeitlin <vz-swig@zeitlins.org>
2016-04-20 23:06:32 +0200

    Fix abstract_typedef C test code
    Don't refer to the typedef name (especially with a typo in it...), it is not
    supposed to be exported.
    
    Also fix the write() method call: use the right class for it and pass it the
    correct arguments.

--- Examples/test-suite/c/abstract_typedef_runme.c
+++ Examples/test-suite/c/abstract_typedef_runme.c
@@ -4,14 +4,11 @@
 
 int main(int argc, const char *argv[]) {
 	Engine *e = new_Engine();
-	PerseEngine *pe = new_PersEngine();
 	A *a = new_A();
 
-	assert(A_write(e) == true);
-	assert(A_write(pe) == true);
+	assert(AbstractBaseClass_write(a, e) == true);
 
 	delete_A(a);
-	delete_PerseEngine(pe);
 	delete_Engine(e);
 
 	return 0;
