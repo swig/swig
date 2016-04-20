af9dd72bb67811cf7eb93fbbc34e8f4c3a41310c
Vadim Zeitlin <vz-swig@zeitlins.org>
2016-04-20 18:49:43 +0200

    Fix function name in cpp_basic_template_function test
    Use "_wrap_" prefix to make the test compile and pass.

--- Examples/test-suite/c/cpp_basic_template_function_runme.c
+++ Examples/test-suite/c/cpp_basic_template_function_runme.c
@@ -2,7 +2,7 @@
 #include "cpp_basic_template_function/cpp_basic_template_function_wrap.h"
 
 int main() {
-    assert(GetMaxInt(3, 5) == 5);
+    assert(_wrap_GetMaxInt(3, 5) == 5);
     
     return 0;
 }
