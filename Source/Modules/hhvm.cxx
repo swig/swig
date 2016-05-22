c72686d0ea867b10295c91c1f2dda7bc86c9928f
Nishant Gupta <nish.gupta01@gmail.com>
2016-05-22 14:08:04 +0530

    Improve skeleton

--- Source/Modules/hhvm.cxx
+++ Source/Modules/hhvm.cxx
@@ -6,6 +6,7 @@ protected:
   File *f_runtime;
   File *f_header;
   File *f_wrappers;
+  File *f_register;
   File *f_init;
 
 public:
@@ -21,6 +22,7 @@ public:
   virtual int top(Node *n) {
     /* Get the module name */
     String *module = Getattr(n,"name");
+    String *cap_module = NewStringf("%(upper)s", module);
 
     /* Get the output file name */
     String *outfile = Getattr(n,"outfile");
@@ -35,34 +37,61 @@ public:
     f_init = NewString("");
     f_header = NewString("");
     f_wrappers = NewString("");
+    f_register = NewString("");
 
-     /* Register file targets with the SWIG file handler */
-     Swig_register_filebyname("begin", f_begin);
-     Swig_register_filebyname("header", f_header);
-     Swig_register_filebyname("wrapper", f_wrappers);
-     Swig_register_filebyname("runtime", f_runtime);
-     Swig_register_filebyname("init", f_init);
+    /* Register file targets with the SWIG file handler */
+    Swig_register_filebyname("begin", f_begin);
+    Swig_register_filebyname("header", f_header);
+    Swig_register_filebyname("wrapper", f_wrappers);
+    Swig_register_filebyname("runtime", f_runtime);
+    Swig_register_filebyname("init", f_init);
 
-     /* Output module initialization code */
-     Swig_banner(f_begin);
-     //...
+    /* Output module initialization code */
+    Swig_banner(f_header);
 
-     /* Emit code for children */
-     Language::top(n);
+    Printf(f_header, "\n");
+    Printf(f_header, "#include \"hphp/runtime/ext/extension.h\"\n");
+    Printf(f_header, "#include \"hphp/runtime/base/execution-context.h\"\n");
+    Printf(f_header, "#include \"hphp/runtime/vm/native-data.h\"\n");
+    Printf(f_header, "\n");
+    Printf(f_header, "namespace HPHP {\n");
+    Printf(f_header, "\n");
 
-     //...
-     /* Write all to the file */
-     Dump(f_runtime, f_begin);
-     Dump(f_header, f_begin);
-     Dump(f_wrappers, f_begin);
-     Wrapper_pretty_print(f_init, f_begin);
+    /* Emit code for children */
+    Language::top(n);
+
+    /* module extension class declaration */
+    Printf(f_register, "class %sExtension : public Extension {\n", cap_module);
+    Printf(f_register, "public:\n");
+
+    /* TODO: Take extension version as input */ 
+    Printf(f_register, "  %sExtension(): Extension(\"%s\", \"1.0\") {}\n\n", cap_module, module);
+    Printf(f_register, "  void moduleInit() override {\n");
+    
+    /* All registrations go here */
+
+    Printf(f_register, "    loadSystemlib();\n");
+    Printf(f_register, "  }\n");
+    Printf(f_register, "} s_%s_extension;\n\n", module);
+    Printf(f_register, "HHVM_GET_MODULE(%s);\n\n", module);
+
+    /* close namespace HPHP */
+    Printf(f_register, "}");
+
+    /* Write all to the file */
+    Dump(f_runtime, f_begin);
+    Dump(f_header, f_begin);
+    Dump(f_wrappers, f_begin);
+    Dump(f_register, f_begin);
+    Wrapper_pretty_print(f_init, f_begin);
 
      /* Cleanup files */
-     Delete(f_runtime);
-     Delete(f_header);
-     Delete(f_wrappers);
-     Delete(f_init);
-     Delete(f_begin);
+    Delete(f_runtime);
+    Delete(f_header);
+    Delete(f_wrappers);
+    Delete(f_register);
+    Delete(f_init);
+    Delete(f_begin);
 
     return SWIG_OK;
   }
@@ -80,7 +109,6 @@ public:
     Printf(f_wrappers,"           action : %s\n", action);
     return SWIG_OK;
   }
-
 };
 
 extern "C" Language *
