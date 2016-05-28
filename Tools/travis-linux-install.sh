1f0acadcf0b6c8a5a8c9a02ab1716b4441a3261a
William S Fulton <wsf@fultondesigns.co.uk>
2016-05-28 17:46:47 +0100

    Add gcc-6 Travis testing and use gcc-5.3 instead of gcc-5.2
    Test Python, Java and C# with gcc-6
    Test gcc-5 on Trusty instead of on Precise to pick up gcc-5.3 instead of gcc-5.2

--- Tools/travis-linux-install.sh
+++ Tools/travis-linux-install.sh
@@ -7,12 +7,15 @@ sudo apt-get -qq update
 
 if [[ "$CC" == gcc-5 ]]; then
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-	sudo add-apt-repository -y ppa:boost-latest/ppa
 	sudo apt-get -qq update
-	sudo apt-get install -qq g++-5 libboost1.55-dev
-else
-	sudo apt-get -qq install libboost-dev
+	sudo apt-get install -qq g++-5
+elif [[ "$CC" == gcc-6 ]]; then
+	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+	sudo apt-get -qq update
+	sudo apt-get install -qq g++-6
 fi
+
+sudo apt-get -qq install libboost-dev
 
 WITHLANG=$SWIGLANG
 
