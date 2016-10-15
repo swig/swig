58461c1ffddc90d9b409cc9d83fbe73764adc325
William S Fulton <wsf@fultondesigns.co.uk>
2016-10-15 17:56:02 +0100

    php run test fixes for new functions in testcase

--- Examples/test-suite/php/cpp_static_runme.php
+++ Examples/test-suite/php/cpp_static_runme.php
@@ -4,11 +4,11 @@ require "tests.php";
 require "cpp_static.php";
 
 // New functions
-check::functions(array(staticfunctiontest_static_func,staticfunctiontest_static_func_2,staticfunctiontest_static_func_3));
+check::functions(array(staticfunctiontest_static_func,staticfunctiontest_static_func_2,staticfunctiontest_static_func_3,is_python_builtin));
 // New classes
-check::classes(array(StaticMemberTest,StaticFunctionTest));
+check::classes(array(StaticMemberTest,StaticFunctionTest,cpp_static,StaticBase,StaticDerived));
 // New vars
-check::globals(array(staticmembertest_static_int));
+check::globals(array(staticmembertest_static_int,staticbase_statty,staticderived_statty));
 
 check::done();
 ?>
