87ae580d7595367b8ab8e3a9f62c0dbe5e0ad267
Vadim Zeitlin <vz-swig@zeitlins.org>
2017-02-01 01:38:43 +0100

    Get rid of unnecessary "typedef enum" in C++ code
    No real changes, just remove this C-ism from C++ DoxygenParser class code.

--- Source/DoxygenTranslator/src/DoxygenParser.h
+++ Source/DoxygenTranslator/src/DoxygenParser.h
@@ -24,7 +24,7 @@
 class DoxygenParser {
 private:
 
-  typedef enum {
+  enum DoxyCommandEnum {
     NONE = -1,
     SIMPLECOMMAND,
     COMMANDWORD,
@@ -44,7 +44,7 @@ private:
     PARAGRAPH_END,
     PLAINSTRING,
     COMMAND
-  } DoxyCommandEnum;
+  };
 
 
   /** This class contains parts of Doxygen comment as a token. */
