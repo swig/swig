6398614c9278db40d41dca630a672bcb39835276
William S Fulton <wsf@fultondesigns.co.uk>
2017-04-20 07:57:48 +0100

    Update ruby tests for change in swig_assert_equal_simple name

--- Examples/test-suite/ruby/cpp11_shared_ptr_upcast_runme.rb
+++ Examples/test-suite/ruby/cpp11_shared_ptr_upcast_runme.rb
@@ -5,56 +5,56 @@ require 'cpp11_shared_ptr_upcast'
 include Cpp11_shared_ptr_upcast
 
 # non-overloaded
-simple_assert_equal( 7, derived_num1(Derived.new(7)) )
-simple_assert_equal( 7, derived_num2([Derived.new(7)]) )
-simple_assert_equal( 7, derived_num3({0 => Derived.new(7)}) )
+swig_assert_equal_simple( 7, derived_num1(Derived.new(7)) )
+swig_assert_equal_simple( 7, derived_num2([Derived.new(7)]) )
+swig_assert_equal_simple( 7, derived_num3({0 => Derived.new(7)}) )
 
-simple_assert_equal(-1, base_num1(Derived.new(7)) )
-simple_assert_equal(-1, base_num2([Derived.new(7)]) )
-simple_assert_equal(-1, base_num3({0 => Derived.new(7)}) )
+swig_assert_equal_simple(-1, base_num1(Derived.new(7)) )
+swig_assert_equal_simple(-1, base_num2([Derived.new(7)]) )
+swig_assert_equal_simple(-1, base_num3({0 => Derived.new(7)}) )
 
-simple_assert_equal( 999, derived_num1(nil) )
-simple_assert_equal( 999, derived_num2([nil]) )
-simple_assert_equal( 999, derived_num3({0 => nil}) )
+swig_assert_equal_simple( 999, derived_num1(nil) )
+swig_assert_equal_simple( 999, derived_num2([nil]) )
+swig_assert_equal_simple( 999, derived_num3({0 => nil}) )
 
-simple_assert_equal( 999, base_num1(nil) )
-simple_assert_equal( 999, base_num2([nil]) )
-simple_assert_equal( 999, base_num3({0 => nil}) )
+swig_assert_equal_simple( 999, base_num1(nil) )
+swig_assert_equal_simple( 999, base_num2([nil]) )
+swig_assert_equal_simple( 999, base_num3({0 => nil}) )
 
 # overloaded
-simple_assert_equal( 7, derived_num(Derived.new(7)) )
-simple_assert_equal( 7, derived_num([Derived.new(7)]) )
-simple_assert_equal( 7, derived_num({0 => Derived.new(7)}) )
+swig_assert_equal_simple( 7, derived_num(Derived.new(7)) )
+swig_assert_equal_simple( 7, derived_num([Derived.new(7)]) )
+swig_assert_equal_simple( 7, derived_num({0 => Derived.new(7)}) )
 
-simple_assert_equal(-1, base_num(Derived.new(7)) )
-simple_assert_equal(-1, base_num([Derived.new(7)]) )
-simple_assert_equal(-1, base_num({0 => Derived.new(7)}) )
+swig_assert_equal_simple(-1, base_num(Derived.new(7)) )
+swig_assert_equal_simple(-1, base_num([Derived.new(7)]) )
+swig_assert_equal_simple(-1, base_num({0 => Derived.new(7)}) )
 
 # ptr to shared_ptr
-simple_assert_equal( 7, derived2_num1(Derived2.new(7)) )
-simple_assert_equal( 7, derived2_num2([Derived2.new(7)]) )
-simple_assert_equal( 7, derived2_num3({0 => Derived2.new(7)}) )
+swig_assert_equal_simple( 7, derived2_num1(Derived2.new(7)) )
+swig_assert_equal_simple( 7, derived2_num2([Derived2.new(7)]) )
+swig_assert_equal_simple( 7, derived2_num3({0 => Derived2.new(7)}) )
 
-simple_assert_equal( -1, base2_num1(Derived2.new(7)) )
+swig_assert_equal_simple( -1, base2_num1(Derived2.new(7)) )
 
 begin
     # Upcast for pointers to shared_ptr in this generic framework has not been implemented
-    simple_assert_equal( -1, base2_num2([Derived2.new(7)]) )
+    swig_assert_equal_simple( -1, base2_num2([Derived2.new(7)]) )
     raise RuntimeError, "Failed to catch TypeError"
 rescue TypeError
 end
 begin
     # Upcast for pointers to shared_ptr in this generic framework has not been implemented
-    simple_assert_equal( -1, base2_num3({0 => Derived2.new(7)}) )
+    swig_assert_equal_simple( -1, base2_num3({0 => Derived2.new(7)}) )
     raise RuntimeError, "Failed to catch TypeError"
 rescue TypeError
 end
 
-simple_assert_equal( 888, derived2_num1(nil) )
-simple_assert_equal( 999, derived2_num2([nil]) ) # although 888 would be more consistent
-simple_assert_equal( 999, derived2_num3({0 => nil}) ) # although 888 would be more consistent
+swig_assert_equal_simple( 888, derived2_num1(nil) )
+swig_assert_equal_simple( 999, derived2_num2([nil]) ) # although 888 would be more consistent
+swig_assert_equal_simple( 999, derived2_num3({0 => nil}) ) # although 888 would be more consistent
 
-simple_assert_equal( 888, base2_num1(nil) )
-simple_assert_equal( 999, base2_num2([nil]) ) # although 888 would be more consistent
-simple_assert_equal( 999, base2_num3({0 => nil}) ) # although 888 would be more consistent
+swig_assert_equal_simple( 888, base2_num1(nil) )
+swig_assert_equal_simple( 999, base2_num2([nil]) ) # although 888 would be more consistent
+swig_assert_equal_simple( 999, base2_num3({0 => nil}) ) # although 888 would be more consistent
 
