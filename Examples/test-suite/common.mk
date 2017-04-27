b4efa7b16e4a8cdb28a28ccecb9e77179673665e
Christophe Duvernois <christophe.duvernois@gmail.com>
2017-04-27 23:35:05 +0200

    #526 : propagate c++11 noexcept to director classes test case

--- Examples/test-suite/common.mk
+++ Examples/test-suite/common.mk
@@ -543,6 +543,7 @@ CPP11_TEST_CASES += \
 	cpp11_default_delete \
 	cpp11_delegating_constructors \
 	cpp11_director_enums \
+	cpp11_directors \
 	cpp11_explicit_conversion_operators \
 	cpp11_final_override \
 	cpp11_function_objects \
