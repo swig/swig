8cc1300fde3b1c266eb34bce6ae04570fe94671e
Florian Märkl <info@florianmaerkl.de>
2017-07-22 20:25:40 +0200

    Kotlin/Native simple function wrappers

--- Source/Modules/kotlin_native.cxx
+++ Source/Modules/kotlin_native.cxx
@@ -27,6 +27,9 @@ protected:
   File *f_wrappers;
   File *f_init;
 
+  File *f_begin_h;
+  File *f_wrappers_h;
+
 public:
   KOTLIN_NATIVE() {
   }
@@ -41,6 +44,7 @@ public:
   virtual int top(Node *n) {
     // Initialize all of the output files
     String *outfile = Getattr(n, "outfile");
+    String *outfile_h = Getattr(n, "outfile_h");
 
     f_begin = NewFile(outfile, "w", SWIG_output_files());
     if (!f_begin) {
@@ -48,10 +52,19 @@ public:
       SWIG_exit(EXIT_FAILURE);
     }
 
+    f_begin_h = NewFile(outfile_h, "w", SWIG_output_files());
+    if (!f_begin_h) {
+      FileErrorDisplay(outfile_h);
+      SWIG_exit(EXIT_FAILURE);
+    }
+
+
     f_runtime = NewString("");
     f_init = NewString("");
     f_header = NewString("");
     f_wrappers = NewString("");
+
+    f_wrappers_h = NewString("");
 
     // Register file targets with the SWIG file handler
     Swig_register_filebyname("begin", f_begin);
@@ -60,19 +73,30 @@ public:
     Swig_register_filebyname("runtime", f_runtime);
     Swig_register_filebyname("init", f_init);
 
-    // Output module initialization code
-    Swig_banner(f_begin);
+    //Swig_register_filebyname("wrapper_h", f_wrappers_h);
 
+
+    Swig_banner(f_begin);
     Printf(f_runtime, "\n\n#ifndef SWIGKOTLINNATIVE\n#define SWIGKOTLINNATIVE\n#endif\n\n");
+
+    Swig_banner(f_begin_h);
+
+    // TODO: is there any standard way to generate include guards in swig?
+    Printf(f_begin_h, "\n\n#pragma once\n\n");
+
+
 
     // emit code
     Language::top(n);
 
-    // Write all to the file
+
+    // Write all to the files
     Dump(f_runtime, f_begin);
     Dump(f_header, f_begin);
     Dump(f_wrappers, f_begin);
     Wrapper_pretty_print(f_init, f_begin);
+
+    Dump(f_wrappers_h, f_begin_h);
 
     // Cleanup files
     Delete(f_runtime);
@@ -81,9 +105,36 @@ public:
     Delete(f_init);
     Delete(f_begin);
 
+    Delete(f_wrappers_h);
+    Delete(f_begin_h);
+
     return SWIG_OK;
   }
 
+
+  virtual int functionWrapper(Node *n) {
+    String   *name   = Getattr(n, "sym:name");
+    SwigType *type   = Getattr(n, "type");
+    ParmList *parms  = Getattr(n, "parms");
+
+    Wrapper *wrapper = NewWrapper();
+    String *wname = Swig_name_wrapper(name);
+    String *parm_str = ParmList_str(parms);
+
+    Printv(wrapper->def, type, " ", wname, "(", parm_str, ") {", NIL);
+    Printv(wrapper->code, "return ALL_OK;", NIL);
+    Printv(wrapper->code, "}", NIL);
+
+    Wrapper_print(wrapper, f_wrappers);
+    DelWrapper(wrapper);
+
+    Printv(f_wrappers_h, type, " ", wname, "(", parm_str, ");\n", NIL);
+
+    Delete(wname);
+    Delete(parm_str);
+
+    return SWIG_OK;
+  }
 };
 
 
