8b286bbca80a24c3bf3ad90bd670d6a9cd31e548
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-11-06 16:52:56 +0100

    Adding %matlabcode_static, %matlabcode_class

--- Source/Modules/matlab.cxx
+++ Source/Modules/matlab.cxx
@@ -74,6 +74,7 @@ protected:
   String *get_field;
   String *set_field;
   String *static_methods;
+  String *wrap_class_m;
 
   // Current constant
   int con_id;
@@ -155,6 +156,7 @@ MATLAB::MATLAB():
   get_field(0),
   set_field(0),
   static_methods(0),
+  wrap_class_m(0),
   have_constructor(false),
   have_destructor(false),
   num_gateway(0),
@@ -2075,6 +2077,9 @@ int MATLAB::classHandler(Node *n) {
     SWIG_exit(EXIT_FAILURE);
   }
 
+  // User class instructions
+  wrap_class_m = NewString("");
+
   // Declare MATLAB class
   Printf(f_wrap_m, "classdef %s < ", Getattr(n, "sym:name"));
 
@@ -2164,6 +2169,9 @@ int MATLAB::classHandler(Node *n) {
   Printf(f_wrap_m, "%s", static_methods);
   Printf(f_wrap_m, "  end\n");
 
+  // Dump user defined class instructions
+  Printf(f_wrap_m, "%s", wrap_class_m);
+
   // Finalize file
   Printf(f_wrap_m, "end\n");
 
@@ -2172,6 +2180,8 @@ int MATLAB::classHandler(Node *n) {
   base_init = 0;
   Delete(f_wrap_m);
   f_wrap_m = 0;
+  Delete(wrap_class_m);
+  wrap_class_m = 0;
   //note: don't Delete class_name as it's not a new object
   class_name = 0;
   Delete(mfile);
@@ -2609,6 +2619,10 @@ int MATLAB::insertDirective(Node *n) {
 
   if (Cmp(section, "matlab") == 0 && f_wrap_m) {
     Printv(f_wrap_m, code, NIL);
+  } else if (Cmp(section, "matlab_static") == 0 && static_methods) {
+    Printv(static_methods, code, NIL);
+  } else if (Cmp(section, "matlab_class") == 0 && wrap_class_m) {
+    Printv(wrap_class_m, code, NIL);
   } else if (Cmp(section, "matlabsetup") == 0) {
     Printv(f_setup_m, code, NIL);
   } else {
