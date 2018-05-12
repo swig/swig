a3f73e66381dc0090a60295a956fc24cc1bf596c
Ian Rogers <irogers@google.com>
2018-05-11 22:43:56 -0400

    [Java] Warn when finalizers aren't generated.
    A common bug pattern is to %ignoreall and then to forget to %unignore a
    destructor. Without a finalizer the delete method isn't called and so
    such bugs can be the source of memory leaks without runtime exceptions.
    Warn when a finalizer isn't generated so that such memory leaks are easy
    to avoid.

--- Source/Modules/java.cxx
+++ Source/Modules/java.cxx
@@ -2072,6 +2072,15 @@ public:
       // Finalize method
       if (*Char(destructor_call)) {
 	Printv(proxy_class_def, typemapLookup(n, "javafinalize", typemap_lookup_type, WARN_NONE), NIL);
+      } else {
+        // Warn when no finalizer is created and no javafinalize is provided
+        if (typemapLookup(n, "javafinalize", typemap_lookup_type, WARN_NONE) ==
+            0) {
+          Swig_warning(
+              WARN_JAVA_HIDDEN_DESTRUCTOR, Getfile(n), Getline(n),
+              "Skipping Java finalizer as C++ destructor does not have "
+              "public access");
+        }
       }
       // delete method
       Printv(destruct, tm, NIL);
