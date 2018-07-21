bb73a1ce5254b92d7419f4131c9de5dc8fc608f0
furylynx <furylynx@users.noreply.github.com>
2018-07-21 21:19:28 +0200

    fixed nvm

--- Tools/travis-linux-install.sh
+++ Tools/travis-linux-install.sh
@@ -40,9 +40,11 @@ case "$SWIGLANG" in
 				if [[ -z "$VER" ]]; then
 					travis_retry sudo apt-get install -qq nodejs node-gyp
 				else
-					travis_retry wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
-					travis_retry sudo nvm install ${VER}
-					sudo nvm use ${VER}
+					travis_retry wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.10/install.sh | bash
+					export NVM_DIR="$HOME/.nvm"
+					[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+					travis_retry nvm install ${VER}
+					nvm use ${VER}
 					travis_retry npm install -g node-gyp
 				fi
 				;;
