1cbabb2e06a2937d4d7b5f50e28361467c134994
furylynx <furylynx@users.noreply.github.com>
2018-07-21 16:17:54 +0200

    travis tests for different node versions

--- Tools/travis-linux-install.sh
+++ Tools/travis-linux-install.sh
@@ -37,8 +37,9 @@ case "$SWIGLANG" in
 	"javascript")
 		case "$ENGINE" in
 			"node")
-				travis_retry curl -sL https://deb.nodesource.com/setup_${VER}.x | sudo -E bash -
-				travis_retry sudo apt-get install -y nodejs
+				travis_retry wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
+				travis_retry sudo nvm install ${VER}
+				sudo nvm use ${VER}
 				travis_retry npm install -g node-gyp
 				;;
 			"jsc")
