dcbccc6f6f83fa41484b99c4de1fb9df514a4dab
William S Fulton <wsf@fultondesigns.co.uk>
2018-10-09 22:43:19 +0100

    Test non-default compare template argument in std::map

--- Examples/test-suite/python/li_std_map_runme.py
+++ Examples/test-suite/python/li_std_map_runme.py
@@ -67,3 +67,15 @@ if [i for i in mii.itervalues()] != [2]:
     raise RuntimeError("itervalues")
 if [i for i in mii.iteritems()] != [(1, 2)]:
     raise RuntimeError("iteritems")
+
+
+slmap = li_std_map.StringLengthNumberMap()
+li_std_map.populate(slmap)
+
+keys = " ".join([k for k in slmap.keys()])
+if keys != "a aa zzz xxxx aaaaa":
+    raise RuntimeError("Keys are wrong or in wrong order: " + keys)
+
+values = " ".join([str(v) for v in slmap.values()])
+if values != "1 2 3 4 5":
+    raise RuntimeError("Values are wrong or in wrong order: " + values)
