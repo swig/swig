6e2b54be2f127ac03b5c23557bfd0037f362b46c
William S Fulton <wsf@fultondesigns.co.uk>
2018-12-20 08:05:54 +0000

    Testcase for testing __new__ override Python 3.6 fix
    Issue #1357

--- Examples/test-suite/python/python_pythoncode_runme.py
+++ Examples/test-suite/python/python_pythoncode_runme.py
@@ -3,3 +3,7 @@ import python_pythoncode
 # No need to actually do anything, this is a regression test for a bug which
 # caused an invalid python_pythoncode.py to be generated, so if we can import
 # it the bug is still fixed.
+
+# A later test enhancement checking __new__ requires some code...
+f = python_pythoncode.get_foo()
+f = python_pythoncode.Foo()
