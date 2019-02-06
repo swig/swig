f6da155fda1d86660dc65e3ea65e8e88da0c5a6c
William S Fulton <wsf@fultondesigns.co.uk>
2019-02-06 22:55:15 +0000

    Fix mzscheme simple example

--- Examples/mzscheme/simple/runme.scm
+++ Examples/mzscheme/simple/runme.scm
@@ -4,7 +4,7 @@
 
 (display (get-time))
 
-(printf "My-variable = ~a~n" (my-variable))
+(printf "My-variable = ~a~n" (My-variable))
 
 (let loop ((i 0))
   (when (< i 14) (begin (display i)
@@ -17,8 +17,8 @@
   (when (< i 250)
 	(begin
 	  (let loopi ((j 1))
-	    (when (< j 250) (begin (my-variable (+ (my-variable) (mod i j)))
+	    (when (< j 250) (begin (My-variable (+ (My-variable) (mod i j)))
 				   (loopi (+ j 1)))))
 	  (loop (+ i 1)))))
 
-(printf "My-variable = ~a~n" (my-variable))
+(printf "My-variable = ~a~n" (My-variable))
