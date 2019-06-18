ed0d574f40d5014cd802dcdb6796c0df8253c98c
Seth R Johnson <johnsonsr@ornl.gov>
2019-06-18 19:38:23 -0400

    Fix/reduce functionality of bindc

--- Examples/test-suite/fortran/fortran_bindc_runme.F90
+++ Examples/test-suite/fortran/fortran_bindc_runme.F90
@@ -28,30 +28,11 @@ subroutine test_arrays
   use fortran_bindc
   use ISO_C_BINDING
   implicit none
-  type(XY), allocatable :: globalxyarr(:)
   type(XYArray) :: xyarr
-  type(XYArray) :: xyarr_ptr
   type(XY) :: tempxy
   integer :: i
   integer(c_short) :: short_array(5) = [ 5_c_short, 40_c_short, 300_c_short, 2000_c_short, 10000_c_short ]
   integer(c_short) :: summed
-
-  ! Get a copy of the global xy array
-  globalxyarr = get_globalXYArray()
-  ASSERT(size(globalxyarr) == 3)
-  do i = 1,3
-    globalxyarr(i)%x = i * 2
-    globalxyarr(i)%y = i * 2 + 1
-  end do
-  write(0,*) "Global values:", globalxyarr
-
-  ! Get a C pointer to the global xy array using a static method
-  xyarr_ptr = xyarr_ptr%frompointer(globalxyarr(1))
-
-  ! Check the values: 0 C indexing => 1 Fortran indexing
-  tempxy = xyarr_ptr%getitem(0)
-  ASSERT(tempxy%x == 2)
-  ASSERT(tempxy%y == 3)
 
   ! Allocate a new C array
   xyarr = XYArray(3);
@@ -60,15 +41,6 @@ subroutine test_arrays
     tempxy%y = 10 * i + 1
     call xyarr%setitem(i - 1, tempxy)
   end do
-
-  if (is_cplusplus == 0) then
-    ! C assignment: copy xyarr to the object pointed to by xyarr_ptr (i.e. the global array) . This works because the default assingnment traits do a memcpy,
-    ! but the C++ class lacks a copy assignment operator.
-    xyarr_ptr = xyarr
-    
-    ASSERT(globalxyarr(1)%x == 10)
-    ASSERT(globalxyarr(1)%y == 11)
-  end if
 
   ! Sum a fortran array using a bound(C) function
   summed = sum_array(short_array)
