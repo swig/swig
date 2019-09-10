a845683c489290f2ff5fc6f2a916939fe1a93a15
Seth R Johnson <johnsonsr@ornl.gov>
2019-09-10 10:39:56 -0400

    Revert "Add "const" qualifier to "self" in member get"
    This reverts commit 2b5545c73477d7ff280a0a777591b8db1dff9ced.

--- Source/Swig/cwrap.c
+++ Source/Swig/cwrap.c
@@ -1496,7 +1496,6 @@ int Swig_MembergetToFunction(Node *n, String *classname, int flags) {
   type = Getattr(n, "type");
 
   t = NewString(classname);
-  SwigType_add_qualifier(t, "const");
   SwigType_add_pointer(t);
   parms = NewParm(t, "self", n);
   Setattr(parms, "self", "1");
