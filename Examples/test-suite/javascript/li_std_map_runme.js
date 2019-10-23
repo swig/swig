f5d5086c21252198b21897eadda82564e63a522e
Vadim Zeitlin <vz-swig@zeitlins.org>
2019-10-23 18:08:02 +0200

    Add a simple test for std::map<> in JavaScript backend
    Verify that basic operations work as intended.

---
+++ Examples/test-suite/javascript/li_std_map_runme.js
@@ -0,0 +1,36 @@
+var li_std_map = require("li_std_map");
+
+var collectionSize = 20;
+var midCollection = collectionSize / 2;
+
+// Set up an string to int map
+var simap = new li_std_map.StringIntMap();
+for (var i = 0; i < collectionSize; i++) {
+    var val = i * 18;
+    simap.set(i.toString(), val);
+}
+
+if (simap.size() != collectionSize)
+    throw "Size test failed";
+
+// Item indexing test
+simap.set("0", 200);
+if (simap.get("0") != 200)
+    throw "Item property test failed";
+simap.set("0", 0 * 18);
+
+// Key presence test
+for (var i = 0; i < collectionSize; i++) {
+    if (!simap.has_key(i.toString()))
+        throw "has_key test " + i + " failed";
+}
+
+// Delete test
+simap.del("7");
+if (simap.has_key("7"))
+    throw "Delete test failed";
+
+// Clear test
+simap.clear();
+if (simap.size() != 0)
+    throw "Clear test failed";
