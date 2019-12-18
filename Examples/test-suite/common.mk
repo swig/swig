b52af4039856cce3dc924d970572350b85f2652d
Vadim Zeitlin <vz-swig@zeitlins.org>
2019-12-18 16:24:28 +0100

    Disable Doxygen tests when using Java 8 or older
    Check Java version in configure and define SKIP_DOXYGEN_TEST_CASES if
    it's less than 9, which is required by the new implementation of
    CommentParser used in the Doxygen tests.

--- Examples/test-suite/common.mk
+++ Examples/test-suite/common.mk
@@ -610,11 +610,14 @@ CPP11_TEST_BROKEN = \
 #	cpp11_reference_wrapper \     # No typemaps
 
 # Doxygen support test cases: can only be used with languages supporting
-# Doxygen comment translation, currently only Python and Java.
+# Doxygen comment translation (currently Python and Java) and only if not
+# disabled by configure via SKIP_DOXYGEN_TEST_CASES.
+ifneq ($(SKIP_DOXYGEN_TEST_CASES),1)
 python_HAS_DOXYGEN := 1
 java_HAS_DOXYGEN := 1
 
 $(eval HAS_DOXYGEN := $($(LANGUAGE)_HAS_DOXYGEN))
+endif
 
 ifdef HAS_DOXYGEN
 DOXYGEN_TEST_CASES += \
