6f628e0fa9e2659f5a7ca08b9954e81d4cd4f012
goto40 <pierre.bayerl@googlemail.com>
2021-03-14 12:56:41 +0100

    experiments

--- Tests/DOH/basic_tests.cxx
+++ Tests/DOH/basic_tests.cxx
@@ -1,16 +1,30 @@
 #include "catch.hpp"
+#include <string>
 extern "C" {
     #include "doh.h"
 }
 
-TEST_CASE( "Strings", "[DOH]" ) {
-    GIVEN( "A C-string with 'Hello World'" ) {
-        DOHconst_String_or_char_ptr text = "Hello World";
-        WHEN( "An DOH object is created from the string" ) {
-            DOH* obj = DohStr(text);
-            THEN( "The obj does not start with the first char of the string" ) {
-                CHECK( *reinterpret_cast<const char*>(obj) != *reinterpret_cast<const char*>(text));
-            } 
-        }
-    }
+TEST_CASE( "First_Steps", "[DOH]" ) {
+    DOHconst_String_or_char_ptr text = "Hello World";
+    DOH* obj = DohStr(text);
+
+    // compare sizof(int) bytes of text and obj:
+    // (must be different)
+    CHECK(
+        reinterpret_cast<const int*>(obj)[0] !=
+        reinterpret_cast<const int*>(text)[0]
+    );
+
+    // use DohData to get raw string info back
+    CHECK(
+        std::string(reinterpret_cast<const char*>(DohData(obj))) ==
+        std::string(reinterpret_cast<const char*>(text))
+    );
+
+    // however the obj has its own memory
+    CHECK(
+        reinterpret_cast<const char*>(DohData(obj)) !=
+        reinterpret_cast<const char*>(text)
+    );
+
 }
