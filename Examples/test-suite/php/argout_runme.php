451998f27ba9a89d1b2d9aadfecf914f8b55afb3
Olly Betts <olly@survex.com>
2021-04-14 17:05:19 +1200

    Fix in typemap for void** and void*&
    Mark the parameter as "byref" and write back through the reference
    after the call.
    
    Adjust testcase argout to uncomment the part that's meant to test this,
    and to remove lingering traces of PHP's old call-time pass-by-reference
    (which was completely removed before PHP 7).
    
    Fixes #1457

--- Examples/test-suite/php/argout_runme.php
+++ Examples/test-suite/php/argout_runme.php
@@ -22,16 +22,10 @@ check::equal(4,inctr($tr),"4==incr($tr)");
 check::equal(5,intp_value($tr),"5==$tr");
 
 # Check the voidhandle call, first with null
-unset($handle);
-# FIXME: Call-time pass-by-reference has been deprecated for ages, and was
-# removed in PHP 5.4.  We need to rework 
-#voidhandle(&$handle);
-#check::resource($handle,"_p_void",'$handle is not _p_void');
-#$handledata=handle($handle);
-#check::equal($handledata,"Here it is","\$handledata != \"Here it is\"");
-
 $handle=NULL;
 voidhandle($handle);
-check::isnull($handle,'$handle not null');
+check::resource($handle,"_p_void",'$handle is not _p_void');
+$handledata=handle($handle);
+check::equal($handledata,"Here it is","\$handledata != \"Here it is\"");
 
 check::done();
