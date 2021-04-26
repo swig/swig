2b3522fc96f721b2f576b8f4379899ac1c7155e0
William S Fulton <wsf@fultondesigns.co.uk>
2021-04-26 20:44:40 +0100

    Javascript node install on Travis
    node-gyp 8 is now installed by default, but doesn't work with older Javascript versions

--- Tools/travis-linux-install.sh
+++ Tools/travis-linux-install.sh
@@ -44,6 +44,8 @@ case "$SWIGLANG" in
 					travis_retry npm install -g node-gyp@$VER
 				elif [ "$VER" == "8" ] ; then
 					travis_retry npm install -g node-gyp@6
+				elif [ "$VER" == "10" ] || [ "$VER" == "12" ] || [ "$VER" == "14" ] ; then
+					travis_retry npm install -g node-gyp@7
 				else
 					travis_retry npm install -g node-gyp
 				fi
