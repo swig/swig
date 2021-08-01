901f399f8c60bf1a05e14c3b1a9de5c0e601d4dc
Kris Thielemans <k.thielemans@ucl.ac.uk>
2021-08-01 16:49:08 +0100

    [CI] generalise travis-linux-install.sh
    - move content to CI-linux-install.sh which is independent of Travis
    - create equivalent for Github Actions (GHA)

--- Tools/GHA-linux-install.sh
+++ Tools/GHA-linux-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Install Linux packages where the version has been overidden in .travis.yml
-
-lsb_release -a
+#lsb_release -a
 # find location of current script (only works in bash)
 script_dir="$( dirname "${BASH_SOURCE[0]}")"
 
 # run generic script
-RETRY=travis-retry
+RETRY=
 source "$script_dir"/CI-linux-install.sh
