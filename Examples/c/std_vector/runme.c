d2e3cce7d4bb75c58f2d33b32e57964a42d215df
Vadim Zeitlin <vz-swig@zeitlins.org>
2021-10-05 00:57:45 +0200

    Avoid -Wformat warnings in C std_vector example
    Use "%zd" for printing size_t values rather than "%d".

--- Examples/c/std_vector/runme.c
+++ Examples/c/std_vector/runme.c
@@ -8,20 +8,20 @@ int main() {
   VA *va = Klass_va_get(klass);
 
   printf("Vector of ints:\n");
-  printf("size=%d\ncapacity=%d\n\n", Vint_size(vint), Vint_capacity(vint));
+  printf("size=%zd\ncapacity=%zd\n\n", Vint_size(vint), Vint_capacity(vint));
   
   int i;
   for (i = 0; i < 10; i++)
     Vint_push_back(vint, i*i);
   
-  printf("size=%d\ncapacity=%d\n\n", Vint_size(vint), Vint_capacity(vint));
+  printf("size=%zd\ncapacity=%zd\n\n", Vint_size(vint), Vint_capacity(vint));
   
   for (i = 0; i < Vint_size(vint); i++)
     printf("%d%c", Vint_get(vint, i), i+1 == Vint_size(vint) ? '\n' : ',');
   
   Vint_clear(vint); 
   Vint_reserve(vint, 100);
-  printf("\nsize=%d\ncapacity=%d\n", Vint_size(vint), Vint_capacity(vint));
+  printf("\nsize=%zd\ncapacity=%zd\n", Vint_size(vint), Vint_capacity(vint));
   
   printf("\nVector of objects:\n");
   
