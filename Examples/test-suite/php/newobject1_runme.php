ee3c9bd69755bfc81e411059b901bf2f265d5915
Olly Betts <olly@survex.com>
2021-12-03 16:09:07 +1300

    [php] Check fooCount() in newobject1_runme.php

--- Examples/test-suite/php/newobject1_runme.php
+++ Examples/test-suite/php/newobject1_runme.php
@@ -9,9 +9,16 @@ check::classes(array('Foo'));
 // No new vars
 check::globals(array());
 
+check::equal(Foo::fooCount(), 0, "no Foo objects expected");
 $foo = Foo::makeFoo();
 check::equal(get_class($foo), "Foo", "static failed");
+check::equal(Foo::fooCount(), 1, "1 Foo object expected");
 $bar = $foo->makeMore();
 check::equal(get_class($bar), "Foo", "regular failed");
+check::equal(Foo::fooCount(), 2, "2 Foo objects expected");
+$foo = null;
+check::equal(Foo::fooCount(), 1, "1 Foo object expected");
+$bar = null;
+check::equal(Foo::fooCount(), 0, "no Foo objects expected");
 
 check::done();
