c917f80c99dae396f67f3738769293361791dc55
juierror <siwa.boonpun@gmail.com>
2022-01-01 17:14:08 +0700

    debug: undefined ArrayInt6

--- Examples/test-suite/go/cpp11_std_array_runme.go
+++ Examples/test-suite/go/cpp11_std_array_runme.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import . "swigtests/cpp11_std_array"
+import "swigtests/cpp11_std_array"
 
 // func ToArray6(array [6]int) ArrayInt6 {
 //   var outputArray ArrayInt6
@@ -11,9 +11,9 @@ import . "swigtests/cpp11_std_array"
 //   return outputArray
 // }
 
-func CompareContainers(actual ArrayInt6, expected []int) error {
+func CompareContainers(actual cpp11_std_array.ArrayInt6, expected []int) error {
   if actual.Size() != len(expected) {
-    return fmt.Errorf("Sizes are different: %d %d", actual.size(), len(expected))
+    return fmt.Errorf("Sizes are different: %d %d", actual.Size(), len(expected))
   }
   for i := 0; i < actual.Size(); i++ {
 	actualValue := actual.Get(i)
@@ -22,14 +22,14 @@ func CompareContainers(actual ArrayInt6, expected []int) error {
 	  return fmt.Errorf("Value is wrong for element %d. Expected %d got: %d", i, expectedValue, actualValue)
 	}
   }
-  if actual.isEmpty() {
+  if actual.IsEmpty() {
 	return fmt.Errorf("ai should not be empty")
   }
   return nil
 }
 
 func main() {
-  ai := NewArrayInt6()
+  ai := cpp11_std_array.NewArrayInt6()
   ps := [6]int{0, 0, 0, 0, 0, 0}
   CompareContainers(ai, ps)
 
@@ -41,29 +41,29 @@ func main() {
 
   // Check return
   vals = [6]int{-2, -1, 0, 0, 1, 2}
-  CompareContainers(ArrayOutVal(), vals);
-  CompareContainers(ArrayOutConstRef(), vals);
-  CompareContainers(ArrayOutRef(), vals);
-  CompareContainers(ArrayOutPtr(), vals);
+  CompareContainers(cpp11_std_array.ArrayOutVal(), vals);
+  CompareContainers(cpp11_std_array.ArrayOutConstRef(), vals);
+  CompareContainers(cpp11_std_array.ArrayOutRef(), vals);
+  CompareContainers(cpp11_std_array.ArrayOutPtr(), vals);
 
   // Check passing arguments
   vals = [6]int{9, 8, 7, 6, 5, 4}
-  valsArrayInt6 := NewArrayInt6()
+  valsArrayInt6 := cpp11_std_array.NewArrayInt6()
   for i := 0; i < len(vals); i++ {
 	valsArrayInt6.Set(i, vals[i])
   }
 
-  ai = ArrayInVal(valsArrayInt6);
+  ai = cpp11_std_array.ArrayInVal(valsArrayInt6);
   CompareContainers(ai, vals);
 
-  ai = ArrayInConstRef(valsArrayInt6);
+  ai = cpp11_std_array.ArrayInConstRef(valsArrayInt6);
   CompareContainers(ai, vals);
 
-  ai = NewArrayInt6(valsArrayInt6);
+  ai = cpp11_std_array.NewArrayInt6(valsArrayInt6);
   cpp11_std_array.arrayInRef(ai);
   CompareContainers(ai, vals);
 
-  ai = NewArrayInt6(valsArrayInt6);
+  ai = cpp11_std_array.NewArrayInt6(valsArrayInt6);
   cpp11_std_array.arrayInPtr(ai);
   CompareContainers(ai, vals);
 
