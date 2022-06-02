ee50af92c44212a14a6b95674626c8bbb2bba7e8
Seth R Johnson <johnsonsr@ornl.gov>
2022-06-02 11:34:54 -0400

    Fix rename_camel testcase to work for Fortran

--- Examples/test-suite/php/rename_camel_runme.php
+++ Examples/test-suite/php/rename_camel_runme.php
@@ -6,7 +6,7 @@ require "tests.php";
 check::werror(true);
 
 check::classes(array("rename_camel","GeometryFactory","ByteOrderValues"));
-check::functions(array("CamelCase","CamelCase1","CamelCase2","CamelCase3","lowerCamelCase","lowerCamelCase1","lowerCamelCase2","lowerCamelCase3","under_case","under_case1","under_case2","under_case3","Import","foo","hello"));
+check::functions(array("CamelCase","CamelCase1","CamelCase2","CamelCase3","lowerCamelCase","lowerCamelCase1","lowerCamelCase2","lowerCamelCase3","under_case","under_case1","under_case2","under_case3","Import","foo","hello_"));
 check::constants(array("HELLO","HI_THERE","BYE","SEE_YOU"));
 
 
