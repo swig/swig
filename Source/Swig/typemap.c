6254de05efbb7e8969b0507ebc84b675a35d93ee
Seth R Johnson <johnsonsr@ornl.gov>
2022-06-03 06:50:25 -0400

    Avoid emitting locals duiring keyword typemap lookup

--- Source/Swig/typemap.c
+++ Source/Swig/typemap.c
@@ -2101,8 +2101,9 @@ static void replace_embedded_typemap(String *s, ParmList *parm_sublist, Wrapper 
 	    }
 	    found_colon = Strchr(tmap_method, ':');
 	    if (found_colon) {
+	      /* Substitute from a keyword argument to a typemap. Avoid emitting local variables from the attached typemap by passing NULL for the file. */
 	      String *temp_tmap_method = NewStringWithSize(Char(tmap_method), found_colon - Char(tmap_method));
-	      Swig_typemap_attach_parms(temp_tmap_method, to_match_parms, f);
+	      Swig_typemap_attach_parms(temp_tmap_method, to_match_parms, NULL);
 	      Delete(temp_tmap_method);
 	    } else {
 	      Swig_typemap_attach_parms(tmap_method, to_match_parms, f);
