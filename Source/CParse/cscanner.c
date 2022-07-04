22d8ee5469b89bad8bbac913e3ff18bfbdf29c4e
Julien Marrec <julien.marrec@gmail.com>
2022-07-04 15:25:01 +0200

    Avoid infinite loop when unmattched double brackets

--- Source/CParse/cscanner.c
+++ Source/CParse/cscanner.c
@@ -357,7 +357,10 @@ static int yylook(void) {
     case SWIG_TOKEN_LLBRACKET:
       do {
         tok = Scanner_token(scan);
-      } while (tok != SWIG_TOKEN_RRBRACKET);
+      } while ((tok != SWIG_TOKEN_RRBRACKET) && (tok > 0));
+      if (tok <= 0) {
+        Swig_error(cparse_file, cparse_line, "Unbalanced double brackets, missing closing (']]'). Reached end of input.\n");
+      }
       break;
 
     case SWIG_TOKEN_RRBRACKET:
