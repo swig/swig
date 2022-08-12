23ce0655a61eef61e15b7fc78b81393a362c7981
William S Fulton <wsf@fultondesigns.co.uk>
2022-08-12 23:50:54 +0100

    Add Guile support for std::unique_ptr and std::auto_ptr
    Equivalent to Python/Ruby implementations.

---
+++ Examples/test-suite/guile/cpp11_std_unique_ptr_runme.scm
@@ -0,0 +1,3 @@
+(dynamic-call "scm_init_cpp11_std_unique_ptr_module" (dynamic-link "./libcpp11_std_unique_ptr"))
+(load "testsuite.scm")
+(load "../schemerunme/cpp11_std_unique_ptr.scm")
---
+++ Examples/test-suite/schemerunme/cpp11_std_unique_ptr.scm
@@ -0,0 +1,75 @@
+(define checkCount
+  (lambda (expected-count)
+    (define actual-count (Klass-getTotal-count))
+    (if (not (= actual-count expected-count)) (error "Counts incorrect, expected:" expected-count " actual:" actual-count))))
+
+; Test raw pointer handling involving virtual inheritance
+(define kini (new-KlassInheritance "KlassInheritanceInput"))
+(checkCount 1)
+(define s (useKlassRawPtr kini))
+(if (not (string=? s "KlassInheritanceInput"))
+  (error "Incorrect string: " s))
+(delete-Klass kini)
+(checkCount 0)
+
+; unique_ptr as input
+(define kin (new-Klass "KlassInput"))
+(checkCount 1)
+(define s (takeKlassUniquePtr kin))
+(checkCount 0)
+(if (not (string=? s "KlassInput"))
+  (error "Incorrect string: " s))
+(if (not (is-nullptr kin))
+  (error "is_nullptr failed"))
+(delete-Klass kin) ; Should not fail, even though already deleted
+(checkCount 0)
+
+(define kin (new-Klass "KlassInput"))
+(checkCount 1)
+(define s (takeKlassUniquePtr kin))
+(checkCount 0)
+(if (not (string=? s "KlassInput"))
+  (error "Incorrect string: " s))
+(if (not (is-nullptr kin))
+  (error "is_nullptr failed"))
+(expect-throw 'misc-error 
+              (takeKlassUniquePtr kin))
+; TODO: check the exception message
+(delete-Klass kin) ; Should not fail, even though already deleted
+(checkCount 0)
+
+(define kin (new-Klass "KlassInput"))
+(define notowned (get-not-owned-ptr kin))
+(expect-throw 'misc-error 
+              (takeKlassUniquePtr notowned))
+; TODO: check the exception message
+(checkCount 1)
+(delete-Klass kin)
+(checkCount 0)
+
+(define kini (new-KlassInheritance "KlassInheritanceInput"))
+(checkCount 1)
+(define s (takeKlassUniquePtr kini))
+(checkCount 0)
+(if (not (string=? s "KlassInheritanceInput"))
+  (error "Incorrect string: " s))
+(if (not (is-nullptr kini))
+  (error "is_nullptr failed"))
+(delete-Klass kini) ; Should not fail, even though already deleted
+(checkCount 0)
+
+; unique_ptr as output
+(define k1 (makeKlassUniquePtr "first"))
+(define k2 (makeKlassUniquePtr "second"))
+(checkCount 2)
+
+(delete-Klass k1)
+(checkCount 1)
+
+(if (not (string=? (Klass-getLabel k2) "second"))
+  (error "wrong object label" ))
+
+(delete-Klass k2)
+(checkCount 0)
+
+(exit 0)
