a8e66dc2dcfaf1560c055df18b426c470479d838
Momtchil Momtchev <momtchil@momtchev.com>
2023-04-23 17:21:53 +0200

    add napi to the install script

--- Tools/CI-linux-install.sh
+++ Tools/CI-linux-install.sh
@@ -40,7 +40,7 @@ case "$SWIGLANG" in
 		;;
 	"javascript")
 		case "$ENGINE" in
-			"node")
+			"node"|"napi")
 				$RETRY wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.10/install.sh | bash
 				export NVM_DIR="$HOME/.nvm"
 				[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
