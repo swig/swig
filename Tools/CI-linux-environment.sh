418f42ff84d0e89667174ed795a1ebd9dbc1d6ec
Momtchil Momtchev <momtchil@momtchev.com>
2023-04-23 16:16:28 +0200

    add NAPI to CI

--- Tools/CI-linux-environment.sh
+++ Tools/CI-linux-environment.sh
@@ -14,7 +14,7 @@ case "$SWIGLANG" in
 		;;
 	"javascript")
 		case "$ENGINE" in
-			"node")
+			"node"|"napi")
 				export NVM_DIR="$HOME/.nvm"
 				[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 				nvm use ${VER}
