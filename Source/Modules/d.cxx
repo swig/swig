ece7ba0218416162c8ac27871340f5f9b6147876
Olly Betts <olly@survex.com>
2023-05-04 13:17:30 +1200

    [D] Fix typo in -d2 option description

--- Source/Modules/d.cxx
+++ Source/Modules/d.cxx
@@ -4634,7 +4634,7 @@ extern "C" Language *swig_d(void) {
  * ----------------------------------------------------------------------------- */
 const char *D::usage = "\
 D Options (available with -d)\n\
-     -d2                  - Generate code for D2/Phobos (The default, left for backward compatible)\n\
+     -d2                  - Generate code for D2/Phobos (The default, left for backward compatibility)\n\
      -package <pkg>       - Write generated D modules into package <pkg>\n\
      -splitproxy          - Write each D type to a dedicated file instead of\n\
                             generating a single proxy D module.\n\
