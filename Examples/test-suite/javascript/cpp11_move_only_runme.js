0aac6bb691239bef66952a68a0f37696027865a0
Momtchil Momtchev <momtchil@momtchev.com>
2023-05-21 13:56:49 +0200

    do not call a factory as a constructor

--- Examples/test-suite/javascript/cpp11_move_only_runme.js
+++ Examples/test-suite/javascript/cpp11_move_only_runme.js
@@ -2,7 +2,7 @@ var cpp11_move_only = require("cpp11_move_only");
 
 // Output
 cpp11_move_only.Counter.reset_counts();
-var mo = new cpp11_move_only.MoveOnly.create();
+var mo = cpp11_move_only.MoveOnly.create();
 cpp11_move_only.Counter.check_counts(1, 0, 0, 2, 0, 2);
 
 cpp11_move_only.Counter.reset_counts();
@@ -11,7 +11,7 @@ cpp11_move_only.Counter.check_counts(2, 1, 0, 0, 1, 2);
 
 // Move semantics not used
 cpp11_move_only.Counter.reset_counts();
-var mo = new cpp11_move_only.MovableCopyable.createConst();
+var mo = cpp11_move_only.MovableCopyable.createConst();
 cpp11_move_only.Counter.check_counts(2, 1, 1, 0, 0, 2);
 
 // Input
