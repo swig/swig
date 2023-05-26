376198531b4fa222679fd7494bda714b551cbfe0
Olly Betts <olly@survex.com>
2023-05-26 16:17:04 +1200

    [tcl] Expand li_std_string_runme.tcl
    Add tests from other targets languages' runmes.

--- Examples/test-suite/tcl/li_std_string_runme.tcl
+++ Examples/test-suite/tcl/li_std_string_runme.tcl
@@ -3,13 +3,36 @@ if [ catch { load ./li_std_string[info sharedlibextension] li_std_string} err_ms
 	puts stderr "Could not load shared object:\n$err_msg"
 }
 
+test_value "Fee"
 
-Structure s 
+if {[test_value "Fi"] != "Fi"} { error "bad test_value"}
+
+test_const_reference "Fo"
+
+if {[test_const_reference "Fum"] != "Fum"} { error "bad test_const_reference"}
+
+set stringPtr [test_pointer_out]
+
+test_pointer $stringPtr
+
+set stringPtr [test_const_pointer_out]
+
+test_const_pointer $stringPtr
+
+set stringPtr [test_reference_out]
+
+test_reference $stringPtr
+
+# Global variables
+if {$ConstGlobalString != "const global string"} { error "bad ConstGlobalString_get"}
+
+# Member variables
+Structure s
 if {"[s cget -MemberString2]" != "member string 2"} { error "bad string map"}
 s configure -MemberString2 "hello"
 if {"[s cget -MemberString2]" != "hello"} { error "bad string map"}
 
-if {"[s cget -ConstMemberString]" != "const member string"} { error "bad string map"}
+if {[s cget -ConstMemberString] != "const member string"} { error "bad ConstMemberString"}
 
 if {"$GlobalString2" != "global string 2"} { error "bad string map"}
 if {"$Structure_StaticMemberString2" != "static member string 2"} { error "bad string map"}
@@ -19,6 +42,10 @@ if {"$GlobalString2" != "hello"} { error "bad string map"}
 
 set Structure_StaticMemberString2 "hello"
 if {"$Structure_StaticMemberString2" != "hello"} { error "bad string map"}
+
+if {$Structure_ConstStaticMemberString != "const static member string"} { error "bad ConstStaticMemberString"}
+
+test_const_reference_returning_void "foo"
 
 if {[stdstring_empty] != ""} { error "bad stdstring_empty test" }
 if {[c_empty] != ""} { error "bad c_empty test" }
