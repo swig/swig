b99790c6db44be59f2edcf1e49720bd7dde76aba
Momtchil Momtchev <momtchil@momtchev.com>
2023-05-29 20:19:38 +0200

    remove debugging leftovers

--- Examples/test-suite/javascript/member_pointer_const_runme.js
+++ Examples/test-suite/javascript/member_pointer_const_runme.js
@@ -11,7 +11,7 @@ function check(what, expected, actual) {
 // Get the pointers
 
 area_pt = member_pointer_const.areapt();
-//perim_pt = member_pointer_const.perimeterpt();
+perim_pt = member_pointer_const.perimeterpt();
 
 // Create some objects
 
@@ -20,33 +20,31 @@ s = new member_pointer_const.Square(10);
 // Do some calculations
 
 check("Square area ", 100.0, member_pointer_const.do_op(s, area_pt));
-/*check("Square perim", 40.0, member_pointer_const.do_op(s, perim_pt));
+check("Square perim", 40.0, member_pointer_const.do_op(s, perim_pt));
 
-memberPtr = cvar.areavar;
-memberPtr = cvar.perimetervar;
+memberPtr = member_pointer_const.areavar;
+memberPtr = member_pointer_const.perimetervar;
 
 // Try the variables
-check("Square area ", 100.0, member_pointer_const.do_op(s, cvar.areavar));
-check("Square perim", 40.0, member_pointer_const.do_op(s, cvar.perimetervar));
+check("Square area ", 100.0, member_pointer_const.do_op(s, member_pointer_const.areavar));
+check("Square perim", 40.0, member_pointer_const.do_op(s, member_pointer_const.perimetervar));
 
 // Modify one of the variables
 member_pointer_const.areavar = perim_pt;
 
-check("Square perimeter", 40.0, member_pointer_const.do_op(s, cvar.areavar));
+check("Square perimeter", 40.0, member_pointer_const.do_op(s, member_pointer_const.areavar));
 
 // Try the constants
+memberPtr = member_pointer_const.AREAPT;
+memberPtr = member_pointer_const.PERIMPT;
+memberPtr = member_pointer_const.NULLPT;
 
-memberPtr = AREAPT;
-memberPtr = PERIMPT;
-memberPtr = NULLPT;
-
-check("Square area ", 100.0, member_pointer_const.do_op(s, AREAPT));
-check("Square perim", 40.0, member_pointer_const.do_op(s, PERIMPT));
+check("Square area ", 100.0, member_pointer_const.do_op(s, member_pointer_const.AREAPT));
+check("Square perim", 40.0, member_pointer_const.do_op(s, member_pointer_const.PERIMPT));
 
 // Typedefs
 check("Square perim", 40.0, member_pointer_const.do_op_td(s, perim_pt));
 
-check("Add by value", 3, member_pointer_const.call1(ADD_BY_VALUE, 1, 2));
-check("Add by pointer", 7, member_pointer_const.call2(ADD_BY_POINTER, 3, 4));
-check("Add by reference", 11, member_pointer_const.call3(ADD_BY_REFERENCE, 5, 6));
-*/
+check("Add by value", 3, member_pointer_const.call1(member_pointer_const.ADD_BY_VALUE, 1, 2));
+check("Add by pointer", 7, member_pointer_const.call2(member_pointer_const.ADD_BY_POINTER, 3, 4));
+check("Add by reference", 11, member_pointer_const.call3(member_pointer_const.ADD_BY_REFERENCE, 5, 6));
