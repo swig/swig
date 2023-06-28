515dfea3e90578d40ee18fb9e65d4c3d5dd0072a
Cristian Le <cristian.le@mpsd.mpg.de>
2023-06-28 09:50:08 +0200

    Refactor CMake Project
    - Added basic options
    - Moved source definitions in individual files
    - Added install/export instructions
    - Removed CPack
    - Moved autotools mimicking to MimickAutotools
    - Prepare testing infrasturcture
    
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

---
+++ cmake/MimickAutotools.cmake
@@ -0,0 +1,26 @@
+include(CheckIncludeFiles)
+include(CheckIncludeFile)
+include(CheckIncludeFileCXX)
+include(CheckTypeSize)
+include(CheckSymbolExists)
+include(CheckFunctionExists)
+include(CheckLibraryExists)
+include(CheckCSourceCompiles)
+
+# TODO: Not needed these days. Hard-code to 1
+check_include_file("inttypes.h" HAVE_INTTYPES_H)
+check_include_file("stddef.h" HAVE_STDDEF_H)
+check_include_file("stdint.h" HAVE_STDINT_H)
+check_include_file("stdio.h" HAVE_STDIO_H)
+check_include_file("stdlib.h" HAVE_STDLIB_H)
+check_include_file("string.h" HAVE_STRING_H)
+check_include_file("strings.h" HAVE_STRINGS_H)
+check_include_file("sys/stat.h" HAVE_SYS_STAT_H)
+check_include_file("sys/types.h" HAVE_SYS_TYPES_H)
+check_include_file("unistd.h" HAVE_UNISTD_H)
+check_include_files("stdlib.h;stdarg.h;string.h;float.h" STDC_HEADERS)
+
+# TODO: Move this to cmake natives
+check_include_file_cxx("boost/shared_ptr.hpp" HAVE_BOOST)
+check_library_exists(dl dlopen "" HAVE_LIBDL)
+check_function_exists(popen HAVE_POPEN)
