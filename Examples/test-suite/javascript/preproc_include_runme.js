95175fd809cbc6a27978c232185832e10ea32b74
Momtchil Momtchev <momtchil@momtchev.com>
2023-07-19 18:55:16 +0200

    more async tests

--- Examples/test-suite/javascript/preproc_include_runme.js
+++ Examples/test-suite/javascript/preproc_include_runme.js
@@ -1,23 +1,23 @@
 var preproc_include = require("preproc_include");
 
-if (preproc_include.multiply10(10) != 100)
+if (/* await */(preproc_include.multiply10(10)) != 100)
   throw "RuntimeError";
 
-if (preproc_include.multiply20(10) != 200)
+if (/* await */(preproc_include.multiply20(10)) != 200)
   throw "RuntimeError";
 
-if (preproc_include.multiply30(10) != 300)
+if (/* await */(preproc_include.multiply30(10)) != 300)
   throw "RuntimeError";
 
-if (preproc_include.multiply40(10) != 400)
+if (/* await */(preproc_include.multiply40(10)) != 400)
   throw "RuntimeError";
 
-if (preproc_include.multiply50(10) != 500)
+if (/* await */(preproc_include.multiply50(10)) != 500)
   throw "RuntimeError";
 
-if (preproc_include.multiply60(10) != 600)
+if (/* await */(preproc_include.multiply60(10)) != 600)
   throw "RuntimeError";
 
-if (preproc_include.multiply70(10) != 700)
+if (/* await */(preproc_include.multiply70(10)) != 700)
   throw "RuntimeError";
 
