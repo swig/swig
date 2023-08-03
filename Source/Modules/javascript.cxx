545a5732daa5acaa1fa0c6a0885aadeee4902433
Momtchil Momtchev <momtchil@momtchev.com>
2023-08-03 14:07:56 +0200

    set the sync name of native functions

--- Source/Modules/javascript.cxx
+++ Source/Modules/javascript.cxx
@@ -3276,6 +3276,7 @@ int NAPIEmitter::emitNativeFunction(Node *n) {
   String *wrapname = Getattr(n, "wrap:name");
   enterFunction(n);
   state.function(WRAPPER_NAME, wrapname);
+  state.function("name:sync", state.function(NAME));
   emitFunctionDeclaration(n, false);
   exitFunction(n);
   return SWIG_OK;
