ec5f5ba314ea5c65ed318ba4ba2258048f59bcd8
Seth R Johnson <johnsonsr@ornl.gov>
2023-08-10 08:13:38 -0400

    Add support for unique ptr (but virtual inheritance is broken!)

---
+++ Examples/test-suite/fortran/cpp11_std_unique_ptr_runme.F90
@@ -0,0 +1,26 @@
+! File : cpp11_std_unique_ptr_runme.F90
+
+#include "fassert.h"
+
+program cpp11_std_unique_ptr_runme
+  implicit none
+
+  call test_raw
+
+contains
+subroutine test_raw
+  use cpp11_std_unique_ptr
+  use ISO_C_BINDING
+  implicit none
+  type(Klass) :: k
+  type(KlassInheritance) :: kini
+  character(kind=C_CHAR, len=:), allocatable :: s
+  kini = KlassInheritance("KlassInheritanceInput")
+  ASSERT(kini%getLabel() == "KlassInheritanceInput")
+  ASSERT(k%getTotal_count() == 1)
+  s = useKlassRawPtr(kini)
+  ASSERT(s == "KlassInheritanceInput")
+  call kini%release()
+end subroutine
+end program
+
