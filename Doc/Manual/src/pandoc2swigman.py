15c586268d49728b78f638f383b74f1298d198da
Seth R Johnson <johnsonsr@ornl.gov>
2023-08-11 07:56:00 -0400

    Update and regenerate documentation (fixing pandoc issues)

--- Doc/Manual/src/pandoc2swigman.py
+++ Doc/Manual/src/pandoc2swigman.py
@@ -2,40 +2,44 @@
 """Convert Pandoc-generated HTML to SWIG html
 
 Requires Python 3.x"""
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------#
 from html.entities import codepoint2name
 import os
 import re
 from string import printable
+
 ###############################################################################
 
 RE_HEADER = re.compile(r'^<h(\d) id="([^"]+)">(.*)</h(\d)>$')
-RE_PRE    = re.compile(r'^<pre class="([^"]+)">')
-RE_ENDPRE = re.compile(r'</pre>')
-RE_LINK   = re.compile(r'<a href="#([^"]+)">')
-RE_TITLE   = re.compile(r'^<title>(.*)</title>$')
+RE_PRE = re.compile(r'^<pre class="([^"]+)">')
+RE_ENDPRE = re.compile(r"</pre>")
+RE_LINK = re.compile(r'<a href="#([^"]+)">')
+RE_TITLE = re.compile(r"^<title>(.*)</title>$")
 
 NEW_HEADER = '<H{lev:d}><a name="{link}">{title}</a></H{lev:d}>\n'
 
 SELECTOR = {
-        'fortran': "targetlang",
-        'f90':     "targetlang",
-        'swig':    "code",
-        'cpp':     "code",
-        'c++':     "code",
-        'c':       "code",
-        'sh':      "shell",
-        }
+    "fortran": "targetlang",
+    "f90": "targetlang",
+    "swig": "code",
+    "cpp": "code",
+    "c++": "code",
+    "c": "code",
+    "sh": "shell",
+}
 
 NONASCII_TO_HTML = {}
 for k in set(codepoint2name) - set(ord(x) for x in printable):
     NONASCII_TO_HTML[k] = "&" + codepoint2name[k] + ";"
 
+
 def convert_link(link):
-    return "Fortran_" + link.replace("-","_")
+    return "Fortran_" + link.replace("-", "_")
+
 
 def repl_link_match(match):
     return r'<a href="Fortran.html#{}">'.format(convert_link(match.group(1)))
+
 
 def swiggify(oldf, newf, section):
     # Replace header and navigation
@@ -47,7 +51,8 @@ def swiggify(oldf, newf, section):
             title = match.group(1)
         elif line == "</nav>":
             break
-    newf.write("""\
+    newf.write(
+        """\
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -57,7 +62,10 @@ def swiggify(oldf, newf, section):
 </head>
 <body bgcolor="#FFFFFF">
 <H1><a name="{section}">{title}</a></H1>
-""".format(title=title, section=section))
+""".format(
+            title=title, section=section
+        )
+    )
 
     in_code = False
     for line in oldf:
@@ -68,15 +76,15 @@ def swiggify(oldf, newf, section):
             match = RE_PRE.match(line)
             if match:
                 code = SELECTOR[match.group(1)]
-                line = '\n<div class="{}"><pre>{}'.format(
-                    code, line[match.end():])
+                end = match.end()
+                line = '\n<div class="{}"><pre>{}'.format(code, line[end:])
                 in_code = True
 
         if not in_code:
             match = RE_HEADER.match(line)
             if match:
                 (lev, link, title, lev2) = match.groups()
-                lev = int(lev) + 1 # lower the heading level
+                lev = int(lev) + 1  # lower the heading level
                 link = convert_link(link)
                 line = NEW_HEADER.format(lev=lev, link=link, title=title)
 
@@ -90,14 +98,16 @@ def swiggify(oldf, newf, section):
 
         newf.write(line)
 
+
 def run(path):
     basename = os.path.splitext(os.path.basename(path))[0]
     temp_path = path + ".updated"
-    with open(path, 'r') as oldf:
-        with open(temp_path, 'w') as newf:
+    with open(path, "r") as oldf:
+        with open(temp_path, "w") as newf:
             swiggify(oldf, newf, basename)
     os.rename(temp_path, path)
 
-#-----------------------------------------------------------------------------#
-if __name__ == '__main__':
+
+# -----------------------------------------------------------------------------#
+if __name__ == "__main__":
     run("../Fortran.html")
--- Doc/Manual/src/regenerate.sh
+++ Doc/Manual/src/regenerate.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -ev
 
+PYTHON=python3
+
+# Convert markdown to standalone HTML
 pandoc --from=gfm+smart --no-highlight \
   -M title:"SWIG and Fortran" -M author:"Seth R Johnson" \
+  --wrap=none \
   --toc --standalone -H style.css -o ../Fortran.html Fortran.md
-python pandoc2swigman.py
-(cd .. && python maketoc.py)
+# Transform Pandoc formatting to SWIG manual-style formatting
+$PYTHON pandoc2swigman.py
+# Generate table of contents (and update top-level TOC)
+(cd .. && $PYTHON maketoc.py)
