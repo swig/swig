7ca1fbfa8b55704f04587f877acc7199383747b2
Momtchil Momtchev <momtchil@momtchev.com>
2023-08-12 22:52:29 +0200

    async throwing of random objects

--- Examples/test-suite/javascript/napi_async_reject_runme.js
+++ Examples/test-suite/javascript/napi_async_reject_runme.js
@@ -6,13 +6,13 @@ const notRej = Symbol();
 function isRejected(promise) {
   if (!(promise instanceof Promise)) throw new Error('Expected a Promise');
   return promise
-  .then(() => {
-    throw notRej;
-  })
-  .catch((e) => {
-    if (e === notRej) throw new Error('Promise resolved');
-    return e;
-  })
+    .then(() => {
+      throw notRej;
+    })
+    .catch((e) => {
+      if (e === notRej) throw new Error('Promise resolved');
+      return e;
+    });
 }
 
 if (/* await */(napi_async_reject.fnAsync(1)) !== 2)
@@ -33,3 +33,28 @@ if (fail) throw new Error('Did not throw');
 const rej = /* await */(isRejected(napi_async_reject.fnAsync(-1)));
 if (!rej.message.match('must be positive')) throw new Error(rej);
 
+
+// Synchronous throwing of random objects
+try {
+  napi_async_reject.throwsSync();
+  fail = true;
+} catch (e) {
+  if (!(e instanceof napi_async_reject.CustomError))
+    throw new Error('Error not of the expected type');
+  if (e.msg !== 'expected error')
+    throw new Error('Unexpected error');
+}
+if (fail) throw new Error('Did not throw');
+
+// Asynchronous throwing of random objects
+/* await */(napi_async_reject.throwsAsync())
+  .then(() => {
+    fail = true;
+  })
+  .catch((e) => {
+    if (!(e instanceof napi_async_reject.CustomError))
+      throw new Error('Error not of the expected type');
+    if (e.msg !== 'expected error')
+      throw new Error('Unexpected error');
+  });
+if (fail) throw new Error('Did not throw');
--- Examples/test-suite/napi_async_reject.i
+++ Examples/test-suite/napi_async_reject.i
@@ -1,5 +1,6 @@
 %module napi_async_reject
 
+
 // This test is specific to Node-API
 %feature("async", "Async");
 %feature("sync", "Sync");
@@ -8,8 +9,20 @@
   if ($1 <= 0) SWIG_exception_fail(SWIG_ValueError, "must be positive");
 }
 
+%catches(CustomError) throws;
+%immutable;
+
 %inline %{
+  struct CustomError {
+    const char *msg;
+    CustomError(const char *_msg): msg(_msg) {};
+  };
+
   int fn(int positive) {
     return positive + 1;
+  }
+
+  void throws() {
+    throw CustomError("expected error");
   }
 %}
