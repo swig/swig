900b11cf815c0dd98f057a6c890fc4e99621182e
Momtchil Momtchev <momtchil@momtchev.com>
2023-08-14 16:02:55 +0200

    run the examples in async mode but ignore errors

--- Examples/javascript/example.mk
+++ Examples/javascript/example.mk
@@ -19,10 +19,6 @@ ifneq (jsc, $(ENGINE))
 SWIGOPT += -DV8_VERSION=$(JSV8_VERSION)
 endif
 
-# Examples are always in sync by default mode
-ifneq (,$(findstring async,$(SWIG_FEATURES)))
-SWIGOPT += -sync
-endif
 
 check: build
 	$(MAKE) -f $(EXAMPLES_TOP)/Makefile SRCDIR='$(SRCDIR)' TARGET='$(TARGET)' javascript_run
