f64ce2cdb76a5a4ce0749320d2c5fcbd38b9ddc5
William S Fulton <wsf@fultondesigns.co.uk>
2023-08-17 01:11:33 +0100

    Add in missing feature:python:tp_watched for python-3.12

--- Source/Modules/python.cxx
+++ Source/Modules/python.cxx
@@ -4204,7 +4204,7 @@ public:
     printSlot(f, getSlot(), "tp_print");
     Printv(f, "#endif\n", NIL);
     Printv(f, "#if PY_VERSION_HEX >= 0x030c0000\n", NIL);
-    printSlot(f, getSlot(), "tp_watched");
+    printSlot(f, getSlot(n, "feature:python:tp_watched"), "tp_watched", "char");
     Printv(f, "#endif\n", NIL);
 
     Printv(f, "#ifdef COUNT_ALLOCS\n", NIL);
