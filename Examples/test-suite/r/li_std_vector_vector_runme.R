04355afeb3e00ddd0a22734539ae629b3003362a
AndLLA <44858649+AndLLA@users.noreply.github.com>
2023-09-11 19:35:57 +0200

    Rtest3 (#2448)
    Improve li_std_vector_vector runtime test for R
    
    - Fix unittest_sequence to fail if elements don't match.
    - li_std_vector_vector testcase fixes.
    
    Replace list with c because vectors in R are not exactly lists,
    therefore comparing vectors (returned by C++) with R lists
    complicates things a lot. Added as.integer because by default
    numbers are numeric and I guess we want to check the exact type
    returned by C++ which is integer the first check fails because
    make_vector_int returns a vector of strings (containing numbers),
    not integers (in the previous version this test would pass
    because R does an implicit 
    
    Closes #2448

--- Examples/test-suite/r/li_std_vector_vector_runme.R
+++ Examples/test-suite/r/li_std_vector_vector_runme.R
@@ -6,23 +6,24 @@ source("li_std_vector_vector.R")
 cacheMetaData(1)
 
 v = make_vector_int()
-unittest_sequence(v, list(1, 2, 3, 4, 5))
+unittest_sequence(v, as.integer(c(1, 2, 3, 4, 5)))
+
 v = make_vector_vector_int()
 unittest(length(v), 1)
-unittest_sequence(v[[1]], list(1, 2, 3, 4, 5))
+unittest_sequence(v[[1]], as.integer(c(1, 2, 3, 4, 5)))
 
 v = make_vector_bool()
-unittest_sequence(v, list(TRUE, FALSE, FALSE, FALSE, TRUE))
+unittest_sequence(v, c(TRUE, FALSE, FALSE, FALSE, TRUE))
 print(v)
 v = make_vector_vector_bool()
 unittest(length(v), 1)
 print(v)
-unittest_sequence(v[[1]], list(FALSE, TRUE, TRUE, TRUE, FALSE)) # Does not actually fail if no match, arg, needs fixing
+unittest_sequence(v[[1]], c(FALSE, TRUE, TRUE, TRUE, FALSE))
 
 v = make_vector_string()
-unittest_sequence(v, list("aa", "bb", "cc", "dd", "ee"))
+unittest_sequence(v, c("aa", "bb", "cc", "dd", "ee"))
 v = make_vector_vector_string()
 unittest(length(v), 1)
-unittest_sequence(v[[1]], list("1", "2", "3", "4", "5"))
+unittest_sequence(v[[1]], c("1", "2", "3", "4", "5"))
 
 q(save="no")
--- Examples/test-suite/r/unittest.R
+++ Examples/test-suite/r/unittest.R
@@ -18,10 +18,26 @@ unittesttol <- function(x,y,z) {
 }
 
 unittest_sequence <- function (x,y) {
-  if (setequal(x, y)) {
+  x = as.vector(x)
+  y = as.vector(y)
+  
+  if (length(x) == 0 && length(y) == 0) {
     print("PASS")
-  } else {
-    print("FAIL")
-    stop("Test failed")
+    return()
   }
+  
+  if (class(x[1]) != class(y[1])) {
+    print("FAILED")
+    stop(paste("Test failed: ", class(x[1]), " != ", class(y[1])))
+  }
+  
+  try(expr = {
+    if (!any(x != y)) {
+      print("PASS")
+      return()
+    }
+  }, silent = T)
+  
+  print("FAIL")
+  stop("Test failed")
 }
