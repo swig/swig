3dc204e2e10b9096281fd518cf685bc1e5a9ee06
Cristian Le <cristian.le@mpsd.mpg.de>
2023-09-19 09:44:42 +0200

    Configure some actions to use upstream setup action
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- Tools/CI-linux-install.sh
+++ Tools/CI-linux-install.sh
@@ -18,26 +18,6 @@ WITHLANG=$SWIGLANG
 
 case "$SWIGLANG" in
 	"")     ;;
-	"csharp")
-		$RETRY sudo apt-get -qq install mono-devel
-		;;
-	"d")
-		if [[ $VER =~ ^2\. ]]; then
-			$RETRY wget http://downloads.dlang.org/releases/2.x/${VER}/dmd_${VER}-0_amd64.deb
-			$RETRY sudo dpkg -i dmd_${VER}-0_amd64.deb
-		else
-			$RETRY sudo apt-get -qq install "$VER"
-		fi
-		;;
-	"go")
-		if [[ "$VER" ]]; then
-		  mkdir -p $HOME/bin
-		  curl -sL -o $HOME/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-		  chmod +x $HOME/bin/gimme
-		  eval "$($HOME/bin/gimme ${VER}.x)"
-		  $HOME/bin/gimme --list
-		fi
-		;;
 	"javascript")
 		case "$ENGINE" in
 			"node"|"napi")
@@ -69,18 +49,8 @@ case "$SWIGLANG" in
 	"guile")
 		$RETRY sudo apt-get -qq install guile-${VER:-2.0}-dev
 		;;
-	"lua")
-		if [[ -z "$VER" ]]; then
-			$RETRY sudo apt-get -qq install lua5.2 liblua5.2-dev
-		else
-			$RETRY sudo apt-get -qq install lua${VER} liblua${VER}-dev
-		fi
-		;;
 	"mzscheme")
 		$RETRY sudo apt-get -qq install racket
-		;;
-	"ocaml")
-		$RETRY sudo apt-get -qq install ocaml camlp4
 		;;
 	"octave")
 		if [[ "$VER" ]]; then
@@ -92,66 +62,9 @@ case "$SWIGLANG" in
 			$RETRY sudo apt-get -qq install liboctave-dev
 		fi
 		;;
-	"php")
-		if [[ "$VER" ]]; then
-			$RETRY sudo apt-get -qq remove "php*-cli" "php*-dev" # Multiple versions are pre-installed
-			$RETRY sudo add-apt-repository -y ppa:ondrej/php
-			$RETRY sudo apt-get -qq update
-			$RETRY sudo apt-get -qq install php$VER-cli php$VER-dev
-		fi
-		;;
 	"python")
-		pip install --user pycodestyle
-		if [[ "$PY2" ]]; then
-			WITHLANG=$SWIGLANG
-		else
-			WITHLANG=${SWIGLANG}3
-		fi
-		if [[ "$VER" ]]; then
-			$RETRY sudo add-apt-repository -y ppa:deadsnakes/ppa
-			$RETRY sudo apt-get -qq update
-			$RETRY sudo apt-get -qq install python${VER}-dev
-			WITHLANG=$WITHLANG=$SWIGLANG$VER
-		elif [[ "$PY2" ]]; then
-			$RETRY sudo apt-get install -qq python-dev
-		else
-			$RETRY sudo apt-get install -qq python3-dev
-		fi
-		;;
-	"r")
-		$RETRY sudo apt-get -qq install r-base
-		;;
-	"ruby")
-		if [[ "$VER" ]]; then
-			case "$VER" in
-				3.1 | 3.2 | 3.3 )
-					# Ruby 3.1+ support is currently only rvm master (2023-04-19)
-					# YOLO
-					curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-					curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-					curl -sSL https://get.rvm.io | bash -s stable
-					set +x
-					source $HOME/.rvm/scripts/rvm
-					$RETRY rvm get master
-					rvm reload
-					rvm list known
-					set -x
-					;;
-				* )
-					# Install from PPA as that also contains packages needed for the build.
-					sudo apt-add-repository -y ppa:rael-gc/rvm
-					sudo apt-get update
-					sudo apt-get install rvm
-					sudo usermod -a -G rvm $USER
-					set +x
-					source /etc/profile.d/rvm.sh
-					set -x
-					;;
-			esac
-			set +x
-			$RETRY rvm install $VER
-			set -x
-		fi
+	  # Quick compatibility fix for old CI system
+		WITHLANG=${SWIGLANG}3
 		;;
 	"scilab")
 		if [[ -z "$VER" ]]; then
