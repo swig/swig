ed30a80d7f7c76bcf4fedfc24a548e6fab2b35be
Olly Betts <olly@survex.com>
2024-02-26 12:37:52 +1300

    [php] Add runme for testcase constant_expr

---
+++ Examples/test-suite/php/constant_expr_runme.php
@@ -0,0 +1,18 @@
+<?php
+
+require "tests.php";
+
+// New functions
+check::functions(array('bar', 'test2', 'xx', 'yy'));
+// New classes
+check::classes(array('constant_expr'));
+// New vars
+check::globals(array('X','a','d_array','s1a','s2a','s2b','s3a','s3b','s4a','s4b','s5a','s5b','s6a','s6b','s7a','s7b','s8a','s8b'));
+
+check::equal(XX, xx());
+check::equal(YY, yy());
+
+check::equal(constant_expr::XX, constant_expr::xx());
+check::equal(constant_expr::YY, constant_expr::yy());
+
+check::done();
