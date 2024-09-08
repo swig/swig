487d6d378444b469d7c8a09da2af98e2c7cc06f3
Momtchil Momtchev <momtchil@momtchev.com>
2024-09-08 09:44:05 +0200

    adapt to the parser changes
    remove superfluos await
    
    the other extra await
    
    fix the test
    
    make a valid test

--- Examples/test-suite/javascript/keyword_rename_c_runme.js
+++ Examples/test-suite/javascript/keyword_rename_c_runme.js
@@ -2,4 +2,7 @@ var keyword_rename_c = require("keyword_rename_c");
 /* await */(keyword_rename_c._instanceof(1));
 /* await */(keyword_rename_c._finally(1));
 /* await */(keyword_rename_c._yield(0));
-/* await */(keyword_rename_c.yield = 0);
+var s = new keyword_rename_c.S;
+// @ts-expect-error
+if (typeof s._yield === 'number') throw new Error('S._yield included');
+if (typeof s.yield !== 'number') throw new Error('S.yield not included');
