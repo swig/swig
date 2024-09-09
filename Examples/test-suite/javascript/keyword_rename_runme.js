5a1d7ae0355c9985d3af9bc17bc791bf0208f2c6
Vadim Zeitlin <vz-swig@zeitlins.org>
2024-09-09 18:04:19 +0200

    Fix JS keyword rename test to actually test for renaming
    Assigning to a property in JS always works, as it just creates a new
    property if there is no existing one, and so can't be used for testing
    for its existence.
    
    Set the property value in C code and try accessing it from JS instead.
    
    Also check that not only the property with the expected name exists, but
    also that the property with the renamed name does not.
    
    See #2943.

--- Examples/test-suite/javascript/keyword_rename_runme.js
+++ Examples/test-suite/javascript/keyword_rename_runme.js
@@ -2,4 +2,11 @@ var keyword_rename = require("keyword_rename")
 keyword_rename._instanceof(1)
 keyword_rename._finally(1)
 keyword_rename._yield(0)
-keyword_rename.yield = 0
+
+var s = keyword_rename.make_S_with_yield(17);
+
+if (s.yield != 17)
+    throw new Error;
+
+if (s._yield == 17)
+    throw new Error;
