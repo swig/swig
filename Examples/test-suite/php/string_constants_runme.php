2c4ff2f4d67442c77dbd61e1cbd999c111d1042a
Olly Betts <olly@survex.com>
2024-09-22 08:12:45 +1200

    [php] Fix warning from string_constants testcase

--- Examples/test-suite/php/string_constants_runme.php
+++ Examples/test-suite/php/string_constants_runme.php
@@ -7,7 +7,7 @@ check::functions(array());
 // New classes
 check::classes(array('string_constants', 'things'));
 // New vars
-check::globals(array('AA3', 'EE3', 'ES3', 'QQ3', 'SS3', 'XX3', 'ZS3'));
+check::globals(array('AA3', 'EE3', 'ES3', 'PR3', 'QQ3', 'SS3', 'XX3', 'ZS3'));
 
 check::equal(string_constants::QQ1, "\01000! \0018b00!");
 check::equal(string_constants::QQ2, "\01000! \0018b00!");
