8e65f9101edcfc1c272c1e11b4391bdf201c09fb
William S Fulton <wsf@fultondesigns.co.uk>
2024-10-05 13:51:01 +0100

    Add inout octave test
    INOUT is not functional for more than one INOUT argument

--- Examples/test-suite/octave/inout_runme.m
+++ Examples/test-suite/octave/inout_runme.m
@@ -1,27 +1,21 @@
 inout
 
 a = inout.AddOne1(1);
-if (a != 2)
-  error("failed");
-endif
+assert(a, 2)
 
-a = inout.AddOne3(1,1,1);
-if (a != [2,2,2])
-  error("failed");
-endif
+a = inout.AddOne3(10,20,30);
+# TODO: SWIG_Octave_AppendOutput seems broken, only one element comes back from C++ code.
+# assert(all(cell2mat(a) == [11,21,31]));
 
-a = inout.AddOne1p((1,1));
-if (a != (2,2))
-  error("failed");
-endif
+a = inout.AddOne1p({10,20});
+assert(all(cell2mat(a) == [11,21]))
 
-a = inout.AddOne2p((1,1),1);
-if (a != [(2,2),2])
-  error("failed");
-endif
+a = inout.AddOne2p({10,20},30);
+# TODO
+# assert(a == [{11,21},31]))
+assert(a{1}, 11); # Wrong
+assert(a{2}, 21); # Wrong
 
-a = inout.AddOne3p(1,(1,1),1);
-if (a != [2,(2,2),2])
-  error("failed");
-endif
-
+a = inout.AddOne3p(1,{1,1},1);
+# TODO
+# assert(a == [2,{2,2},2])
