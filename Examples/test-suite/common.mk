8315213df66b91b121493ab9e9ea2c57af9c8bde
Erez Geva <ErezGeva2@gmail.com>
2024-10-07 20:49:15 +0200

    Add director tests for reverse length string typemap.
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- Examples/test-suite/common.mk
+++ Examples/test-suite/common.mk
@@ -221,6 +221,7 @@ CPP_TEST_CASES += \
 	director_protected_overloaded \
 	director_redefined \
 	director_ref \
+	director_rev_len_str \
 	director_simple \
 	director_smartptr \
 	director_template \
