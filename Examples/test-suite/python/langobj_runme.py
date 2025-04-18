5dd85c36d3cc7aa519b5d99270527034676f8f18
Momtchil Momtchev <momtchil@momtchev.com>
2025-04-18 11:25:50 +0200

    merge upstream

--- Examples/test-suite/python/langobj_runme.py
+++ Examples/test-suite/python/langobj_runme.py
@@ -3,6 +3,10 @@ from langobj import *
 
 
 x = 256*256+1
+
+# avoid an immortal object for the ref count check to always work such as when using no-gil
+x += 1
+
 rx = sys.getrefcount(x)
 v = identity(x)
 rv = sys.getrefcount(v)
