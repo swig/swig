954bf445f3b56c9f30ad008e049575ae0fc19ed1
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2025-07-17 07:41:50 +0100

    Replace 'assert' with a 'check' function

--- Examples/test-suite/python/python_bufferinterface_runme.py
+++ Examples/test-suite/python/python_bufferinterface_runme.py
@@ -6,30 +6,35 @@ if sys.version_info < (3,0):
 
 import python_bufferinterface
 
+def check(what, expected, actual):
+    if expected != actual:
+        raise RuntimeError(
+            "Failed: ", what, " Expected: ", expected, " Actual: ", actual)
+
 # test not relevant unless certain conditions are met (builtin, limited API)
 if not python_bufferinterface.buffers_supported():
     exit(0)
 
 data = python_bufferinterface.ReadOnlyData()
 view = memoryview(data)
-assert(view.readonly == True)
-assert(view[:10] == b"This string represents"[:10])
-assert(python_bufferinterface.cvar.released == False)
+check('readonly', view.readonly, True)
+check('read data', view[:10], b"This string represents"[:10])
+check('not released', python_bufferinterface.cvar.released, False)
 view.release()
-assert(python_bufferinterface.cvar.released == True)
+check('released', python_bufferinterface.cvar.released, True)
 
 data = python_bufferinterface.ReadWriteData()
 view = memoryview(data)
-assert(view.readonly == False)
+check('readonly', view.readonly, False)
 text = b'Lorem ipsum dolor sit amet'
 view[:len(text)] = b'Lorem ipsum dolor sit amet'
-assert(view[:len(text)] == text)
-assert(python_bufferinterface.cvar.released == False)
+check('read data', view[:len(text)], text)
+check('not released', python_bufferinterface.cvar.released, False)
 view.release()
-assert(python_bufferinterface.cvar.released == True)
+check('released', python_bufferinterface.cvar.released, True)
 
 view = memoryview(data)
-assert(view[:len(text)] == text)
-assert(python_bufferinterface.cvar.released == False)
+check('written data', view[:len(text)], text)
+check('not released', python_bufferinterface.cvar.released, False)
 del view
-assert(python_bufferinterface.cvar.released == True)
+check('released', python_bufferinterface.cvar.released, True)
