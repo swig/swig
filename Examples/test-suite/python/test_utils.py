fd3b799d42ef92db85a69bd3ea422c3d2399997b
crusaderky <crusaderky@gmail.com>
2025-07-29 17:56:59 +0100

    Test

---
+++ Examples/test-suite/python/test_utils.py
@@ -0,0 +1,54 @@
+import random
+import string
+import sys
+
+
+def random_string(length=None):
+    if length is None:
+        length = random.randint(0, 16)
+    domain = string.ascii_letters + string.digits
+    if sys.version_info < (3, 6):
+        chars = [random.choice(domain) for _ in range(length)]
+    else:
+        chars = random.choices(domain, k=length)
+    return "".join(chars)
+
+
+def run_threaded(func, max_workers=8, pass_count=False,
+                 pass_barrier=False, outer_iterations=1,
+                 prepare_args=None):
+    """Runs a function many times in parallel.
+    Copied from numpy/testing/_private/utils.py::run_threaded
+    """
+    if sys.version_info < (3, 2):
+        return
+    import concurrent.futures
+    import threading
+
+    for _ in range(outer_iterations):
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
+        with executor as tpe:
+            if prepare_args is None:
+                args = []
+            else:
+                args = prepare_args()
+            if pass_barrier:
+                barrier = threading.Barrier(max_workers)
+                args.append(barrier)
+            if pass_count:
+                all_args = [[func, i] + args for i in range(max_workers)]
+            else:
+                all_args = [[func] + args for _ in range(max_workers)]
+            try:
+                futures = []
+                for arg in all_args:
+                    futures.append(tpe.submit(*arg))
+            except RuntimeError as e:
+                print("Spawning {} threads failed with "
+                      "error {} (likely due to resource limits on the "
+                      "system running the tests)".format(max_workers, e))
+            finally:
+                if len(futures) < max_workers and pass_barrier:
+                    barrier.abort()
+            for f in futures:
+                f.result()
