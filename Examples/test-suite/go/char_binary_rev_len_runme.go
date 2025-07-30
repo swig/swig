5f36c9ab9a2739da22d0a464e86f51e2c7242436
Eric Larson <larson.eric.d@gmail.com>
2025-07-30 19:09:17 +0100

    Fix Go LENGTH STRING multi-argument typemaps
    Authored by Eric Larson and tweaked slightly to add missing freearg
    typemaps.
    
    Issue #2937

---
+++ Examples/test-suite/go/char_binary_rev_len_runme.go
@@ -0,0 +1,14 @@
+package main
+
+import . "swigtests/char_binary_rev_len"
+
+func main() {
+	t := NewTest()
+	if t.Strlen("hile") != 4 {
+		panic("bad multi-arg typemap")
+	}
+
+	if t.Strlen("hil\000") != 4 {
+		panic("bad multi-arg typemap")
+	}
+}
