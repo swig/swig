f492bbd6bfd9e53bd9d51d9caa46159aaaaf76e5
Erez Geva <ErezGeva2@gmail.com>
2025-08-01 13:51:55 +0200

    Add typecheck.
    Add test with char_binary_rev_len 2 constructors.
    
    Signed-off-by: Goh, Wei Sheng <wei.sheng.goh@intel.com>

--- Examples/test-suite/java/char_binary_rev_len_runme.java
+++ Examples/test-suite/java/char_binary_rev_len_runme.java
@@ -16,10 +16,17 @@ public class char_binary_rev_len_runme {
 
     String hile = "hile";
     if (t.strlen(hile) != 4)
-      throw new RuntimeException("bad multi-arg typemap");
+      throw new RuntimeException("bad multi-arg typemap 1");
 
     String hil0 = "hil\0";
     if (t.strlen(hil0) != 4)
-      throw new RuntimeException("bad multi-arg typemap");
+      throw new RuntimeException("bad multi-arg typemap 2");
+
+    Test t2 = new Test(hile);
+    if (t2.getisize() != 4)
+      throw new RuntimeException("bad multi-arg typemap 3");
+
+    if (t2.strlen("hil\0a") != 5)
+      throw new RuntimeException("bad multi-arg typemap 4");
   }
 }
