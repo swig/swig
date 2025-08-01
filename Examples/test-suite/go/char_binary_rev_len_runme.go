2bc9ede0c1ec896ae4e40900fb9c5767f45387dd
Erez Geva <ErezGeva2@gmail.com>
2025-08-01 13:51:55 +0200

    Add typecheck.
    Add test with char_binary_rev_len 2 constructors.
    
    Signed-off-by: Goh, Wei Sheng <wei.sheng.goh@intel.com>

--- Examples/test-suite/go/char_binary_rev_len_runme.go
+++ Examples/test-suite/go/char_binary_rev_len_runme.go
@@ -7,10 +7,21 @@ func main() {
 	defer DeleteTest(t)
 
 	if t.Strlen("hile") != 4 {
-		panic("bad multi-arg typemap")
+		panic("bad multi-arg typemap 1")
 	}
 
 	if t.Strlen("hil\000") != 4 {
-		panic("bad multi-arg typemap")
+		panic("bad multi-arg typemap 2")
+	}
+
+	t2 := NewTest("hile")
+	defer DeleteTest(t2)
+
+    if t2.Getisize() != 4 {
+		panic("bad multi-arg typemap 3")
+    }
+
+	if t2.Strlen("hil\000a") != 5 {
+		panic("bad multi-arg typemap 4")
 	}
 }
