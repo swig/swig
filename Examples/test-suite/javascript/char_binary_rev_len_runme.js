fae1cd0b255c15688da1cac8f0af5fd4ff24f781
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-04 17:00:54 +0200

    JS async compatibility for char_binary_rev_len.cpptest

--- Examples/test-suite/javascript/char_binary_rev_len_runme.js
+++ Examples/test-suite/javascript/char_binary_rev_len_runme.js
@@ -1,9 +1,9 @@
 var char_binary_rev_len = require("char_binary_rev_len");
 
 var t = new char_binary_rev_len.Test();
-if (t.strlen('hile') != 4) {
+if (/* await */(t.strlen('hile')) != 4) {
   throw("bad multi-arg typemap 1");
 }
-if (t.strlen('hil\0') != 4) {
+if (/* await */(t.strlen('hil\0')) != 4) {
   throw("bad multi-arg typemap 2");
 }
