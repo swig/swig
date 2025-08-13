2a5b60da847aac675715af8ffbb0b9cb2f0fce89
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2025-08-13 18:42:25 +0100

    [Python] Fix handling of "default" typemap
    Fix handling of "default" typemap applied to method which takes a
    single argument when "-builtin" option is used.
    
    Fixes #2786
    Closes #2790
    Closes #3241
    Closes #3243
    
    Co-authored by @ojwb who also suggested it in discussion of #2790.

--- Examples/test-suite/php/default_args_runme.php
+++ Examples/test-suite/php/default_args_runme.php
@@ -4,7 +4,7 @@ require "tests.php";
 // New functions
 check::functions(array('archiving_on', 'archiving_onw', 'doublevalue1','doublevalue2','seek','seek2','seek3','seek4','seek5','seek6','seek7','seek8','seek9','seeka','seekb','anonymous','booltest','casts1','casts2','chartest1','chartest2','chartest3','chartest4','chartest5','chartest6','dummy','afunction','reftest1','reftest2','chops','exceptionspec','constructorcall','cfunc1','cfunc2','cfunc3','nasty_default_expression','slightly_off_square','subscripted_default_arg'));
 // New classes
-check::classes(array('TrickyInPython','default_args','EnumClass','DerivedEnumClass','Tree','Foo','MyClass1','MyClass2','Except','Statics','Tricky','Klass','ConstMethods','Pointf','CDA'));
+check::classes(array('TrickyInPython','default_args','EnumClass','DerivedEnumClass','Tree','Foo','MyClass1','MyClass2','Except','Statics','Tricky','Klass','ConstMethods','Pointf','CDA','Q'));
 // New vars
 check::globals(array('CONST_NUM'));
 
@@ -113,6 +113,15 @@ check::equal(booltest(), True, "booltest failed");
 check::equal(slightly_off_square(10), 102, "slightly_off_square(10)");
 
 check::equal(slightly_off_square(), 291, "slightly_off_square()");
+
+check::equal(Q::slightly_off_square_static(10), 102, "Q::slightly_off_square_static(10)");
+
+check::equal(Q::slightly_off_square_static(), 291, "Q::slightly_off_square_static()");
+
+$q = new Q();
+check::equal($q->slightly_off_square(10), 102, "Q::slightly_off_square(10)");
+
+check::equal($q->slightly_off_square(), 291, "Q::slightly_off_square()");
 
 check::equal(chartest1(), "x", "chartest1()");
 
