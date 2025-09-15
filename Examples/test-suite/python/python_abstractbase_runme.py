fb18e24fa505da176c5a11a4bc20ab6d9ad95a86
Julien Schueller <schueller@phimeca.com>
2025-09-15 08:41:17 +0200

    Python: Drop collections.abc compatibility

--- Examples/test-suite/python/python_abstractbase_runme.py
+++ Examples/test-suite/python/python_abstractbase_runme.py
@@ -3,10 +3,7 @@ import sys
 # collections.abc requires Python 3.3+
 
 from python_abstractbase import *
-if sys.version_info[0:2] >= (3, 3):
-    import collections.abc
-else:
-    import collections
+import collections.abc
 
 # This is expected to fail with -builtin option
 # Builtin types can't inherit from pure-python abstract bases
@@ -17,20 +14,12 @@ def check_issubclass(derived, base):
     if not issubclass(derived, base):
         raise RuntimeError("{} is not a subclass of {}".format(derived, base))
 
-if sys.version_info[0:2] >= (3, 3):
-    check_issubclass(Mapii, collections.abc.MutableMapping)
-    check_issubclass(Multimapii, collections.abc.MutableMapping)
-    check_issubclass(IntSet, collections.abc.MutableSet)
-    check_issubclass(IntMultiset, collections.abc.MutableSet)
-    check_issubclass(IntVector, collections.abc.MutableSequence)
-    check_issubclass(IntList, collections.abc.MutableSequence)
-else:
-    check_issubclass(Mapii, collections.MutableMapping)
-    check_issubclass(Multimapii, collections.MutableMapping)
-    check_issubclass(IntSet, collections.MutableSet)
-    check_issubclass(IntMultiset, collections.MutableSet)
-    check_issubclass(IntVector, collections.MutableSequence)
-    check_issubclass(IntList, collections.MutableSequence)
+check_issubclass(Mapii, collections.abc.MutableMapping)
+check_issubclass(Multimapii, collections.abc.MutableMapping)
+check_issubclass(IntSet, collections.abc.MutableSet)
+check_issubclass(IntMultiset, collections.abc.MutableSet)
+check_issubclass(IntVector, collections.abc.MutableSequence)
+check_issubclass(IntList, collections.abc.MutableSequence)
 
 mapii = Mapii()
 multimapii = Multimapii()
