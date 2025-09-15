9fff0327a237d49219ed6e1f5db830de95adb4d4
Julien Schueller <schueller@phimeca.com>
2025-09-15 08:47:58 +0200

    Python: Drop sys.version_info<3 examples code

--- Examples/python/import_packages/from_init3/runme.py
+++ Examples/python/import_packages/from_init3/runme.py
@@ -12,20 +12,10 @@ def run_except_on_windows(commandline, env=None):
 testname = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 print("Testing " + testname + " - %module(package=...) + python 'import' in __init__.py")
 
-if sys.version_info < (2, 5):
-    print("  Skipping test as Python version is < 2.5 and does not support relative import syntax: 'from . import x'")
-    sys.exit(0)
-
-if sys.version_info < (3, 0):
-    import py2.pkg2
-    print("  Finished importing py2.pkg2")
-    commandline = sys.executable + " -m py2.pkg2.bar"
-    run_except_on_windows(commandline)
-else:
-    import py3.pkg2
-    print("  Finished importing py3.pkg2")
-    # commandline = sys.executable + " -m py3.pkg2.bar"
-    # run_except_on_windows(commandline)
+import py3.pkg2
+print("  Finished importing py3.pkg2")
+# commandline = sys.executable + " -m py3.pkg2.bar"
+# run_except_on_windows(commandline)
 
 # TODO: Commented out code above results in (from python-3.6 onwards):
 # RuntimeWarning: 'py3.pkg2.bar' found in sys.modules after import of package 'py3.pkg2', but prior to execution of 'py3.pkg2.bar'; this may result in unpredictable behaviour
