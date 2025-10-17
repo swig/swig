c2353b9ab642b680028af2e265f08c03a31ffbef
Derek Huang <djh458@stern.nyu.edu>
2025-10-17 00:38:35 -0400

    FindPCRE2.cmake: add GPLv3-compatible Expat licensing terms

--- Tools/cmake/FindPCRE2.cmake
+++ Tools/cmake/FindPCRE2.cmake
@@ -1,5 +1,25 @@
 # FindPCRE2.cmake
+#------------------------------------------------------------------------------#
+# Copyright (c) 2025 Derek Huang
 #
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#------------------------------------------------------------------------------#
 # CMake find module for Perl Compatible Regular Expressions.
 #
 # In particular, it looks for the more recent PCRE2, not the EOL PCRE library.
