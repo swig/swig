8ad9ed6718652c459bf53e0ce10c18da4988bc0f
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-12 18:41:57 +0100

    test the TypeScript support

--- Examples/test-suite/javascript/cpp11_std_function_runme.js
+++ Examples/test-suite/javascript/cpp11_std_function_runme.js
@@ -5,3 +5,12 @@ var fn = /* await */(cpp11_std_function.return_function(420));
 var r = /* await */(cpp11_std_function.call_function(fn, 420, 'Petka'));
 if (r !== 'Petka passed the test')
   throw new Error('failed function call');
+
+var pass = false
+try {
+  // @ts-expect-error
+  /* await */(cpp11_std_function.call_function('invalid', 420, 'Petka'));
+} catch {
+  pass = true
+}
+if (!pass) throw new Error('call_function accepted invalid argument');
