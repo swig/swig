50588e8200b7480edf3e2b45e8ce03597a56b5fd
didier-jorand <jorandd@vivaldi.net>
2026-01-06 18:49:32 +0100

    various improvements and corrections (thanks to aidanhs)

--- Source/Modules/javascript.cxx
+++ Source/Modules/javascript.cxx
@@ -3268,7 +3268,7 @@ int QuickJSEmitter::initialize(Node *n) {
 int QuickJSEmitter::dump(Node *n) {
   /* Get the module name */
   String *module = Getattr(n, "name");
-  
+
   Template initializer_define(getTemplate("js_initializer_define"));
   initializer_define.replace("$jsname", module).pretty_print(f_header);
 
@@ -3280,9 +3280,23 @@ int QuickJSEmitter::dump(Node *n) {
 
   emitNamespaces();
 
+
+  Iterator it;
+  Template t_createNamespace(getTemplate("quickjs_nspace_definition"));
+  for (it = First(namespaces); it.item; it = Next(it)) {
+    Hash *entry = it.item;
+    String *name = Getattr(entry, NAME);
+    String *name_mangled = Getattr(entry, NAME_MANGLED);
+
+    if(Equal(name, moduleName)) {
+      t_createNamespace.replace("$jsmangledname", name_mangled);
+    }
+  }
+
   // compose the initializer function using a template
   Template initializer(getTemplate("js_initializer"));
   initializer.replace("$jsname", module)
+      .replace("$jsglobalnamespace", t_createNamespace.str())
       .replace("$jsregisterclasses", state.globals(INITIALIZER))
       .replace("$jscreatenamespaces", state.globals(CREATE_NAMESPACES))
       .replace("$jsregisternamespaces", state.globals(REGISTER_NAMESPACES))
@@ -3590,11 +3604,14 @@ int QuickJSEmitter::emitNamespaces() {
 	.replace("$jsmangledname", name_mangled)
 	.pretty_print(f_wrap_cpp);
 
-    Template t_createNamespace(getTemplate("quickjs_nspace_definition"));
-    t_createNamespace.replace("$jsmangledname", name_mangled);
-    Append(state.globals(CREATE_NAMESPACES), t_createNamespace.str());
+    // Don't create the global namespace with the rest, it needs to be done earlier
+    if (!Equal(moduleName, name)) {
+      Template t_createNamespace(getTemplate("quickjs_nspace_definition"));
+      t_createNamespace.replace("$jsmangledname", name_mangled);
+      Append(state.globals(CREATE_NAMESPACES), t_createNamespace.str());
+    }
 
-    // Don't register the global namespace as namespace. It is included in the
+    // And don't register the global namespace as namespace either. It is included in the
     // module object.
     if (!Equal(moduleName, name)) {
       Template t_registerNamespace(getTemplate("quickjs_nspace_registration"));
