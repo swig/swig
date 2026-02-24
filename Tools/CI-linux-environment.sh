8d529bd8cd29efafed32818befe69cf4ebfc0106
Erez Geva <ErezGeva2@gmail.com>
2026-02-24 22:08:50 +0100

    Use cached tools on Linux CI (#3344)
    * Use cached tools on Linux
    
    - Use cached tools for python, ruby, go and javascript/node.
      The cached tools are used in case the version match.
    - Check if required PHP version is already installed.
    - Move compiler settings to `CI-linux-install.sh` and merge them
      with actual installation of versioned GCC.
    - Add `update_env()` and `update_path()` functions
      to use in `CI-linux-install.sh`
      as the script should not use GitHub variables directly.
    - Add support for new versions of ruby and go in `Tools/testflags.py`.
    - Fix tests with integer in configure.ac,
      integer conditions should not use string notation.
    - Print used C/C++ standard.
    
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- Tools/CI-linux-environment.sh
+++ Tools/CI-linux-environment.sh
@@ -7,7 +7,7 @@ set -e # exit on failure (same as -o errexit)
 
 case "$SWIGLANG" in
 	"go")
-		if [[ "$VER" ]]; then
+		if [[ -x "$HOME/bin/gimme" ]]; then
 		  eval "$($HOME/bin/gimme ${VER}.x)"
 		  $HOME/bin/gimme --list
 		fi
@@ -15,22 +15,20 @@ case "$SWIGLANG" in
 	"javascript")
 		case "$ENGINE" in
 			"node"|"napi")
-				export NVM_DIR="$HOME/.nvm"
-				[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-				nvm use ${VER}
+				if [[ -n "$USE_NVM" ]]; then
+					[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+					nvm use ${VER}
+				fi
 				;;
-			*)      ;;
 		esac
 		;;
 	"ruby")
 		if ! command -v rvm; then
-			set +x
 			if [ -f /etc/profile.d/rvm.sh ] ; then
 				source /etc/profile.d/rvm.sh
 			elif [ -f $HOME/.rvm/scripts/rvm ] ; then
 				source $HOME/.rvm/scripts/rvm
 			fi
-			set -x
 		fi
 		;;
 	*)	;;
--- Tools/GHA-linux-install.sh
+++ Tools/GHA-linux-install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+update_env()
+{
+	# Add variable to envirounment
+	local var="$1"
+	shift
+	if [[ -n "$@" ]]; then
+		echo "$var=$@" >> $GITHUB_ENV
+	else
+		echo "$var=true" >> $GITHUB_ENV
+	fi
+}
+update_path()
+{
+	# Add new directory to 'PATH'
+	echo "$@" >> $GITHUB_PATH
+}
+
 #lsb_release -a
 # find location of current script (only works in bash)
 script_dir="$( dirname "${BASH_SOURCE[0]}")"
