693d62fca47373d89c5b8dbb9c1e368baf4b7480
Erez Geva <ErezGeva2@gmail.com>
2026-05-31 23:48:10 +0200

    Follow Lua, implement Ruby function_typedef runtime test (#3462)
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- Examples/test-suite/ruby/function_typedef_runme.rb
+++ Examples/test-suite/ruby/function_typedef_runme.rb
@@ -8,4 +8,7 @@ require 'swig_assert'
 require 'function_typedef'
 include Function_typedef
 
-# Hmm... not sure how to test this.
+addfunc = Function_typedef.addfunc
+swig_assert_equal(do_binop1(addfunc, 11, 22), 33, 'do_binop1')
+swig_assert_equal(do_binop2(addfunc, 17, 51), 68, 'do_binop2')
+swig_assert_equal(do_binop3(addfunc, 32, 47), 79, 'do_binop3')
