fc0a909dbdf5c34f0abc8a2fee08a3a2f6d0142d
Erez Geva <ErezGeva2@gmail.com>
2026-07-22 09:55:19 +0200

    Fix apply_signed_char test
    Add is_char_signed() to skip the tests in Lua.
    As test fails if char is unsigned.
    
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- Examples/test-suite/lua/apply_signed_char_runme.lua
+++ Examples/test-suite/lua/apply_signed_char_runme.lua
@@ -1,5 +1,11 @@
 local v=require("apply_signed_char")
 
+-- The test require char to be signed
+-- Skip the test if char is unsigned
+if v.is_char_signed() == 0 then
+  os.exit()
+end
+
 local smallnum = -127
 assert(v.CharValFunction(smallnum) == smallnum)
 assert(v.CCharValFunction(smallnum) == smallnum)
